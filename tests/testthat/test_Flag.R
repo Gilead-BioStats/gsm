f628d6309d767d2b8bc1339c44c5a892aaf1f7c4
Doug Sanders <douglas.sanders2@gilead.com>
2022-01-14 17:57:37 -0800

    added test_Flag.R

--- tests/testthat/test_Flag.R
+++ tests/testthat/test_Flag.R
@@ -1,35 +1,33 @@
 
 context("Tests for the AE_Poisson_Flag function")
 
-library(safetyData)
-library(tidyr)
-library(dplyr)
-
 ae_input <- AE_Map(
     safetyData::adam_adsl, 
     safetyData::adam_adae
 ) 
 
-ae_prep <- AE_Transform(ae_input)
-ae_anly <- AE_Poisson_Analyze(ae_prep)
-ae_threshold<-AE_Poisson_Autothreshold(ae_anly$Residuals)
+ae_prep <- Transform_EventCount(ae_input)
+ae_anly <- Analyze_Poisson(ae_prep)
+
 
 test_that("output created as expected and has correct structure",{
-    ae_flag <- AE_Poisson_Flag(ae_anly, ae_threshold$ThresholdHi, ae_threshold$ThresholdLo)
-    expect_true(is.data.frame(ae_flag))
-    expect_equal(names(ae_flag), c("SiteID", "N", "TotalCount", "TotalExposure", "Rate", "Unit","LogExposure","Residuals","PredictedCount","PValue","ThresholdHi","ThresholdLo","Flag"))
-    expect_equal(sort(unique(ae_input$SiteID)), sort(ae_flag$SiteID))
+    flag <- Flag(ae_anly)
+    expect_true(is.data.frame(flag))
+    expect_equal(names(flag), c("SiteID", "N", "TotalCount", "TotalExposure", "Unit", "Rate",
+                                "LogExposure","Residuals","PredictedCount","PValue","ThresholdLow",
+                                "ThresholdHigh", "ThresholdCol","Flag"))
+    expect_equal(sort(unique(ae_input$SiteID)), sort(flag$SiteID))
 })
 
 test_that("incorrect inputs throw errors",{
-    expect_error(AE_Poisson_Flag(list(),-1,1))
-    expect_error(AE_Poisson_Flag("Hi", -1,1))
-    expect_error(AE_Poisson_Flag(ae_anly,"1","2"))
+    expect_error(Flag(list(),-1,1))
+    expect_error(Flag("Hi", -1,1))
+    expect_error(Flag(ae_anly,"1","2"))
 })
 
-
-test_that("error given if required column not found",{
-    expect_error(AE_Poisson_Analyze(ae_input %>% rename(total = TotalCount)))
-    expect_error(AE_Poisson_Analyze(ae_input %>% select(-Rate)))
-})
+# not always Pvalue
+# test_that("error given if required column not found",{
+#     expect_error(Flag(ae_anly %>% rename(pval = Pvalue)))
+#     expect_error(Flag(ae_anly %>% select(-Rate)))
+# })
 
