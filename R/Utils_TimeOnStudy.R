5e0b6f58dbf14a38e34f26e338a37ed0972cb620
George Wu <george.wu@gilead.coom>
2022-01-19 16:24:19 -0800

    Updated code to fix NA cases and removed reliance on study completion dataset

--- R/Utils_TimeOnStudy.R
+++ R/Utils_TimeOnStudy.R
@@ -1,16 +1,14 @@
-#' 	Utility Function to Calculate Time on Study
+#' Utility Function to Calculate Time on Study
 #'
 #' Calculates time-on-study for subjects in a study using raw visit dataset
 #'
 #' @param  dfVisit data frame of visit information with required columns SUBJID INVID FOLDERNAME RECORDDATE
-#' @param  dfStud data frame of study completion information with required columns SUBJID COMPYN_STD COMPREAS
+#' @param  dfStud optional, if no data frame is supplied will assume no subject completed study, otherwise
+#' requires data frame of study completion information with columns SUBJID COMPYN_STD COMPREAS
 #' @param  dtSnapshot date of data snapshot, if NA, will impute to be current date
 #'
 #' @examples
-#' library(haven)
-#' dfSubid <- read_sas("Y:/p223/s2231015/cdp/rawdata/subid.sas7bdat")
-#' dfVisit <- read_sas("Y:/p223/s2231015/cdp/rawdata/visdt.sas7bdat")
-#' dfStud <- read_sas("Y:/p223/s2231015/cdp/rawdata/studcomp.sas7bdat")
+#'
 #'
 #' @return dataframe of time on study with a column for site ID, subject ID, and a column for time on study in weeks
 #'
@@ -24,12 +22,8 @@ TimeOnStudy <- function( dfVisit = NA,
                          dtSnapshot = NA ){ ## Checks vector for any zeros
 
 
-  # Stop-if-nots
-  if( ! all(c("SUBJID","INVID") %in% names(dfSubid)) ){
-    stop( "SUBJID and INVID columns are required in subid dataset" )
-  }
-
-  if( ! all(c("SUBJID","COMPYN_STD","COMPREAS") %in% names(dfStud)) ){
+  # Stop if missing required variables
+  if( any(!is.na(dfStud)) & ! all(c("SUBJID","COMPYN_STD","COMPREAS") %in% names(dfStud)) ){
     stop( "SUBJID, COMPYN_STD, COMPREAS columns are required in studcomp dataset" )
   }
 
@@ -37,46 +31,71 @@ TimeOnStudy <- function( dfVisit = NA,
     stop( "SUBJID, INVID, FOLDERNAME, RECORDDATE columns are required in visdt dataset" )
   }
 
+  # remove missing visits
+  dfVisit <- dfVisit %>%
+    dplyr::filter( SUBJID != "" ) %>%
+    dplyr::filter( INVID != "")
+
   # need to double check if any subjid has multiple INVIDs
-  dfUniqueID <- unique(dfTOS %>% select(SUBJID, INVID) )
-  if( any(dfUniqueID) > 2 ) stop( "SUBJID has multiple INVID assignments in visdt")
+  dfUniqueID <- unique(dfVisit %>%
+                         select(SUBJID, INVID) )
+  if( any(table(dfUniqueID$SUBJID) > 2) ) stop( "SUBJID has multiple INVID assignments in visdt")
 
   # Set snapshot date to be today if NA or not a date
   if( is.na(dtSnapshot) |
       !lubridate::is.Date(dtSnapshot) ) dtSnapshot <- Sys.Date()
 
   # Create a tibble with variable indicating if subject completed study or not
-  dfComp <- dfStud %>%
-    group_by( SUBJID ) %>%
-    filter( COMPYN_STD == "Y") %>%
-    count() %>%
-    left_join( unique(dfTOS %>% select(SUBJID, INVID) ), by = c("SUBJID") )
+  # if dataset is empty, then no subject completed
+  if( all(is.na(dfStud)) ) {
+
+    dfComp <- tibble( SUBJID = NA, INVID = NA)
+
+  } else {
+  # if dataset is not empty, create dataset to identify subject who are not still on-going in study
+    dfComp <- dfStud %>%
+      group_by( SUBJID ) %>%
+      filter( COMPYN_STD %in% c("Y","N") ) %>%
+      count() %>%
+      left_join( unique(dfVisit %>% select(SUBJID, INVID) ), by = c("SUBJID") )
+
+  }
 
   # Grab identifier information from SUBJID and remove missing SUBJID
   dfTOS <- dfVisit %>%
     dplyr::select( SUBJID, INVID, FOLDERNAME, RECORDDATE ) %>%
-    dplyr::filter( SUBJID != "" ) %>%
-    dplyr::filter( INVID != "") %>%
+
   # Filter out screening visits
     dplyr::filter( !grepl( "Screening", FOLDERNAME) )
 
   # Add-in a record date based on Snapshot date if subject did not complete study
-  dfComp <- dfComp %>%
+  dfImpute <- unique(dfVisit %>% select(SUBJID, INVID)) %>%
+    filter( ! SUBJID %in% dfComp$SUBJID ) %>%
     mutate( RECORDDATE = dtSnapshot ) %>%
-    mutate( FOLDERNAME = "Imputed" ) %>%
-    select( -c(n) )
+    mutate( FOLDERNAME = "Imputed" )
 
-  dfTOS <- bind_rows( dfComp, dfTOS)
+  dfTOS <- bind_rows( dfTOS, dfImpute )
 
-  calc <- function( x ){
-    difftime( max(x$RECORDDATE), min(x$RECORDDATE), units="days" ) + 1
-  }
 
   # Calculate time on study
+  CalcStudyTime <- function( x ){
+
+    # if all NA, then report NA days
+    if( all(is.na(x$RECORDDATE)) ) {
+      nDays <- NA
+    } else {
+      nDays <- difftime( max(x$RECORDDATE , na.rm=T),
+                         min(x$RECORDDATE , na.rm=T),
+                         units="days" ) + 1
+    }
+    return( nDays )
+  }
+
+
   return (
     dfTOS %>%
     dplyr::group_by( SUBJID, INVID ) %>%
-    do( data.frame( val = calc (.) )) %>%
+    do( data.frame( val =  CalcStudyTime (.) )) %>%
     rename( TimeOnStudy = val , SubjectID = SUBJID, SiteID = INVID )
   )
 
