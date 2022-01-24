01a6e8d7e72c90b9299d913b7e6203499e02ba7f
Doug Sanders <douglas.sanders2@gilead.com>
2022-01-24 15:04:50 -0800

    added more tests for vThreshold including NA values, also fixed but in Flag.F where strColumn NA values became 0 in returned Flag Column. They now appear as NA

--- R/Flag.R
+++ R/Flag.R
@@ -4,7 +4,7 @@
 #'
 #' @param dfAnalyzed data frame in format produced by \code{\link{AE_Poisson_Analyze}}
 #' @param strColumn Name of the Column to use for thresholding
-#' @param vThreshold vector of 2 numeric values representing lower and upper threshold values. If NA is provided for either threshold value it is ignored, and no values are flagged based on the threshold. 
+#' @param vThreshold vector of 2 numeric values representing lower and upper threshold values (<,>). If NA is provided for either threshold value it is ignored, and no values are flagged based on the threshold. 
 #' @param strValueColumn Optional, Name of the Column to use for sign of Flag. If value for that row is higher than median of strValueColumn then Flag = 1, if lower then Flag = -1.
 #'
 #' @return input data frame with the columns added for "ThresholdLow","ThresholdHigh","ThresholdCol" and "Flag" 
@@ -32,7 +32,7 @@ Flag <- function( dfAnalyzed , strColumn="PValue", vThreshold=c(0.05,NA),strFlag
     mutate(Flag = case_when(
       !is.na(vThreshold[1]) & (.data[[strColumn]] < vThreshold[1]) ~ -1,
       !is.na(vThreshold[2]) & (.data[[strColumn]] > vThreshold[2]) ~ 1,
-      TRUE ~ 0
+      !is.na(.data[[strColumn]]) ~ 0
     )) 
 
   # if strFlagValueColumn is supplied, it can only affect sign of Flag (1 or -1)
