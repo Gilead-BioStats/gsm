f2287f47dfb198d713ff564f654604a94d02e4ad
Doug Sanders <douglas.sanders2@gilead.com>
2022-01-27 11:21:31 -0800

    integrated TimeOnStudy to PD_Map_Raw - using TimeOnStudy instead of Exposure to calculate Rate

--- R/PD_Map_Raw.R
+++ R/PD_Map_Raw.R
@@ -3,15 +3,15 @@
 #' Convert from raw data format to needed input format for Safety Assessment
 #' Requires the following raw datasets: subid, ex, pd
 #'
-#' @param dfSubid  subid dataset with columns SUBJID INVID
-#' @param dfEx  ex dataset with columns SUBJID EXSTDAT EXENDAT
 #' @param dfPd  PD dataset with columns SUBJID and rows for each Protocol Deviation 
-
+#' @param  dfVisit data frame of visit information with required columns SUBJID INVID FOLDERNAME RECORDDATE
+#' @param  dfStud optional, if no data frame is supplied will assume no subject completed study, otherwise
+#' requires data frame of study completion information with columns SUBJID COMPYN_STD COMPREAS
+#' @param  dtSnapshot date of data snapshot, if NULL, will impute to be current date
 #' 
-#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate, Unit
+#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Rate, Unit
 #' 
 #' @import dplyr
-#' @import lubridate
 #' 
 #' @export
 
@@ -22,19 +22,15 @@ PD_Map_Raw <- function(
   dtSnapshot = NULL 
 ){    
     ### Requires raw datasets: subid, ex, ae
-    if( is.null(dfSubid) ) stop("subid dataset not found")
-    if( is.null(dfEx) )  stop("ex dataset not found")
-    if( is.null(dfPd) )  stop("ae dataset not found")
+    if( is.null(dfPd) )  stop("dfPd dataset not found")
+    if( is.null(dfVisit) )  stop("dfVisit dataset not found")
+    if( is.null(dfStud) )  stop("dfStud dataset not found")
     
     ### Check required columns
     if( ! all(c("SUBJID") %in% names(dfPd)) ) stop( "SUBJID columns are required in PD dataset" )
             
 
-    dfInput02 <- TimeOnStudy(dfVisit, dfStud, dtSnapshot) %>% 
-      rename(EXSTDAT = .data$firstDate, EXENDAT = .data$lastDate)  %>% 
-      mutate(firstDose = lubridate::ymd(.data$EXSTDAT)) %>%
-      mutate(lastDose = lubridate::ymd(.data$EXENDAT)) %>%
-      mutate(Exposure = time_length(.data$lastDose-.data$firstDose, unit="days")+1)
+    dfInput02 <- TimeOnStudy(dfVisit, dfStud, dtSnapshot) 
 
     # get PD count
     pdCounts <- dfPd %>% 
@@ -46,13 +42,9 @@ PD_Map_Raw <- function(
     dfInput <- dfInput02 %>%
         left_join(pdCounts) %>%
         mutate(Count = ifelse(is.na(.data$Count),0,.data$Count))%>%
-        mutate(Exposure = ifelse(is.na(.data$Exposure),1,.data$Exposure))%>%
         mutate(Unit="Days")%>%
-        mutate(Rate = .data$Count / .data$Exposure) 
-    # count / time on study.
-    # subject / site / count / time on study / rate / unit
-    
-    # don't drop unit, add issue to potentially drop Unit col.
+        mutate(Rate = .data$Count / as.numeric(.data$TimeOnStudy) ) %>%
+        select( .data$SubjectID,   .data$SiteID,  .data$Count ,  .data$TimeOnStudy ,  .data$Rate ,  .data$Unit)
 
     return(dfInput)    
 }
