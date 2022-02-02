be038d1351a76f6ec0b87760ea7937194e51625e
Jeremy Wildfire <jwildfire@gmail.com>
2022-02-02 10:20:40 -0500

    simplified data step

--- R/AE_Map_Adam.R
+++ R/AE_Map_Adam.R
@@ -6,7 +6,10 @@
 #' @param dfADAE ADaM AE data with the following required columns: USUBJID
 #'
 #' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate, Unit
-#' 
+#'
+#' @examples 
+#' dfInput <- AE_Map_Adam( safetyData::adam_adsl, safetyData::adam_adae )
+#'  
 #' @export
 
 AE_Map_Adam <- function( dfADSL, dfADAE ){
@@ -16,28 +19,16 @@ AE_Map_Adam <- function( dfADSL, dfADAE ){
     all(c("USUBJID", "SITEID", "TRTEDT", "TRTSDT") %in% names(dfADSL)),
     "USUBJID" %in% names(dfADAE)
   )
-  dfADAE <- safetyData::adam_adae
   
-  dfADSL <- safetyData::adam_adsl
-  
-  
-  dfADAE <- dfADAE %>% filter(USUBJID %in% dfADSL$USUBJID)
-  
-  dfADSLcount <-  dfADSL %>% 
-    group_by(.data$USUBJID) %>%
-    summarise( Count = as.numeric(sum(dfADAE$USUBJID %in% .data$USUBJID) ))
-  
-  
-  dfInput <- dfADSL %>% 
-    right_join(dfADSLcount, by = 'USUBJID') %>%
+  dfInput <-  dfADSL %>% 
     rename(SubjectID = .data$USUBJID) %>%
     rename(SiteID = .data$SITEID) %>%
-    mutate(Exposure = (as.numeric(.data$TRTEDT - .data$TRTSDT)+1)/7 ) %>% 
+    mutate(Exposure = as.numeric(.data$TRTEDT - .data$TRTSDT)+1) %>% 
+    mutate(Unit = 'Days') %>%
+    rowwise() %>%
+    mutate(Count =sum(dfADAE$USUBJID==.data$SubjectID)) %>% 
     mutate(Rate = .data$Count/.data$Exposure) %>%
-    mutate(Rate = round(.data$Rate, 4) ) %>%
-    mutate(Unit = 'Week') %>%
     select(.data$SubjectID,.data$SiteID, .data$Count, .data$Exposure, .data$Rate, .data$Unit)
-  
-  
+
   return(dfInput)
 }
