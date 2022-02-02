774fecaf50bc6b9ba3392c9c4bb5da9554ef1cfb
Jeremy Wildfire <jwildfire@gmail.com>
2022-02-02 15:50:09 -0500

    refactor map functions and clear checks. #77

--- R/AE_Map_Raw.R
+++ R/AE_Map_Raw.R
@@ -3,10 +3,10 @@
 #' Convert from raw data format to needed input format for Safety Assessment
 #' Requires the following raw datasets: subid, ex, ae, da (optional)
 #'
-#' @param dfAe AE dataset with columns SUBJID and rows for each AE record
-#' @param dfExposure exposure dataset calculated via \code{\link{Transform_Exposure}}
+#' @param dfAE AE dataset with columns SUBJID and rows for each AE record
+#' @param dfExposure exposure dataset calculated via \code{\link{TreatmentExposure}}
 #' 
-#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate, Unit
+#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate
 #' 
 #' 
 #' @export
@@ -16,14 +16,14 @@ AE_Map_Raw <- function( dfAE = NULL, dfExposure = NULL){
         "ae dataset not found"=is.data.frame(dfAE),
         "exposure dataset is not found"=is.data.frame(dfExposure),
         "USUBJID column not found in dfAE"="USUBJID" %in% names(dfAE),
-        "SubjectID, SiteID, Exposure and columns not found in dfExposure"=all(c("SubjectID","SiteID","Exposure") %in% names(dfAE))
+        "SubjectID, SiteID and Exposure columns not found in dfExposure"=all(c("SubjectID","SiteID","Exposure") %in% names(dfAE))
     )
 
     dfInput <-  dfExposure %>% 
         rowwise() %>%
-        mutate(Count =sum(dfADAE$USUBJID==.data$SubjectID)) %>% 
+        mutate(Count =sum(dfAE$USUBJID==.data$SubjectID)) %>% 
         mutate(Rate = .data$Count/.data$Exposure) %>%
-        select(.data$SubjectID,.data$SiteID, .data$Count, .data$Exposure, .data$Rate, .data$Unit)
+        select(.data$SubjectID,.data$SiteID, .data$Count, .data$Exposure, .data$Rate)
         
     return(dfInput)
 }
--- R/PD_Map_Raw.R
+++ R/PD_Map_Raw.R
@@ -3,44 +3,28 @@
 #' Convert from raw data format to needed input format for Safety Assessment
 #' Requires the following raw datasets: subid, ex, pd
 #'
-#' @param dfTos data.frame Time on Study data in format produced by \code{\link{TimeOnStudy}} 
-#' @param dfPd  PD dataset with columns SUBJID and rows for each Protocol Deviation 
-#'
+#' @param dfPD  PD dataset with columns USUBJID and rows for each Protocol Deviation 
+#' @param dfTOS Time on Study data.set in format produced by \code{\link{TimeOnStudy}} 
 #' 
-#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Rate, Unit
+#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate
 #' 
 #' @import dplyr
 #' 
 #' @export
 
-PD_Map_Raw <- function( 
-  dfTos = NULL,
-  dfPd = NULL
-){    
-    ### Requires raw datasets: subid, ex, ae
-    if( is.null(dfPd) )  stop("dfPd dataset not found")
-    if( is.null(dfTOS) )  stop("dfTOS dataset not found")
+PD_Map_Raw <- function( dfPD = NULL, dfTOS = NULL ){
+    stopifnot(
+        "PD dataset not found"=is.data.frame(dfPD),
+        "Time on Study dataset is not found"=is.data.frame(dfTOS),
+        "USUBJID column not found in dfPD"="USUBJID" %in% names(dfPD),
+        "SubjectID, SiteID and TimeOnStudy columns not found in dfTOS"=all(c("SubjectID","SiteID","Exposure") %in% names(dfTOS))
+    )
 
-    
-    ### Check required columns
-    if( ! all(c("SUBJID") %in% names(dfPd)) ) stop( "SUBJID columns are required in PD dataset" )
-    if( ! all(c("SubjectID" ,"SiteID", "firstDate","lastDate", "TimeOnStudy") %in% names(dfTOS)) ) stop( "SUBJID columns are required in PD dataset" )
-            
+    dfInput <-  dfTOS %>% 
+        rowwise() %>%
+        mutate(Count = sum(dfPD$USUBJID==.data$SubjectID)) %>% 
+        mutate(Rate = .data$Count/.data$TimeOnStudy) %>%
+        select(.data$SubjectID, .data$SiteID, .data$Count, .data$TimeOnStudy, .data$Rate)
 
-    # get PD count
-    pdCounts <- dfPd %>% 
-        mutate(SubjectID = as.character(.data$SUBJID)) %>%
-        group_by(.data$SubjectID)%>%
-        summarise(Count=n())%>%
-        select(.data$SubjectID, .data$Count)
-
-    dfInput <- dfTOS %>%
-        left_join(pdCounts) %>%
-        mutate(Count = ifelse(is.na(.data$Count),0,.data$Count))%>%
-        mutate(TimeOnStudy = as.numeric(.data$TimeOnStudy)) %>%
-        mutate(Unit="Days")%>%
-        mutate(Rate = .data$Count / .data$TimeOnStudy ) %>%
-        select( .data$SubjectID,   .data$SiteID,  .data$Count ,  .data$TimeOnStudy ,  .data$Rate ,  .data$Unit)
-
-    return(dfInput)    
+    return(dfInput)
 }
