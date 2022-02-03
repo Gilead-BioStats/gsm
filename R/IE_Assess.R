99d817313a0f542fa9d31cdbf42ae7a2a75ab464
Doug Sanders <douglas.sanders2@gilead.com>
2022-02-03 12:20:15 -0800

    Changed IE_Map_Raw to only output columns SubjectID SiteID Count, NOTE that count is set to what was previously invalidmissing.  IE_Assess updated to reflect this change.  Investigated changing IE_Analyze's output but the proposed columns (i.e onl SideID, N, Pvalue) would then not work with the Flagging (which wants  Estimate according to IE_Assess wrapper.  #91

--- R/IE_Assess.R
+++ R/IE_Assess.R
@@ -1,6 +1,6 @@
 #' Inclusion/Exclusion Assessment 
 #' 
-#' @param dfInput input dataset with subject information containing : (required variables)
+#' @param dfInput input dataset with subject information containing :  SubjectID SiteID Count
 #' @param nThreshold threshold values Flagging, integer values greater than will be flagged.
 #' @param cLabel Assessment label 
 #' @param bDataList Should all assessment datasets be returned as a list? If False (the default), only the finding data frame is returned
@@ -12,7 +12,7 @@
 IE_Assess <- function( dfInput, nThreshold=0.5,  cLabel="", bDataList=FALSE){
   lAssess <- list()
   lAssess$dfInput <- dfInput
-  lAssess$dfTransformed <- gsm::Transform_EventCount( lAssess$dfInput, cCountCol = "InvalidMissing")
+  lAssess$dfTransformed <- gsm::Transform_EventCount( lAssess$dfInput, cCountCol = "Count")
   lAssess$dfAnalyzed <- gsm::IE_Analyze( lAssess$dfTransformed ) 
   lAssess$dfFlagged <- gsm::Flag( lAssess$dfAnalyzed , vThreshold = c(NA,nThreshold), strColumn = "Estimate" )
   lAssess$dfSummary <- gsm::Summarize( lAssess$dfFlagged, cAssessment="Inclusion/Exclusion", cLabel= cLabel)
