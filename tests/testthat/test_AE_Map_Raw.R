2a71cf1d7896e4f6f828473ccfb73feecae689b2
Doug Sanders <douglas.sanders2@gilead.com>
2022-02-04 11:37:54 -0800

    added some tests for AE_Map_Raw using clindata

--- tests/testthat/test_AE_Map_Raw.R
+++ tests/testthat/test_AE_Map_Raw.R
@@ -1,13 +1,68 @@
-#test_that("output created as expected and has correct structure",{
-    # TODO update once Raw test data is available
-    #ae_input <- AE_Map_Raw(NULL)
-    #expect_true(is.data.frame(ae_input))
-    #expect_equal(
-    #    names(ae_input), 
-    #    c("SubjectID","SiteID","FirstDoseDate","LastDoseDate","Count","Exposure","Rate","Unit"))
-#})
+
+dfExposure <- TreatmentExposure(  dfEx = clindata::raw_ex,  dfSdrg = NULL, dtSnapshot = NULL)
+
+test_that("output created as expected and has correct structure",{
+  ae_input <- AE_Map_Raw(dfAE = clindata::raw_ae,dfExposure = dfExposure )
+   expect_true(is.data.frame(ae_input))
+  
+   expect_equal(
+   names(ae_input),
+   c("SubjectID","SiteID","Count","Exposure","Rate"))
+ })
 
 test_that("incorrect inputs throw errors",{
     expect_error(AE_Map_Raw(list(), list()))
     expect_error(AE_Map_Raw("Hi","Mom"))
 })
+
+
+test_that("incorrect inputs throw errors",{
+ 
+  expect_error(AE_Map_Raw(list(), list()))
+  expect_error(AE_Map_Raw( clindata::raw_ae, list()))
+  expect_error(AE_Map_Raw(list(),  dfExposure))
+  expect_error(AE_Map_Raw("Hi","Mom"))
+})
+
+
+test_that("error given if required column not found",{
+  expect_error(
+    AE_Map_Raw( 
+      clindata::raw_ae %>% rename(ID = SUBJID), 
+      dfExposure
+    )
+  )
+  
+  # Why does this not throw error?
+  
+  # expect_error(
+  #   AE_Map_Raw( 
+  #     clindata::raw_ae , 
+  #     dfExposure  %>% select(-SubjectID)
+  #   )
+  # )
+  
+  expect_error(
+    AE_Map_Raw( 
+      clindata::raw_ae , 
+      dfExposure  %>% select(-Exposure)
+    )
+  )
+  
+  expect_error(
+    AE_Map_Raw( 
+      clindata::raw_ae , 
+      dfExposure  %>% select(-SiteID)
+    )
+  )
+  
+  # renaming or dropping non-required cols is fine
+  expect_silent(
+    AE_Map_Raw( 
+      clindata::raw_ae  %>% select(-PROJECT), 
+      dfExposure
+    )
+  )
+})
+
+head(clindata::raw_ae)
