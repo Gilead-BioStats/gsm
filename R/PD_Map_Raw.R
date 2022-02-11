f1ffb450e834c6d1938592b7d577939104d61973
Doug Sanders <douglas.sanders2@gilead.com>
2022-02-10 17:39:26 -0800

    tests, docs and example done - #117

--- R/PD_Map_Raw.R
+++ R/PD_Map_Raw.R
@@ -3,10 +3,33 @@
 #' Convert from raw data format to needed input format for Safety Assessment
 #' Requires the following raw datasets: subid, ex, pd
 #'
+#' @details
+#' 
+#' This function combines raw AE data with exposure data calculated by clindata::TreatmentExposure to create the required input for \code{\link{AE_Assess}}. 
+#' 
+#' @section Data Specification:
+#' 
+#' This function creates an input dataset for the Adverse Event Assessment (\code{\link{AE_Assess}}) by adding Adverse Event Counts to basic subject-level treatment exposure data from \code{\link{TreatmentExposure}}. 
+#' 
+#' The following columns are required:
+#' - `dfPD`
+#'     - `SUBJID` - Unique subject ID
+#' - `dfTOS`
+#'     - `SubjectID` - Unique subject ID
+#'     - `SiteID` - Site ID
+#'     - `Exposure` - Treatment Exposure in days.
+#' 
+#' 
+#'
 #' @param dfPD  PD dataset with SUBJID column and rows for each Protocol Deviation 
-#' @param dfTOS Time on Study data.set in format produced by \code{\link{TimeOnStudy}} 
+#' @param dfTOS Time on Study data.set in format produced by clindata::TimeOnStudy 
 #' 
 #' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count, Exposure, Rate
+#' 
+#' 
+#' @examples
+#'  dfTos <- clindata::TimeOnStudy(dfVisit = clindata::raw_visdt,dfStud = clindata::raw_studcomp) 
+#'  dfInput <- PD_Map_Raw(dfPD = clindata::raw_protdev,dfTOS = dfTos)
 #' 
 #' @import dplyr
 #' 
