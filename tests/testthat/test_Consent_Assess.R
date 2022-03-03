6367d8822e710a81c082f57fcb76b27b0cb79f3a
Doug Sanders <douglas.sanders2@gilead.com>
2022-03-03 14:00:33 -0800

    added tests to Consent_Assess

--- tests/testthat/test_Consent_Assess.R
+++ tests/testthat/test_Consent_Assess.R
@@ -27,12 +27,17 @@ test_that("list of df created when bDataList=TRUE",{
     expect_equal(names(consent_list),c('dfInput','dfTransformed','dfAnalyzed','dfFlagged','dfSummary'))
 })
 
+# Add tests for NA values in columns: SubjectID, SiteID, Count
+# Add tests for nThreshold
+
 test_that("incorrect inputs throw errors",{
     expect_error(Consent_Assess(list()))
     expect_error(Consent_Assess("Hi"))
     expect_error(Consent_Assess(consent_input, cLabel=123))
     expect_error(Consent_Assess(consent_input, cMethod="abacus"))
     expect_error(Consent_Assess(consent_input, bDataList="Yes"))
+    expect_error(Consent_Assess(consent_input, nThreshold = "A"))
+    expect_error(Consent_Assess(consent_input, nThreshold = c(1,2)))
 })
 
 
@@ -54,8 +59,32 @@ target_output <- tibble::tribble(
   "Main Consent",     "",       3, 1L,      1L,     1
 )
 
+target_output_NA_SiteID <- tibble::tribble(
+  ~Assessment, ~Label, ~SiteID, ~N, ~Score, ~Flag,
+  "Main Consent",     "",       2, 2L,      1L,     1,
+  "Main Consent",     "",       3, 1L,      1L,     1,
+  "Main Consent",     "",       NA, 1L,      1L,     1
+)
+
 test_that("output is correct given example input",{
   expect_equal(consent_summary,target_output)
+})
+
+test_that("NA in dfInput$SubjectID does not affect resulting dfSummary output for Consent_Assess",{
+  consent_input_in <- consent_input; consent_input_in[1:2,"SubjectID"] = NA
+  consent_summary <- Consent_Assess(consent_input_in, bDataList=FALSE)
+  expect_equal(consent_summary,target_output)
+})
+
+test_that("NA in dfInput$SiteID results in NA for SiteID in dfSummary output for Consent_Assess",{
+  consent_input_in <- consent_input; consent_input_in[1,"SiteID"] = NA
+  consent_summary <- Consent_Assess(consent_input_in, bDataList=FALSE)
+  expect_equal(consent_summary,target_output_NA_SiteID)
+})
+
+test_that("NA in dfInput$Count results in Error for Consent_Assess",{
+  consent_input_in <- consent_input; consent_input_in[1,"Count"] = NA
+  expect_error(Consent_Assess(consent_input_in))
 })
 
 
