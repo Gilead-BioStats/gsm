cd7e13d502981775b00f2b53b2421211d5101b20
Doug Sanders <douglas.sanders2@gilead.com>
2022-03-02 16:20:18 -0800

    addressed 206 Consent_Map_Raw QC items

--- R/Consent_Map_Raw.R
+++ R/Consent_Map_Raw.R
@@ -18,7 +18,7 @@
 #' - `dfRDSL`
 #'     - `SubjectID` - Unique subject ID
 #'     - `SiteID` - Site ID
-#'     - `RandData` - Randomization Date
+#'     - `RandDate` - Randomization Date
 #'
 #' @param dfConsent consent data frame with columns: SUBJID, CONSCAT_STD , CONSYN , CONSDAT.
 #' @param dfRDSL Subject-level Raw Data (RDSL) required columns: SubjectID SiteID RandDate.
@@ -27,14 +27,13 @@
 #' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count.
 #' 
 #' @import dplyr
-#' @import lubridate
 #' 
 #' @examples
 #'
 #' input <- Consent_Map_Raw(
 #'  dfConsent = clindata::raw_consent, 
 #'  dfRDSL = clindata::rawplus_rdsl, 
-#'  strConsentReason = NULL
+#'  strConsentReason = "mainconsent"
 #' )
 #' 
 #' @export 
@@ -44,19 +43,39 @@ Consent_Map_Raw <- function( dfConsent,dfRDSL, strConsentReason = "mainconsent")
     "dfConsent dataset not found"=is.data.frame(dfConsent),
     "dfRDSL dataset is not found"=is.data.frame(dfRDSL),
     "SUBJID, CONSCAT_STD , CONSYN , CONSDAT column not found in dfConsent"=c("SUBJID", "CONSCAT_STD" , "CONSYN" , "CONSDAT" ) %in% names(dfConsent),
-    "SubjectID, SiteID and RandData column not found in dfIxrsrand"= c("SubjectID", "SiteID" , "RandDate") %in% names(dfRDSL)
+    "SubjectID, SiteID and RandDate column not found in dfRDSL"= c("SubjectID", "SiteID" , "RandDate") %in% names(dfRDSL),
+    "NAs found in SUBJID column of dfConsent" = all(!is.na(dfConsent$SUBJID)),
+    "NAs found in SubjectID column of dfRDSL" = all(!is.na(dfRDSL$SubjectID))
   )
+  if(!is.null(strConsentReason)){
+    stopifnot(
+      "strConsentReason is not character"=is.character(strConsentReason),
+      "strConsentReason has multiple values, specify only one"= length(strConsentReason)==1
+    )
+  }
+
+  
+  dfRDSLSiteIDNACount <- sum(is.na(dfRDSL[['SiteID']]))
+  if(dfRDSLSiteIDNACount>0){
+    warning(paste0("Dropped ",dfRDSLSiteIDNACount," record(s) from dfRDSL where SiteID is NA."))
+    dfRDSL <- dfRDSL %>% filter(!is.na(.data[['SiteID']]))
+  }
+  
   
   dfConsent<- dfConsent %>%
     select(.data$SUBJID, .data$CONSCAT_STD , .data$CONSYN , .data$CONSDAT)%>%
     rename(SubjectID = .data$SUBJID)
   
+  missIE <- anti_join( dfConsent, dfRDSL, by="SubjectID")
+  if( nrow(missIE) > 0 ) warning("Not all SubjectID in dfConsent found in dfRDSL")
+  
   dfInput <- dfRDSL %>%
     select(.data$SubjectID, .data$SiteID, .data$RandDate)%>%
-    left_join(dfConsent, by='SubjectID')
+    inner_join(dfConsent, by='SubjectID')
   
     if(!is.null(strConsentReason)){
       dfInput <- dfInput %>% filter(tolower(.data$CONSCAT_STD) == tolower(strConsentReason))
+      stopifnot("supplied strConsentReason not found in data" = nrow(dfInput) != 0 ) 
     }
   
   dfInput <-  dfInput %>%
@@ -65,7 +84,7 @@ Consent_Map_Raw <- function( dfConsent,dfRDSL, strConsentReason = "mainconsent")
     mutate(flag_missing_rand = is.na(.data$RandDate))%>%
     mutate(flag_date_compare = .data$CONSDAT >= .data$RandDate ) %>%
     mutate(any_flag=.data$flag_noconsent | .data$flag_missing_consent | .data$flag_missing_rand | .data$flag_date_compare) %>%
-    mutate(Count = as.numeric(.data$any_flag)) %>%
+    mutate(Count = as.numeric(.data$any_flag, na.rm = TRUE)) %>%
     select(.data$SubjectID, .data$SiteID, .data$Count) 
 
   return(dfInput)
--- tests/testthat/test_Consent_Map_Raw.R
+++ tests/testthat/test_Consent_Map_Raw.R
@@ -107,6 +107,28 @@ test_that("error given if required column not found",{
       dfRDSL = dfRDSL_test %>% select(-RGMNDTN)
     )
   )
+  
+  
+  expect_error(
+    Consent_Map_Raw(
+      dfConsent_test,
+      dfRDSL = dfRDSL_test %>% select(- SubjectID)
+    )
+  )
+  
+  expect_error(
+    Consent_Map_Raw(
+      dfConsent_test,
+      dfRDSL = dfRDSL_test %>% select(- SiteID)
+    )
+  )
+  
+  expect_error(
+    Consent_Map_Raw(
+      dfConsent_test,
+      dfRDSL = dfRDSL_test %>% select(- RandDate)
+    )
+  )
 })
 
 
@@ -117,6 +139,84 @@ test_that("output is correct given clindata example input",{
 
   
   
+})
+
+dfConsent_test_NA1 <- tibble::tribble(~SUBJID, ~CONSCAT_STD , ~CONSYN , ~CONSDAT,
+                                  NA,       "MAINCONSENT",    "Yes", "2014-12-25",
+                                  1,       "MAINCONSENT",     "No", "2014-12-25"  )
+
+dfRDSL_test_NA1<- tibble::tribble(~SubjectID, ~SiteID, ~RandDate,
+                               1,  1, "2013-12-25",
+                               2,  2, "2015-12-25")
+
+
+
+dfConsent_test_NA2 <- tibble::tribble(~SUBJID, ~CONSCAT_STD , ~CONSYN , ~CONSDAT,
+                                      1,       "MAINCONSENT",    "Yes", "2014-12-25",
+                                      1,       "MAINCONSENT",     "No", "2014-12-25"  )
+
+dfRDSL_test_NA2<- tibble::tribble(~SubjectID, ~SiteID, ~RandDate,
+                                  NA,  1, "2013-12-25",
+                                  2,  2, "2015-12-25")
+
+
+
+
+
+test_that("NA's in SubjectID and SUBJID are handled correctly",{
+  expect_error(Consent_Map_Raw(dfConsent = dfConsent_test_NA1, dfRDSL = dfRDSL_test_NA1))
+  expect_error(Consent_Map_Raw(dfConsent = dfConsent_test_NA2, dfRDSL = dfRDSL_test_NA2))
+ 
+})
+
+test_that("Incorrect strConsentReason throws errors",{
+  expect_error(Consent_Map_Raw(dfConsent = dfConsent_test, dfRDSL = dfRDSL_test, strConsentReason = c("A","B")))
+  expect_error(Consent_Map_Raw(dfConsent = dfConsent_test, dfRDSL = dfRDSL_test, strConsentReason = 1.23))
+  expect_error(Consent_Map_Raw(dfConsent = dfConsent_test, dfRDSL = dfRDSL_test, strConsentReason = "Name_Not_in_data"))
+  
+})
+
+
+dfConsent_test2 <- tibble::tribble(~SUBJID, ~CONSCAT_STD , ~CONSYN , ~CONSDAT,
+                                  1,         "MAINCONSENT",    "Yes", "2014-12-25",
+                                  1,       "MAINCONSENT",     "No", "2014-12-25")
+ 
+dfRDSL_test2 <- tibble::tribble(~SubjectID, ~SiteID, ~RandDate,
+                               1,  1, "2013-12-25",
+                               2,  2, "2015-12-25")
+
+
+dfInput_test2 <-  tibble::tribble(
+  ~SubjectID, ~SiteID, ~Count,
+  1,       1,   1,
+  1,       1,   1)
+
+
+test_that("NA's in data are removed and handled correctly",{
+ dfConsent_test_in <-  dfConsent_test2; dfConsent_test_in[1,2] = NA
+  expect_equal( tibble::tribble(
+    ~SubjectID, ~SiteID, ~Count,
+    1,       1,   1),  Consent_Map_Raw(dfConsent = dfConsent_test_in, dfRDSL = dfRDSL_test2))
+  
+  dfConsent_test_in <-  dfConsent_test2; dfConsent_test_in[1,3] = NA
+  expect_equal( tibble::tribble(
+    ~SubjectID, ~SiteID, ~Count,
+    1,       1,   1,
+    1,       1,   1),  Consent_Map_Raw(dfConsent = dfConsent_test_in, dfRDSL = dfRDSL_test2))
+  
+  dfRDSL_in <-  dfRDSL_test2; dfRDSL_in[2,2] = NA
+  expect_equal( tibble::tribble(
+    ~SubjectID, ~SiteID, ~Count,
+    1,       1,   1,
+    1,       1,   1),  suppressWarnings(Consent_Map_Raw(dfConsent = dfConsent_test2, dfRDSL = dfRDSL_in)))
+  
+
+ 
+})
+
+test_that("Warning provided for missing (NA) SiteID's in dfRDSL",{
+  dfRDSL_in <-  dfRDSL_test2; dfRDSL_in[2,2] = NA
+  expect_warning(Consent_Map_Raw( dfConsent = dfConsent_test2, dfRDSL = dfRDSL_in ))
 })
 
 
