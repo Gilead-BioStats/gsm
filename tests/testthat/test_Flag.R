e53884164b4835176ce5d4c741d7117b08b1c84f
Matt Roumaya <matt.rouyama@gilead.com>
2022-03-04 15:37:42 +0000

    closes #229 - qc updates for Flag()

--- tests/testthat/test_Flag.R
+++ tests/testthat/test_Flag.R
@@ -13,6 +13,8 @@ test_that("output created as expected and has correct structure",{
     expect_true(is.data.frame(flag))
     expect_equal(sort(unique(ae_input$SiteID)), sort(flag$SiteID))
     expect_true(all(names(ae_anly_wilcoxon) %in% names(flag)))
+    expect_equal(names(flag), c("SiteID", "N", "TotalCount", "TotalExposure", "Rate", "Estimate",
+                                "PValue", "ThresholdLow", "ThresholdHigh", "ThresholdCol", "Flag"))
 })
 
 test_that("strFlagValueColumn paramter works as intended",{
@@ -40,18 +42,18 @@ test_that("Expected Columns are added to dfFlagged",{
 })
 
 test_that("vThreshold parameter works as intended",{
-sim1 <- Flag(data.frame(vals=seq(1:100)), strColumn="vals", vThreshold=c(10,NA))
+sim1 <- Flag(data.frame(SiteID = seq(1:100), vals=seq(1:100)), strColumn="vals", vThreshold=c(10,NA))
 expect_equal(sim1$Flag, c(rep(-1,9), rep(0,91)))
-sim2 <- Flag(data.frame(vals=seq(1:100)), strColumn="vals", vThreshold=c(NA,91))
+sim2 <- Flag(data.frame(SiteID = seq(1:100), vals=seq(1:100)), strColumn="vals", vThreshold=c(NA,91))
 expect_equal(sim2$Flag, c(rep(0,91), rep(1,9)))
-sim3 <- Flag(data.frame(vals=seq(1:100)), strColumn="vals", vThreshold=c(2,91))
+sim3 <- Flag(data.frame(SiteID = seq(1:100), vals=seq(1:100)), strColumn="vals", vThreshold=c(2,91))
 expect_equal(sim3$Flag, c(-1,rep(0,90), rep(1,9)))
-sim4 <- Flag(data.frame(vals=seq(from = -100, to = 100)), strColumn="vals", vThreshold=c(-91,91))
+sim4 <- Flag(data.frame(SiteID = seq(1:201), vals=seq(from = -100, to = 100)), strColumn="vals", vThreshold=c(-91,91))
 expect_equal(sim4$Flag,c(rep(-1,9),rep(0,183), rep(1,9)))
 })
 
 test_that("NA values in strColumn result in NA in Flag column",{
-  NAsim <- Flag(data.frame(vals=c(seq(1:90),rep(NA,10))), strColumn="vals", vThreshold=c(10,NA))
+  NAsim <- Flag(data.frame(SiteID = seq(1:100), vals=c(seq(1:90),rep(NA,10))), strColumn="vals", vThreshold=c(10,NA))
   expect_equal(NAsim$Flag, c(rep(-1,9), rep(0,81),rep(NA,10)))
 })
 
