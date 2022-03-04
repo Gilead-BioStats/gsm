c2c6dc17532131f5f2dde0babffa4d53da31faae
Matt Roumaya <matt.rouyama@gilead.com>
2022-03-04 21:22:09 +0000

    closes #228 - qc updates for Analyze_Wilcoxon()

--- tests/testthat/test_Analyze_Wilcoxon.R
+++ tests/testthat/test_Analyze_Wilcoxon.R
@@ -15,10 +15,14 @@ test_that("output created as expected and has correct structure",{
 test_that("incorrect inputs throw errors",{
     expect_error(Analyze_Wilcoxon(list()))
     expect_error(Analyze_Wilcoxon("Hi"))
+    expect_error(Analyze_Wilcoxon(ae_prep, strOutcome = "oops"))
+    expect_error(Analyze_Wilcoxon(ae_prep, strOutcome = 1))
+    expect_error(Analyze_Wilcoxon(ae_prep %>% mutate(SiteID = ifelse(SiteID == first(SiteID), NA, SiteID))))
 })
 
 test_that("error given if required column not found",{
   expect_error(Analyze_Wilcoxon(ae_input %>% rename(total = TotalCount)))
   expect_error(Analyze_Wilcoxon(ae_input %>% select(-Rate)))
+  expect_error(Analyze_Wilcoxon(ae_input %>% select(-SiteID)))
 })
 
