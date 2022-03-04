fd507adb1b399f9b27ea81ec47783bb690e4d1b5
Matt Roumaya <matt.rouyama@gilead.com>
2022-03-04 22:04:39 +0000

    closes 227 - qc updates for Analyze_Poisson()

--- R/Analyze_Poisson.R
+++ R/Analyze_Poisson.R
@@ -1,52 +1,54 @@
 #' Poisson Analysis - Site Residuals
-#' 
+#'
 #' @details
 #'
-#' Fits a Poisson model to site level data and adds columns capturing Residual and Predicted Count for each site. 
-#' 
-#' @section Statistical Methods:
-#' 
-#' This function fits a poisson model to site-level data and then calculates residuals for each site. The poisson model is run using standard methods in the `stats` package by fitting a `glm` model with family set to `poisson` using a "log" link. Site-level residuals are calculated  `stats::predict.glm` via `broom::augment`. 
-#' 
-#' @section Data Specification: 
-#' 
-#' The input data (` dfTransformed`) for the Analyze_Poisson is typically created using \code{\link{Transform_EventCount}} and should be one record per Site with columns for: 
-#' - `SubjectID` - Unique subject ID
-#' - `SiteID` - Site ID
-#' - `TotalCount` - Number of Events 
-#' - `TotalExposure` - Number of days of exposure 
+#' Fits a Poisson model to site level data and adds columns capturing Residual and Predicted Count for each site.
 #'
-#' @param dfTransformed data.frame in format produced by \code{\link{Transform_EventCount}}. Must include SubjectID, SiteID, TotalCount and TotalExposure. 
-#' 
+#' @section Statistical Methods:
+#'
+#' This function fits a poisson model to site-level data and then calculates residuals for each site. The poisson model is run using standard methods in the `stats` package by fitting a `glm` model with family set to `poisson` using a "log" link. Site-level residuals are calculated  `stats::predict.glm` via `broom::augment`.
+#'
+#' @section Data Specification:
+#'
+#' The input data (`dfTransformed`) for Analyze_Poisson is typically created using \code{\link{Transform_EventCount}} and should be one record per site with required columns for:
+#' - `SiteID` - Site ID
+#' - `N` - Number of participants
+#' - `TotalCount` - Number of Adverse Events
+#' - `TotalExposure` - Number of days of exposure
+#' - `Rate` - Rate of exposure (TotalCount / TotalExposure)
+#'
+#' @param dfTransformed data.frame in format produced by \code{\link{Transform_EventCount}}. Must include SubjectID, SiteID, TotalCount and TotalExposure.
+#'
+#' @import dplyr
 #' @importFrom stats glm offset poisson pnorm
 #' @importFrom broom augment
-#' 
-#' @return input data frame with columns added for "Residuals" and "PredictedCount"
-#' 
-#' @examples 
+#'
+#' @return input data.frame with columns added for "Residuals" and "PredictedCount"
+#'
+#' @examples
 #' dfInput <- AE_Map_Adam( safetyData::adam_adsl, safetyData::adam_adae )
 #' dfTransformed <- Transform_EventCount( dfInput, cCountCol = 'Count', cExposureCol = "Exposure" )
-#' dfAnalyzed <- Analyze_Poisson( dfTransformed ) 
-#' 
+#' dfAnalyzed <- Analyze_Poisson( dfTransformed )
+#'
 #' @export
-
 
 Analyze_Poisson <- function( dfTransformed ){
     stopifnot(
-        is.data.frame(dfTransformed), 
-        all(c("SiteID", "N", "TotalExposure", "TotalCount") %in% names(dfTransformed))    
+        "dfTransformed is not a data.frame" = is.data.frame(dfTransformed),
+        "One or more of these columns: SiteID, N, TotalExposure, TotalCount, Rate" = all(c("SiteID", "N", "TotalExposure", "TotalCount", "Rate") %in% names(dfTransformed)),
+        "NA value(s) found in SiteID" = all(!is.na(dfTransformed[["SiteID"]]))
     )
 
     dfModel <- dfTransformed %>% mutate(LogExposure = log( .data$TotalExposure) )
 
     cModel <- stats::glm(
-        TotalCount ~ stats::offset(LogExposure), family=stats::poisson(link="log"), 
+        TotalCount ~ stats::offset(LogExposure), family=stats::poisson(link="log"),
         data=dfModel
     )
 
-    dfAnalyzed <- broom::augment(cModel, dfModel, type.predict = "response") %>% 
+    dfAnalyzed <- broom::augment(cModel, dfModel, type.predict = "response") %>%
     rename(
-        Residuals=.data$.resid, 
+        Residuals=.data$.resid,
         PredictedCount=.data$.fitted,
     ) %>%
     select(.data$SiteID, .data$N, .data$TotalExposure, .data$TotalCount, .data$Rate, .data$Residuals, .data$PredictedCount) %>%
--- tests/testthat/test_Analyze_Poisson.R
+++ tests/testthat/test_Analyze_Poisson.R
@@ -1,13 +1,26 @@
 ae_input <- AE_Map_Adam(
-    safetyData::adam_adsl, 
+    safetyData::adam_adsl,
     safetyData::adam_adae
-) 
+)
+
+createNA <- function(x) {
+
+  df <-  AE_Map_Adam(safetyData::adam_adsl, safetyData::adam_adae) %>%
+        Transform_EventCount(cCountCol = "Count", cExposureCol = "Exposure")
+
+  df[[x]][1] <- NA
+
+  Analyze_Poisson(df)
+}
+
+
 
 test_that("output created as expected and has correct structure",{
     ae_prep <- Transform_EventCount( ae_input, cCountCol = 'Count', cExposureCol = "Exposure" )
     ae_anly <- Analyze_Poisson(ae_prep)
     expect_true(is.data.frame(ae_anly))
     expect_equal(sort(unique(ae_input$SiteID)), sort(ae_anly$SiteID))
+    expect_equal(names(ae_anly), c("SiteID", "N", "TotalExposure", "TotalCount", "Rate", "Residuals", "PredictedCount"))
 })
 
 test_that("incorrect inputs throw errors",{
@@ -17,6 +30,20 @@ test_that("incorrect inputs throw errors",{
 
 
 test_that("error given if required column not found",{
-    expect_error(Analyze_Poisson(ae_input %>% rename(total = TotalCount)))
+    expect_error(Analyze_Poisson(ae_input %>% select(-SiteID)))
+    expect_error(Analyze_Poisson(ae_input %>% select(-N)))
+    expect_error(Analyze_Poisson(ae_input %>% select(-TotalCount)))
+    expect_error(Analyze_Poisson(ae_input %>% select(-TotalExposure)))
     expect_error(Analyze_Poisson(ae_input %>% select(-Rate)))
 })
+
+test_that("NA values are caught", {
+    expect_error(createNA("SiteID"))
+
+    # currently accept NA values
+    # expect_error(createNA("N"))
+    # expect_error(createNA("TotalCount"))
+    # expect_error(createNA("TotalExposure"))
+    # expect_error(createNA("Rate"))
+})
+
