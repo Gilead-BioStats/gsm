46f836ff346fd5051b77c9576192681235d6c810
Jeremy Wildfire <jwildfire@gmail.com>
2022-03-09 09:49:12 -0500

    refactor visualize_count

--- R/Visualize_Count.R
+++ R/Visualize_Count.R
@@ -6,41 +6,47 @@
 #' @return site level plot object
 #'
 #' @examples
-#' dfInput <- IE_Map_Raw(clindata::raw_ie_all,
-#'                       clindata::rawplus_rdsl,
-#'                       strCategoryCol = "IECAT_STD",
-#'                       strResultCol = "IEORRES")
+#' ie_input <- IE_Map_Raw(
+#'    clindata::raw_ie_all , 
+#'    clindata::rawplus_rdsl,
+#'    strCategoryCol = 'IECAT_STD', 
+#'    strResultCol = 'IEORRES'
+#' )
+#' ie_assess <- IE_Assess(ie_input, bDataList=TRUE)
+#' Visualize_Count(ie_assess$dfAnalyzed)
+#' 
+#' consent_input <- Consent_Map_Raw(
+#'   dfConsent = clindata::raw_consent,
+#'   dfRDSL = clindata::rawplus_rdsl,
+#'   strConsentReason = NULL
+#' )
+#' 
+#' consent_assess<-Consent_Assess(dfInput, bDataList=TRUE)
+#' Visualize_Count(consent_assess$dfAnalyzed)
 #'
 #' @import ggplot2
 #' @importFrom stats reorder
 #'
 #' @export
-Visualize_Count <- function(dfInput, strTitle = NULL) {
 
-dfPlot <- dfInput %>%
-  group_by(.data$SiteID) %>%
-  summarize(SiteID = unique(.data$SiteID),
-            N = sum(.data$Total),
-            Valid = sum(.data$Valid),
-            Invalid = sum(.data$Invalid),
-            Missing = sum(.data$Missing),
-            Count = sum(.data$Count))
+Visualize_Count <- function(dfAnalyzed, strTotalCol="N", strCountCol="TotalCount", strFlagCol="Flag", strTitle="") {
 
-p <- ggplot(data = dfPlot,
-            aes(x = reorder(.data$SiteID, -.data$N))
-            ) +
-  geom_bar(aes(y = .data$N), stat = "identity", color = "black", fill = "white") +
-  geom_bar(aes(y = .data$Invalid), stat = "identity", fill = "red") +
-  ggtitle(strTitle) +
-  labs(
-    x = "Site ID",
-    y = "Inclusion/Exclusion Issues"
-  ) +
-  theme(
-    panel.grid.major.x = element_blank(),
-    axis.text.x = element_text(angle=90, vjust = 0.5),
-    legend.position="none"
-  )
+p <- ggplot(
+        data = dfAnalyzed,
+        aes(x = reorder(.data$SiteID, -.data$N))
+    ) +
+    geom_bar(aes(y = .data[[strTotalCol]]), stat = "identity", color = "black", fill = "white") +
+    geom_bar(aes(y = .data[[strCountCol]]), stat = "identity", fill = "red") +
+    ggtitle(strTitle) +
+    labs(
+        x = "Site ID",
+        y = "Event Count"
+    ) +
+    theme(
+        panel.grid.major.x = element_blank(),
+        axis.text.x = element_text(angle=90, vjust = 0.5),
+        legend.position="none"
+    )
 
-return(p)
+    return(p)
 }
