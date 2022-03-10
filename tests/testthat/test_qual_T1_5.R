4d231d9be2c87db6dbc249425ba7b95b5ba37d1e
Nathan Kosiba <nathan.kosiba@atorusresearch.com>
2022-03-10 20:56:34 +0000

    - remove lubridate from imports - update Visualize_Scatter() examples to not fail with updated AE_Assess() - update qualification to not fail with updated AE_Assess()

--- tests/testthat/test_qual_T1_5.R
+++ tests/testthat/test_qual_T1_5.R
@@ -1,52 +1,20 @@
-test_that("Test that Assessment can return all data in the standard data pipeline (`dfInput`, `dfTransformed`, `dfAnalyzed`, `dfFlagged`, and `dfSummary`) for a poisson test", {
+test_that("Test that (NA, NaN) in input exposure data throws a warning and drops the participant(s) from the analysis.", {
   dfInput <- gsm::AE_Map_Raw(
     dfAE = clindata::raw_ae,
     dfRDSL = clindata::rawplus_rdsl
   )
 
-  # gsm data
-  test_5 <- suppressWarnings(AE_Assess(
-    dfInput = dfInput,
-    bDataList = TRUE
-  ))
+  # data
+  # several NA values
+  dfInputWithNA1 <- dfInput %>%
+    mutate(Exposure = ifelse(substr(SubjectID,4,4) == 1, Exposure, NA_integer_),
+           Rate = ifelse(is.na(Exposure), Exposure, NA_integer_))
 
-  # double programming
-  t5_input <- dfInput
+  # one NA value
+  dfInputWithNA2 <- dfInput %>%
+    mutate(Exposure = ifelse(SubjectID == "1396", NA_integer_, Exposure),
+           Rate = ifelse(is.na(Exposure), Exposure, NA_integer_))
 
-  t5_transformed <- dfInput %>%
-    qualification_transform_counts()
-
-  t5_analyzed <- t5_transformed %>%
-    qualification_analyze_poisson()
-
-  class(t5_analyzed) <- c("tbl_df", "tbl", "data.frame")
-
-  t5_flagged <- t5_analyzed %>%
-    mutate(
-      ThresholdLow = -5,
-      ThresholdHigh = 5,
-      ThresholdCol = "Residuals",
-      Flag = case_when(
-        Residuals < -5 ~ -1,
-        Residuals > 5 ~ 1,
-        is.na(Residuals) ~ NA_real_,
-        is.nan(Residuals) ~ NA_real_,
-        TRUE ~ 0),
-    )
-
-  t5_summary <- t5_flagged %>%
-    mutate(
-      Assessment = "Safety",
-      Label = "",
-      Score = Residuals
-    ) %>%
-    select(Assessment, Label, SiteID, N, Score, Flag)
-
-  t5 <- list("dfInput" = t5_input,
-             "dfTransformed" = t5_transformed,
-             "dfAnalyzed" = t5_analyzed,
-             "dfFlagged" = t5_flagged,
-             "dfSummary" = t5_summary)
-
-  expect_equal(test_5, t5)
+  expect_warning(AE_Assess(dfInputWithNA1))
+  expect_warning(AE_Assess(dfInputWithNA2))
 })
