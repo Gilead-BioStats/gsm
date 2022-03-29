64f667c2b4e4e24b930cfc995c91476369315d63
Doug Sanders <95380773+dsanders2gilead@users.noreply.github.com>
2022-03-29 15:39:05 -0700

    Update tests/testthat/test_util_filter_df.R
    Co-authored-by: Matt Roumaya <40671730+mattroumaya@users.noreply.github.com>

--- tests/testthat/test_util_filter_df.R
+++ tests/testthat/test_util_filter_df.R
@@ -17,4 +17,11 @@ test_that("entire dataframe returned if strCol or strValue not found or NULL", {
   expect_equal(suppressWarnings(util_filter_df(clindata::rawplus_covlab, strCol = NULL, strValue = 1)),clindata::rawplus_covlab )
 })
 
-
+test_that("filtered results are returned correctly", {
+  
+  data <- util_filter_df(covlab, strCol = "LBTEST", strValue = "Monocytes") %>% 
+    pull(LBTEST)
+  
+  expect_true(all(data == "Monocytes"))
+  
+})
