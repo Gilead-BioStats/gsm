5a437f98f560e937153c7b25854227a12bcedfd0
Jeremy Wildfire <jwildfire@gmail.com>
2022-04-07 11:15:32 -0400

    add table summarizing assessment checks

--- tests/testthat/test_Study_Report.R
+++ tests/testthat/test_Study_Report.R
@@ -1,5 +1,30 @@
-test_that("Study Table Report runs as expected",{
-    results<- Study_Assess(bQuiet=TRUE) %>% map(~.x$result)
-    Study_Report(assessments=results, meta=list(Project="My Study"))
+test_that("Study Report runs as expected",{
+    lAssessments<- Study_Assess(bQuiet=TRUE)
+    Study_Report(assessments=lAssessments, meta=list(Project="My Study"))
 })
-devtools::install()
+
+test_that("Study Table Report with AE issue",{
+    lData <- list(
+        dfSUBJ= clindata::rawplus_subj,
+        dfAE=clindata::rawplus_ae,
+        dfPD=clindata::rawplus_pd,
+        dfCONSENT=clindata::rawplus_consent,
+        dfIE=clindata::rawplus_ie
+    )
+    lData$dfAE[1:100,'SubjectID'] <- NA
+    lAssessments <- Study_Assess(lData=lData, bQuiet=FALSE)
+    Study_Report(assessments=lAssessments, meta=list(Project="AE Issues"))
+})
+
+test_that("Study Table Report with a subset of domains issue",{
+    lData <- list(
+        dfSUBJ= clindata::rawplus_subj,
+        dfCONSENT=clindata::rawplus_consent,
+        dfIE=clindata::rawplus_ie
+    )
+    lAssessments <- Study_Assess(lData=lData, bQuiet=FALSE)
+    Study_Report(assessments=lAssessments, meta=list(Project="Consent + IE only"))
+})
+
+
+devtools::load_all()
