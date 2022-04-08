c41c54f520b5d7a52377b9255209b3e81397e046
Matt Roumaya <matt.rouyama@gilead.com>
2022-04-08 23:08:01 +0000

    add start/stop to quietDown

--- R/util-quietDown.R
+++ R/util-quietDown.R
@@ -1,8 +1,17 @@
 #' Utility function to suppress all messages in unit tests
 #'
-quietDown <- function() {
+#' @param action 'start' (the default) or 'stop'. Used in `testthat/setup.R` to temporarily divert messages to a log file.
+#'
+quietDown <- function(action = "start") {
+
+  if (action == "start") {
   # Open a file to send messages to
   echoChamber <- file("messages.Rout", open = "wt")
   # Divert messages to that file
   sink(echoChamber, type = "message")
+  }
+
+  if (action == "stop") {
+    sink(type = "message")
+  }
 }
