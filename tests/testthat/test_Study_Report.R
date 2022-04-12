9f6e3671c0d5eec2f6bf52ccd619f57ff0948112
Matt Roumaya <matt.rouyama@gilead.com>
2022-04-12 20:18:42 +0000

    update unit tests

--- tests/testthat/test_Study_Report.R
+++ tests/testthat/test_Study_Report.R
@@ -1,21 +1,28 @@
+source(testthat::test_path("testdata/data.R"))
+
+lData <- list(
+    dfSUBJ= dfSUBJ,
+    dfAE=dfAE,
+    dfPD=dfPD,
+    dfCONSENT=dfCONSENT,
+    dfIE=dfIE
+)
+
 test_that("Study Report runs as expected",{
-    lAssessments<- Study_Assess(bQuiet=TRUE)
-    expect_true(TRUE)
-    #Study_Report(assessments=lAssessments, meta=list(Project="My Study"))
+    lAssessments<- Study_Assess(lData = lData, bQuiet=TRUE)
+    expect_message(
+        withr::with_tempdir(Study_Report(assessments=lAssessments, meta=list(Project="My Study"))),
+        "Output created: gsm_report.html"
+    )
 })
 
 test_that("Study Table Report with AE issue",{
-    lData <- list(
-        dfSUBJ= clindata::rawplus_subj,
-        dfAE=clindata::rawplus_ae,
-        dfPD=clindata::rawplus_pd,
-        dfCONSENT=clindata::rawplus_consent,
-        dfIE=clindata::rawplus_ie
-    )
-    lData$dfAE[1:100,'SubjectID'] <- NA
+    lData$dfAE[1:2,'SubjectID'] <- NA
     lAssessments <- Study_Assess(lData=lData, bQuiet=TRUE)
-    expect_true(TRUE)
-    #Study_Report(assessments=lAssessments, meta=list(Project="AE Issues"))
+    expect_message(
+        withr::with_tempdir(Study_Report(assessments=lAssessments, meta=list(Project="My Study"))),
+        "Output created: gsm_report.html"
+    )
 })
 
 test_that("Study Table Report with a subset of domains issue",{
@@ -26,5 +33,12 @@ test_that("Study Table Report with a subset of domains issue",{
     # )
     # lAssessments <- Study_Assess(lData=lData, bQuiet=FALSE)
     # Study_Report(assessments=lAssessments, meta=list(Project="Consent + IE only"))
+    # expect_message(
+    #     withr::with_tempdir(Study_Report(assessments=lAssessments, meta=list(Project="My Study"))),
+    #     "Output created: gsm_report.html"
+    # )
+
+    # Above needs to be addressed in Study_Assess first, then can be implemented as a unit test here.
+
     expect_true(TRUE)
 })
--- tests/testthat/test_util-runAssessment.R
+++ tests/testthat/test_util-runAssessment.R
@@ -1,20 +1,32 @@
+source(testthat::test_path("testdata/data.R"))
 sae_meta <- yaml::read_yaml(system.file("assessments/sae.yaml", package = 'gsm'))
 rawDataMap <- yaml::read_yaml(system.file("mapping/rawplus.yaml", package = 'clindata'))
 
+dfAE <- dfAE %>%
+    expand(dfAE, ae_serious = dfAE$AE_SERIOUS)
+
+dfAE <- dfAE %>%
+    expand(dfAE, ae_te_flag = dfAE$AE_TE_FLAG) %>%
+    select(-c(AE_SERIOUS, AE_TE_FLAG)) %>%
+    rename(AE_SERIOUS = ae_serious,
+           AE_TE_FLAG = ae_te_flag)
+
 #Valid Assessment Input
-aeData<-list(dfSUBJ= clindata::rawplus_subj, dfAE=clindata::rawplus_ae)
+aeData<-list(dfSUBJ= dfSUBJ, dfAE=dfAE)
 sae <- RunAssessment(sae_meta, lData=aeData, lMapping= rawDataMap, bQuiet=TRUE)
 
 # Invalid Assessment Input
-aeData_inv<- list( dfSUBJ= clindata::rawplus_subj, dfAE=clindata::rawplus_ae)
-aeData_inv$dfAE[1:100,'SubjectID'] <- NA
+aeData_inv<- list(dfSUBJ= dfSUBJ, dfAE=dfAE)
+aeData_inv$dfAE$SubjectID[1:15] <- NA
 sae_inv <- RunAssessment(sae_meta, lData=aeData_inv, lMapping= rawDataMap, bQuiet=TRUE)
 
 
 test_that("Assessment data filtered as expected",{
-    te_ae<- clindata::rawplus_ae %>% filter(.data$AE_TE_FLAG==TRUE)
-    serious_te_ae <- te_ae %>% filter(.data$AE_SERIOUS=="Yes")
-    expect_equal( sae$lRaw$dfAE %>% nrow, serious_te_ae %>% nrow)
+
+    te_ae<- dfAE %>%
+    filter(.data$AE_TE_FLAG==TRUE & .data$AE_SERIOUS == "Yes")
+
+    expect_equal( sae$lRaw$dfAE %>% nrow, te_ae %>% nrow)
 })
 
 test_that("Assessment correctly labeled as valid",{
@@ -24,3 +36,9 @@ test_that("Assessment correctly labeled as valid",{
     expect_false(sae_inv$rawValid)
     expect_false(sae_inv$valid)
 })
+
+
+
+
+
+
