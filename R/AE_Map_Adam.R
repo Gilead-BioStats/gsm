bd74bed42bbc8ed088b1ed2fe92b67667c6ed2f8
Spencer Childress <Spencer.Childress@gilead.com>
2022-05-02 23:49:38 -0400

    fix #406; update function headers; add .lintr; format files with styler; clarify namespace

--- R/AE_Map_Adam.R
+++ R/AE_Map_Adam.R
@@ -1,54 +1,73 @@
 #' AE Assessment - ADaM Mapping
 #'
-#' Convert from ADaM format to input format for Safety Assessment.
+#' Convert analysis adverse event (AE) data, by default ADaM data, to formatted input data to AE
+#' Assessment.
 #'
 #' @details
 #'
-#' This function maps from ADaM ADSL and ADAE data to the required input for \code{\link{AE_Assess}}.
+#' Combines AE data with subject-level treatment exposure data to create formatted input data to
+#' \code{\link{AE_Assess}}.
 #'
 #' @section Data Specification:
 #'
-#' This function creates an input dataset for the Adverse Event Assessment (\code{\link{AE_Assess}}) by adding Adverse Event Counts (from `dfADAE`) to basic subject-level data (from `dfADSL`).
+#' This function creates an input dataset for the AE Assessment (\code{\link{AE_Assess}}) by binding
+#' subject-level adverse event counts (from `dfADAE`) to subject-level data (from `dfADSL`).
 #'
-#' The following columns are required:
-#' - `dfADSL`
-#'   - `USUBJID` - Unique subject ID
-#'   - `SITEID` - Site ID
-#'   - `TRTEDT` - Treatment End date
-#'   - `TRTSDT` - Treatment Start date
-#' - `dfADAE`
-#'    - `USUBJID` - Unique subject ID
+#' | Domain   | Key           | Value   | Description               | Required? |
+#' | -------- | ------------- | ------- | ------------------------- | --------- |
+#' | `dfADAE` | `strIDCol`    | USUBJID | Unique Subject Identifier | Yes       |
+#' | `dfADSL` | `strIDCol`    | USUBJID | Unique Subject Identifier | Yes       |
+#' | `dfADSL` | `strSiteCol`  | SITEID  | Site Identifier           | Yes       |
+#' | `dfADSL` | `strStartCol` | TRTSDT  | Treatment Start Date      | Yes       |
+#' | `dfADSL` | `strEndCol`   | TRTEDT  | Treatment End Date        | Yes       |
 #'
-#' Note that the function can generate data summaries for specific types of AEs by passing filtered ADAE data to dfADAE.
+#' Note that the function can generate data summaries for specific types of AEs by passing filtered
+#' adverse event data to `dfADAE`.
 #'
-#' @param dfs Named list of data.frames. By default, includes `dfADSL` and `dfADAE`
-#' @param lMapping List containing expected columns in each data set. By default, mapping for dfAE is: `strIDCol` = "SUBJID". By default, mapping for dfRDSL is: `strIDCol` = "SubjectID", `strSiteCol` = "SiteID", and `strExposureCol` = "TimeOnTreatment". TODO: add more descriptive info or reference to mapping.
-#' @param bReturnChecks Should error checking list be returned? Default is FALSE.
-#' @param bQuiet Default is TRUE, which means warning messages are suppressed. Set to FALSE to see warning messages.
+#' @param dfs `list` Input data frames:
+#'  - `dfADAE`: `data.frame` One record per AE
+#'  - `dfADSL`: `data.frame` One record per subject
+#' @param lMapping `list` Column metadata with structure `domain$key`, where `key` contains the name of the column.
+#' @param bReturnChecks `logical` Return input checks from `is_mapping_valid`? Default: `FALSE`
+#' @param bQuiet `logical` Suppress warning messages? Default: `TRUE`
 #'
-#' @return Data frame with one record per person data frame with columns: SubjectID, SiteID, Count (Number of Adverse Events), Exposure (Time on Treatment in Days), Rate (AEs/Day)
+#' @return `data.frame` Data frame with one record per subject and these columns:
+#'
+#' | Name        | Description                          |
+#' | ----------- | ------------------------------------ |
+#' | `SubjectID` | Unique Subject Identifier            |
+#' | `SiteID`    | Site Identifier                      |
+#' | `Count`     | Number of Adverse Events             |
+#' | `Exposure`  | Number of Exposure Days              |
+#' | `Rate`      | Exposure Rate (`Count` / `Exposure`) |
+#'
+#' If `bReturnChecks` is `TRUE` `AE_Map_Adam` returns a named `list` with:
+#' - `df`: the data frame described above
+#' - `lChecks`: a named `list` of check results
 #'
 #' @examples
 #' dfInput <- AE_Map_Adam() # Run with defaults
-#' dfInput <- AE_Map_Adam(bReturnChecks=TRUE, bQuiet=FALSE) # Run with error checking and message log
+#' dfInput <- AE_Map_Adam(bReturnChecks = TRUE, bQuiet = FALSE) # Run with error checking and message log
 #'
 #' @import dplyr
 #'
 #' @export
 
 AE_Map_Adam <- function(
-    dfs=list(
-      dfADSL = safetyData::adam_adsl,
-      dfADAE = safetyData::adam_adae),
-    lMapping = NULL,
-    bReturnChecks = FALSE,
-    bQuiet = TRUE
-){
-
-  if(is.null(lMapping)) lMapping <- list(
-     dfADSL = list(strIDCol="USUBJID", strSiteCol = "SITEID", strStartCol = "TRTSDT", strEndCol = "TRTEDT"),
-     dfADAE = list(strIDCol="USUBJID")
- )
+  dfs = list(
+    dfADSL = safetyData::adam_adsl,
+    dfADAE = safetyData::adam_adae
+  ),
+  lMapping = NULL,
+  bReturnChecks = FALSE,
+  bQuiet = TRUE
+) {
+  if (is.null(lMapping)) {
+    lMapping <- list(
+      dfADSL = list(strIDCol = "USUBJID", strSiteCol = "SITEID", strStartCol = "TRTSDT", strEndCol = "TRTEDT"),
+      dfADAE = list(strIDCol = "USUBJID")
+    )
+  }
 
   checks <- CheckInputs(
     context = "AE_Map_Adam",
@@ -57,28 +76,34 @@ AE_Map_Adam <- function(
     mapping = lMapping
   )
 
-  if(checks$status) {
-    if(!bQuiet) cli::cli_h2("Initializing {.fn AE_Map_Adam}")
+  if (checks$status) {
+    if (!bQuiet) cli::cli_h2("Initializing {.fn AE_Map_Adam}")
 
-    dfInput <-  dfs$dfADSL %>%
-      rename(SubjectID = .data$USUBJID) %>%
-      rename(SiteID = .data$SITEID) %>%
-      mutate(Exposure = as.numeric(.data$TRTEDT - .data$TRTSDT)+1) %>%
+    dfInput <- dfs$dfADSL %>%
+      rename(
+        SubjectID = .data$USUBJID,
+        SiteID = .data$SITEID
+      ) %>%
+      mutate(
+        Exposure = as.numeric(.data$TRTEDT - .data$TRTSDT) + 1
+      ) %>%
       rowwise() %>%
-      mutate(Count =sum(dfs$dfADAE$USUBJID==.data$SubjectID)) %>%
-      mutate(Rate = .data$Count/.data$Exposure) %>%
-      select(.data$SubjectID,.data$SiteID, .data$Count, .data$Exposure, .data$Rate) %>%
+      mutate(
+        Count = sum(dfs$dfADAE$USUBJID == .data$SubjectID),
+        Rate = .data$Count / .data$Exposure
+      ) %>%
+      select(.data$SubjectID, .data$SiteID, .data$Count, .data$Exposure, .data$Rate) %>%
       ungroup()
 
-    if(!bQuiet) cli::cli_alert_success("{.fn AE_Map_Adam} returned output with {nrow(dfInput)} rows.")
+    if (!bQuiet) cli::cli_alert_success("{.fn AE_Map_Adam} returned output with {nrow(dfInput)} rows.")
   } else {
-    if(!bQuiet) cli::cli_alert_warning("{.fn AE_Map_Adam} not run because of failed check.")
+    if (!bQuiet) cli::cli_alert_warning("{.fn AE_Map_Adam} not run because of failed check.")
     dfInput <- NULL
   }
 
-  if(bReturnChecks){
-    return(list(df=dfInput, lChecks=checks))
-  }else{
+  if (bReturnChecks) {
+    return(list(df = dfInput, lChecks = checks))
+  } else {
     return(dfInput)
   }
 }
