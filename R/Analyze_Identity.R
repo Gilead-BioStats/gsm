a7220a8628f8749c2b96f6b3bd78bb741d501ae1
Matt Roumaya <matt.rouyama@gilead.com>
2022-06-08 17:10:12 +0000

    add Analyze_Identity + placeholder unit test

---
+++ R/Analyze_Identity.R
@@ -0,0 +1,17 @@
+#' Analyze Identity
+#'
+#' Used in the data pipeline between `Transform` and `Flag` to rename KRI and Score columns.
+#'
+#' @param dfTransformed `data.frame` created by `Transform_EventCount()`
+#' @param strValueCol `character` Name of column that will be copied as `Score`
+#' @param strLabelCol `character` Name of column that will be copied as `ScoreLabel`
+#'
+#' @return `data.frame` that adds two columns to `dfTransformed`: `Score` and `ScoreLabel`
+#'
+#' @export
+
+Analyze_Identity <- function(dfTransformed, strValueCol = 'KRI', strLabelCol="KRIColumn"){
+  dfTransformed %>%
+    mutate(Score = strValueCol,
+           ScoreLabel = strLabelCol)
+}
