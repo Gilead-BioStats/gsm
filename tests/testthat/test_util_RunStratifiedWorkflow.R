567afa5bd8d79b339abafbf0edaa69f8c4ab4cbc
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-02 13:41:28 -0400

    add test for RunStratifiedWorkflow

--- tests/testthat/test_util_RunStratifiedWorkflow.R
+++ tests/testthat/test_util_RunStratifiedWorkflow.R
@@ -1,16 +1,36 @@
 source(testthat::test_path("testdata/data.R"))
 
-workflow <- MakeAssessmentList()$pdCategory
+workflows <- MakeAssessmentList()
+workflow <- workflows$pd
+stratifiedWorkflow <- workflows$pdCategory
+
 data <- list(
     dfSUBJ = dfSUBJ,
     dfPD = dfPD
 )
-mapping <- yaml::read_yaml(system.file('mappings', 'mapping_rawplus.yaml', package = 'gsm'))
+
+mapping <- yaml::read_yaml(
+    system.file('mappings', 'mapping_rawplus.yaml', package = 'gsm')
+)
+
+output <- RunAssessment(
+    workflow,
+    data,
+    mapping,
+    bQuiet = TRUE
+)
+
+stratifiedOutput <- RunStratifiedWorkflow(
+    stratifiedWorkflow,
+    data,
+    mapping
+)
+
+test_that("Structure of stratified output matches structure of standard output", {
+    expect_equal(class(output), class(stratifiedOutput))
+    expect_equal(c(names(output), 'group') %>% sort, names(stratifiedOutput) %>% sort)
+})
 
 test_that("Stratified output is returned", {
-    output <- runStratifiedWorkflow(
-        workflow,
-        data,
-        mapping
-    )
+    
 })
