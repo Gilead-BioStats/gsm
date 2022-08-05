1e34269e768dc0af05089df1d4b7d69e3655b43e
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-05 11:43:42 -0400

    not much - thanks @mattroumaya!

--- R/LB_Assess.R
+++ R/LB_Assess.R
@@ -33,10 +33,9 @@
 #' @examples
 #' dfInput <- LB_Map_Raw()
 #' lb_assessment_chisq <- LB_Assess(dfInput)
-#' \dontrun{
 #' lb_assessment_fisher <- LB_Assess(dfInput, strMethod = "fisher")
 #' lb_assessment_identity <- LB_Assess(dfInput, strMethod = "identity")
-#'}
+#'
 #' @importFrom cli cli_alert_success cli_alert_warning cli_h2 cli_text
 #' @importFrom purrr map map_dbl
 #'
