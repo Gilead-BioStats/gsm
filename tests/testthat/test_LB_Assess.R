6efeab0619194b28c2db9fbef817cff6d5bc0d82
Spencer Childress <spencer.c.childress@gmail.com>
2022-08-08 15:19:44 -0400

    remove `dfInput` test for `LB_Assess`

--- tests/testthat/test_LB_Assess.R
+++ tests/testthat/test_LB_Assess.R
@@ -24,7 +24,7 @@ test_that("output is created as expected", {
 test_that("metadata is returned as expected", {
   assessment <- assess_function(dfInput)
   expect_equal("assess_function()", assessment$strFunctionName)
-  expect_equal("dfInput", assessment$lParams$dfInput)
+  #expect_equal("dfInput", assessment$lParams$dfInput)
   expect_equal("Labs", assessment$lTags$Assessment)
   expect_true("ggplot" %in% class(assessment$chart))
 })
