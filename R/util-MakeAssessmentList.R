2510424a60c5a8a3745af9b04b7359068e76e5e3
Jeremy Wildfire <jwildfire@gmail.com>
2022-08-18 16:05:09 -0400

    updated metadata docs. created experimental workflow folder, and added parameters to makeAssesmentList

--- R/util-MakeAssessmentList.R
+++ R/util-MakeAssessmentList.R
@@ -4,8 +4,10 @@
 #'
 #' `MakeAssessmentList()` is a utility function that creates a workflow mapping for assessments used in `Study_Assess()`.
 #'
+#' @param names `array of character` List of workflows to include. NULL (the default) includes all workflows in the specified locations.
 #' @param path `character` The location of assessment YAML files. If package is specified, function will look in `/inst` folder.
 #' @param package `character` The name of the package with assessments.
+#' @param recursive `boolean` Find files in nested folders? Default FALSE. 
 #'
 #' @examples
 #' MakeAssessmentList(path = "workflow", package = "gsm")
@@ -18,12 +20,12 @@
 #'
 #' @export
 
-MakeAssessmentList <- function(path = "workflow", package = "gsm") {
+MakeAssessmentList <- function(names=NULL, path = "workflow", package = "gsm", recursive=FALSE) {
   if (!is.null(package)) {
-    path <- system.file(path, package = "gsm")
+    path <- system.file(path, package = package)
   }
 
-  yaml_files <- list.files(path, pattern = "\\.yaml$", full.names = TRUE)
+  yaml_files <- list.files(path, pattern = "\\.yaml$", full.names = TRUE, recursive=recursive)
 
   # copied from tools package
   file_path_sans_ext <- function(x) {
@@ -42,5 +44,10 @@ MakeAssessmentList <- function(path = "workflow", package = "gsm") {
       return(assessment)
     })
   names(assessments) <- assessments %>% map_chr(~ .x$name)
+
+  if(!is.null(names)){
+    assessments <- assessments %>% purrr::keep(.x$name %in% names)
+  }
+
   return(assessments)
 }
