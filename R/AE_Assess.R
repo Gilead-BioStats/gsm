fe3302ec8c525e26fd01bdbc16f11e6e96be45b5
Jeremy Wildfire <jwildfire@gmail.com>
2022-08-31 09:59:54 -0400

    start AE_Assess refactor

--- R/AE_Assess.R
+++ R/AE_Assess.R
@@ -17,13 +17,9 @@
 #'   - `"poisson"` (default)
 #'   - `"wilcoxon"`
 #'   - `"identity"`
-#' @param strKRILabel `character` KRI description. Default: `"AEs/Week"`
+#' @param lMapping Column metadata with structure `domain$key`, where `key` contains the name
+#'   of the column.
 #' @param strGroup `character` Grouping variable. `"Site"` (the default) uses the column named in `mapping$strSiteCol`. Other valid options using the default mapping are `"Study"` and `"CustomGroup"`.
-#' @param lTags `list` Assessment tags, a named list of tags describing the assessment that defaults
-#'   to `list(Assessment = "AE")`. `lTags` is returned as part of the assessment (`lAssess$lTags`)
-#'   and each tag is added as a column in `lAssess$dfSummary`.
-#' @param bChart `logical` Generate data visualization? Default: `TRUE`
-#' @param bReturnChecks `logical` Return input checks from [gsm::is_mapping_valid()]? Default: `FALSE`
 #' @param bQuiet `logical` Suppress warning messages? Default: `TRUE`
 #'
 #' @return `list` Assessment, a named list with:
@@ -57,59 +53,17 @@
 AE_Assess <- function(dfInput,
   vThreshold = NULL,
   strMethod = "poisson",
-  strKRILabel = "AEs/Week",
+  lMapping =  yaml::read_yaml(system.file("mappings", "AE_Assess.yaml", package = "gsm")),
   strGroup = "Site",
-  lTags = list(Assessment = "AE"),
-  bChart = TRUE,
-  bReturnChecks = FALSE,
   bQuiet = TRUE) {
   stopifnot(
-    "dfInput is not a data.frame" = is.data.frame(dfInput),
-    "dfInput is missing one or more of these columns: SubjectID, Count, Exposure, and Rate" = all(c("SubjectID", "Count", "Exposure", "Rate") %in% names(dfInput)),
     "strMethod is not 'poisson', 'wilcoxon', or 'identity'" = strMethod %in% c("poisson", "wilcoxon", "identity"),
     "strMethod must be length 1" = length(strMethod) == 1,
-    "strKRILabel must be length 1" = length(strKRILabel) == 1,
     "strGroup must be one of: Site, Study, or CustomGroup" = strGroup %in% c("Site", "Study", "CustomGroup"),
-    "bChart must be logical" = is.logical(bChart),
-    "bReturnChecks must be logical" = is.logical(bReturnChecks),
     "bQuiet must be logical" = is.logical(bQuiet)
   )
 
-  if (!is.null(lTags)) {
-    stopifnot(
-      "lTags is not named" = (!is.null(names(lTags))),
-      "lTags has unnamed elements" = all(names(lTags) != ""),
-      "lTags cannot contain elements named: 'GroupID', 'GroupLabel', 'N', 'KRI', 'KRILabel', 'Score', 'ScoreLabel', or 'Flag'" = !names(lTags) %in%
-        c(
-          "GroupID",
-          "GroupLabel",
-          "N",
-          "KRI",
-          "KRILabel",
-          "Score",
-          "ScoreLabel",
-          "Flag"
-        )
-    )
-
-    if (any(unname(purrr::map_dbl(lTags, ~ length(.))) > 1)) {
-      lTags <- purrr::map(lTags, ~ paste(.x, collapse = ", "))
-    }
-  }
-
-  lAssess <- list(
-    strFunctionName = deparse(sys.call()[1]),
-    lTags = lTags,
-    dfInput = dfInput
-  )
-
-  mapping <- yaml::read_yaml(system.file("mappings", "AE_Assess.yaml", package = "gsm"))
   mapping$dfInput$strGroupCol <- mapping$dfInput[[glue::glue("str{strGroup}Col")]]
-
-  stopifnot(
-    "`strGroup` not found in mapping" = glue::glue("str{strGroup}Col") %in% names(mapping$dfInput),
-    "`strGroupCol` not found in dfInput" = mapping$dfInput$strGroupCol %in% names(dfInput)
-  )
 
   checks <- CheckInputs(
     context = "AE_Assess",
@@ -118,8 +72,11 @@ AE_Assess <- function(dfInput,
     bQuiet = bQuiet
   )
 
+  lAssess <- list()
 
-  if (checks$status) {
+  if (!checks$status) {
+    if (!bQuiet) cli::cli_alert_warning("{.fn AE_Assess} did not run because of failed check.")
+  }else{
     if (!bQuiet) cli::cli_h2("Initializing {.fn AE_Assess}")
     if (!bQuiet) cli::cli_text("Input data has {nrow(lAssess$dfInput)} rows.")
 
@@ -203,9 +160,7 @@ AE_Assess <- function(dfInput,
         if (!bQuiet) cli::cli_alert_success("{.fn Visualize_Scatter} created a chart.")
       }
     }
-  } else {
-    if (!bQuiet) cli::cli_alert_warning("{.fn AE_Assess} did not run because of failed check.")
-  }
+  } 
 
   if (bReturnChecks) lAssess$lChecks <- checks
   return(lAssess)
---
+++ R/Transform_Rate.R
@@ -0,0 +1,61 @@
+#' Transform - Rate
+#'
+#' Calculate a site-level Rate 
+#'
+#' @details
+#'
+#' This function transforms data to prepare it for the Analysis step
+#'
+#' @section Data Specification:
+#'
+#' The input data (`dfInput`) for the AE Assessment is typically created using any of these functions:
+#'  \code{\link{AE_Map_Raw}}
+#'  \code{\link{AE_Map_Adam}}
+#'  \code{\link{Disp_Map_Raw}}
+#'  \code{\link{LB_Map_Raw}}
+#'  \code{\link{PD_Map_Raw}}
+#'
+#' (`dfInput`) must include the columns specified by `strNumeratorCol`, `strDenominatorCol` and `strGroupCol`
+#'
+#' @param dfInput A data.frame with one record per person.
+#' @param strNumeratorCol Required. Numerical or logical. Column to be counted.
+#' @param strDenominatorCol Optional. Numerical `Exposure` column.
+#' @param strGroupCol `character` Name of column for grouping variable. Default: `"SiteID"`
+#'
+#' @return `data.frame` with one row per site with columns SiteID, N, TotalCount with additional columns Exposure and Rate if strExposureCol is used.
+#'
+#' @examples
+#' dfInput <- AE_Map_Adam()
+#' dfTransformed <- Transform_EventCount(dfInput, strNumeratorCol = "Count", strDenominatorCol = "Exposure")
+#'
+#' @import dplyr
+#'
+#' @export
+
+Transform_Rate <- function(
+  dfInput,
+  strNumeratorCol,
+  strDenominatorCol = NULL,
+  strGroupCol = "SiteID"
+) {
+  stopifnot(
+    "dfInput is not a data frame" = is.data.frame(dfInput),
+    "strNumeratorColumn is not numeric" = is.numeric(dfInput[[strExposureCol]]),
+    "strDenominatorColumn is not numeric" = is.numeric(dfInput[[strExposureCol]]),
+    "NA's found in numerator"=!anyNA(dfInput[[strNumeratorCol]]),
+    "NA's found in denominator"=!anyNA(dfInput[[strDenominatorCol]]),
+    "Specified columns found in input data" = c(strNumeratorCol, strDenominatorCol, strGroupCol) %in% names(dfInput)
+  )
+
+  dfTransformed <- dfInput %>%
+    group_by(GroupID = .data[[strGroupCol]]) %>%
+    summarise(
+      N = n(),
+      Numerator = sum(.data[[strNumeratorCol]]),
+      Denominator = sum(.data[[strDenominatorCol]])
+    ) %>%
+    mutate(Metric = .data$TotalCount / .data$TotalExposure) %>%
+    select(.data$GroupID, .data$GroupLabel, everything())
+
+  return(dfTransformed)
+}
