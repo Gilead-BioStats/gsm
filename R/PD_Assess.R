f5530a464ade71d85e0a90c14fb3ba08edf55627
Matt Roumaya <matt.rouyama@gilead.com>
2022-09-07 18:55:42 +0000

    start PD

--- R/PD_Assess.R
+++ R/PD_Assess.R
@@ -17,13 +17,7 @@
 #'   - `"poisson"` (default)
 #'   - `"wilcoxon"`
 #'   - `"identity"`
-#' @param strKRILabel `character` KRI description. Default: `"PDs/Week"`
 #' @param strGroup `character` Grouping variable. `"Site"` (the default) uses the column named in `mapping$strSiteCol`. Other valid options using the default mapping are `"Study"` and `"CustomGroup"`.
-#' @param lTags `list` Assessment tags, a named list of tags describing the assessment that defaults
-#'   to `list(Assessment = "PD")`. `lTags` is returned as part of the assessment (`lAssess$lTags`)
-#'   and each tag is added as a column in `lAssess$dfSummary`.
-#' @param bChart `logical` Generate data visualization? Default: `TRUE`
-#' @param bReturnChecks `logical` Return input checks from [gsm::is_mapping_valid()]? Default: `FALSE`
 #' @param bQuiet `logical` Suppress warning messages? Default: `TRUE`
 #'
 #' @return `list` Assessment, a named list with:
@@ -33,11 +27,6 @@
 #'   - `dfAnalyzed`, returned by [gsm::Analyze_Poisson()] or [gsm::Analyze_Wilcoxon()]
 #'   - `dfFlagged`, returned by [gsm::Flag()]
 #'   - `dfSummary`, returned by [gsm::Summarize()]
-#' - assessment metadata
-#'   - `strFunctionName`
-#'   - `lTags`
-#' - output(s)
-#'   - `chart`
 #'
 #' @includeRmd ./man/md/PD_Assess.md
 #' @includeRmd ./man/md/analyze_rate.md
@@ -57,66 +46,40 @@
 PD_Assess <- function(dfInput,
   vThreshold = NULL,
   strMethod = "poisson",
-  strKRILabel = "PDs/Week",
+  lMapping = yaml::read_yaml(system.file("mappings", "PD_Assess.yaml", package = "gsm")),
   strGroup = "Site",
-  lTags = list(Assessment = "PD"),
-  bChart = TRUE,
-  bReturnChecks = FALSE,
   bQuiet = TRUE) {
-  stopifnot(
-    "dfInput is not a data.frame" = is.data.frame(dfInput),
-    "dfInput is missing one or more of these columns: SubjectID, Count, Exposure, and Rate" = all(c("SubjectID", "Count", "Exposure", "Rate") %in% names(dfInput)),
+  stopifnot
     "strMethod is not 'poisson', 'wilcoxon', or 'identity'" = strMethod %in% c("poisson", "wilcoxon", "identity"),
     "strMethod must be length 1" = length(strMethod) == 1,
-    "strKRILabel must be length 1" = length(strKRILabel) == 1,
     "strGroup must be one of: Site, Study, or CustomGroup" = strGroup %in% c("Site", "Study", "CustomGroup"),
-    "bChart must be logical" = is.logical(bChart),
-    "bReturnChecks must be logical" = is.logical(bReturnChecks),
     "bQuiet must be logical" = is.logical(bQuiet)
   )
 
-  if (!is.null(lTags)) {
-    stopifnot(
-      "lTags is not named" = (!is.null(names(lTags))),
-      "lTags has unnamed elements" = all(names(lTags) != ""),
-      "lTags cannot contain elements named: 'GroupID', 'GroupLabel', 'N', 'KRI', 'KRILabel', 'Score', 'ScoreLabel', or 'Flag'" = !names(lTags) %in%
-        c(
-          "GroupID",
-          "GroupLabel",
-          "N",
-          "KRI",
-          "KRILabel",
-          "Score",
-          "ScoreLabel",
-          "Flag"
-        )
-    )
-
-    if (any(unname(purrr::map_dbl(lTags, ~ length(.))) > 1)) {
-      lTags <- purrr::map(lTags, ~ paste(.x, collapse = ", "))
-    }
-  }
-
-  lAssess <- list(
-    strFunctionName = deparse(sys.call()[1]),
-    lTags = lTags,
-    dfInput = dfInput
-  )
-
-  mapping <- yaml::read_yaml(system.file("mappings", "AE_Assess.yaml", package = "gsm"))
   mapping$dfInput$strGroupCol <- mapping$dfInput[[glue::glue("str{strGroup}Col")]]
 
-  stopifnot(
-    "`strGroup` not found in mapping" = glue::glue("str{strGroup}Col") %in% names(mapping$dfInput),
-    "`strGroupCol` not found in dfInput" = mapping$dfInput$strGroupCol %in% names(dfInput)
-  )
-
-  checks <- CheckInputs(
+  lChecks <- CheckInputs(
     context = "PD_Assess",
-    dfs = list(dfInput = lAssess$dfInput),
+    dfs = list(dfInput = dfInput),
     mapping = mapping,
     bQuiet = bQuiet
   )
+
+
+# set vThreshold if NULL --------------------------------------------------
+  if (is.null(vThreshold)) {
+    vThreshold <- switch(
+      strMethod,
+      poisson = c(-5, 5),
+      wilcoxon = c(0.0001, NA),
+      identity = c(0.000895, 0.003059)
+    )
+  }
+
+
+  ### stop here
+
+
 
   if (checks$status) {
     if (!bQuiet) cli::cli_h2("Initializing {.fn PD_Assess}")
