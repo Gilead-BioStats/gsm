bd5b28adea805aacd34fa20ba8bc6bbda934f26a
Matt Roumaya <matt.rouyama@gilead.com>
2022-09-16 21:37:44 +0000

    small refactor

--- R/Make_Snapshot.R
+++ R/Make_Snapshot.R
@@ -1,6 +1,6 @@
 #' Make Snapshot - create and export Gizmo data model.
 #'
-#' @param lMeta `list` a named list of data frames containing metadata, configuration, and workflow parameters for a given study.
+#' @param lMeta `list` a named list of data frames containing metadata, configuration, and workflow parameters for a given study. TODO: add details about expected lMeta input.
 #' @param lData `list` a named list of domain level data frames. Names should match the values specified in `lMapping` and `lAssessments`, which are generally based on the expected inputs from `X_Map_Raw`.
 #' @param lMapping `list` a named list identifying the columns needed in each data domain.
 #' @param lAssessments a named list of metadata defining how each assessment should be run. By default, `MakeAssessmentList()` imports YAML specifications from `inst/workflow`.
@@ -20,23 +20,22 @@
 #'
 #'
 #' @export
-Make_Snapshot <- function(
-lMeta = list(
-  meta_study = NULL, #from CTMS
-  meta_site = NULL,  #from CTMS
+Make_Snapshot <- function(lMeta = list(
+  config_params = gsm::config_param,
   config_schedule = NULL,
   config_workflow = gsm::config_workflow,
-  config_params = gsm::config_param,
-  meta_workflow = gsm::meta_workflow,
-  meta_params = gsm::meta_param
+  meta_params = gsm::meta_param,
+  meta_site = clindata::ctms_site,
+  meta_study = clindata::ctms_study,
+  meta_workflow = gsm::meta_workflow
 ),
 lData = list(
   dfSUBJ = clindata::rawplus_dm %>% filter(!is.na(timeontreatment)),
   dfAE = clindata::rawplus_ae,
-  dfPD = clindata::rawplus_protdev,
   dfCONSENT = clindata::rawplus_consent,
   dfIE = clindata::rawplus_ie,
   dfLB = clindata::rawplus_lb,
+  dfPD = clindata::rawplus_protdev,
   dfSTUDCOMP = clindata::rawplus_studcomp,
   dfSDRGCOMP = clindata::rawplus_sdrgcomp %>% filter(.data$datapagename == "Blinded Study Drug Completion")
 ),
@@ -44,16 +43,16 @@ lMapping = yaml::read_yaml(system.file("mappings", "mapping_rawplus.yaml", packa
 lAssessments = NULL,
 cPath = NULL,
 bQuiet = TRUE
+
 ){
 
-  # Create Snapshot
-  lSnapshot <- list()
 
-  # lSnapshot$study_status<-meta$meta_study
-  lSnapshot$study_status <- tibble(StudyID = unique(lMeta$config_workflow$studyid)) # placeholder
 
-# study_status ------------------------------------------------------------
-  lSnapshot$study_status$enrolled_participants <- Get_Enrolled(
+  # lSnapshot$status_study<-meta$meta_study
+  status_study <- tibble(StudyID = unique(lMeta$config_workflow$studyid)) # placeholder
+
+# status_study ------------------------------------------------------------
+  status_study$enrolled_participants <- Get_Enrolled(
     dfSUBJ = lData$dfSUBJ,
     dfConfig = lMeta$config_param,
     lMapping = lMapping,
@@ -61,7 +60,7 @@ bQuiet = TRUE
     strBy = "study"
     )
 
-  lSnapshot$study_status$enrolled_sites <- Get_Enrolled(
+  status_study$enrolled_sites <- Get_Enrolled(
     dfSUBJ = lData$dfSUBJ,
     dfConfig = lMeta$config_param,
     lMapping = lMapping,
@@ -69,9 +68,9 @@ bQuiet = TRUE
     strBy = "study"
   )
 
-# site_status -------------------------------------------------------------
-  # lSnapshot$site_status <- meta$meta_site
-  lSnapshot$site_status <- Get_Enrolled(
+# status_site -------------------------------------------------------------
+  # lSnapshot$status_site <- meta$meta_site
+  status_site <- Get_Enrolled(
     dfSUBJ = lData$dfSUBJ,
     dfConfig = lMeta$config_param,
     lMapping = lMapping,
@@ -100,36 +99,36 @@ bQuiet = TRUE
     bind_rows()
 
   # lWorkflowStatus <- parseWorkflowStatus(lResults)
-  lSnapshot$status_workflow <- lMeta$config_workflow %>%
+  status_workflow <- lMeta$config_workflow %>%
     left_join(parseStatus, by = "workflowid")
 
   #lSnapshot$status_workflow$Status <- lWorkflowStatus$Status
   #lSnapshot$status_workflow$Notes<- lWorkflowStatus$Notes
 
 # status_param ------------------------------------------------------------
-  lSnapshot$status_param <- lMeta$config_param
+  status_param <- lMeta$config_param
 
 # status_schedule ---------------------------------------------------------
-  lSnapshot$status_schedule <- lMeta$config_schedule
+  status_schedule <- lMeta$config_schedule
 
 # meta_workflow -----------------------------------------------------------
-  lSnapshot$meta_workflow <- gsm::meta_workflow
+  meta_workflow <- gsm::meta_workflow
 
 # meta_param --------------------------------------------------------------
-  lSnapshot$meta_param <- gsm::meta_param
+  meta_param <- gsm::meta_param
 
 # results_summary ---------------------------------------------------------
-  lSnapshot$results_summary <- lResults %>%
+  results_summary <- lResults %>%
     purrr::imap_dfr(~.x$lResults$lData$dfSummary %>%
                mutate(KRIID = .y,
                       StudyID = unique(lMeta$config_workflow$studyid)))
 
 
-  # lSnapshot$results_summary$StudyID <- meta$study_status[1,'StudyID']
+  # lSnapshot$results_summary$StudyID <- meta$status_study[1,'StudyID']
   #Also need to make sure we're capturing WorkflowID here ...
 
 # results_bounds ----------------------------------------------------------
-  lSnapshot$results_bounds <- lResults %>%
+  results_bounds <- lResults %>%
     purrr::map(~.x$lResults$lData$dfBounds) %>%
     purrr::discard(is.null) %>%
     purrr::imap_dfr(~.x %>% mutate(KRIID = .y)) %>%
@@ -137,6 +136,20 @@ bQuiet = TRUE
            WorkflowID = KRIID) # not sure if this is a correct assumption
 
   #Also need to make sure we're capturing WorkflowID here ...
+
+
+# create lSnapshot --------------------------------------------------------
+
+  lSnapshot <- list(
+    status_study = status_study,
+    status_site = status_site,
+    status_workflow = status_workflow,
+    status_param = status_param,
+    status_schedule = status_schedule,
+    results_summary = results_summary,
+    meta_workflow = meta_workflow,
+    meta_param = meta_param
+    )
 
   if (!is.null(cPath)) {
     #write each snapshot item to location
