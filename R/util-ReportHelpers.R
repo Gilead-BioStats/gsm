6f7bfde92ff849ff6bff895d7453538fa09e8883
Matt Roumaya <matt.rouyama@gilead.com>
2022-09-22 04:24:24 +0000

    fontawesome

--- R/util-ReportHelpers.R
+++ R/util-ReportHelpers.R
@@ -14,10 +14,10 @@
 
 rank_chg <- function(status) {
   if (status == 1) {
-    logo_out <- fontawesome::fa("check-circle", fill = "green")
+    logo_out <- fontawesome::fa("circle", fill = "green")
   }
   if (status == 2) {
-    logo_out <- fontawesome::fa("times-circle", fill = "red")
+    logo_out <- fontawesome::fa("circle", fill = "red")
   }
   if (status == 3) {
     logo_out <- fontawesome::fa("circle", fill = "#EED202")
