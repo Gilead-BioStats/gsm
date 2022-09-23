906eef00ff9b3f04280f33ef970a7952b0ca40c2
Colleen McLaughlin <colleen.mclaughlin@atorusresearch.com>
2022-09-23 14:44:08 +0000

    Removed comma in stopifnot

--- R/Analyze_Fisher.R
+++ R/Analyze_Fisher.R
@@ -50,7 +50,7 @@ Analyze_Fisher <- function(
 ) {
   stopifnot(
     "dfTransformed is not a data.frame" = is.data.frame(dfTransformed),
-    "One or more of these columns: GroupID, or the value in strOutcome not found in dfTransformed" = all(c("GroupID", strOutcome) %in% names(dfTransformed)),
+    "One or more of these columns: GroupID or the value in strOutcome not found in dfTransformed" = all(c("GroupID", strOutcome) %in% names(dfTransformed)),
     "NA value(s) found in GroupID" = all(!is.na(dfTransformed[["GroupID"]])),
     "strOutcome must be length 1" = length(strOutcome) == 1,
     "strOutcome is not character" = is.character(strOutcome)
