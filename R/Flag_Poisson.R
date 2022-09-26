e3c5f4c159d36e19aa4d6a85d85953a833f4a542
Zhongkai-Wang <Zhongkai.Wang6@gilead.com>
2022-09-26 18:15:18 -0400

    Clean up

--- R/Flag_Poisson.R
+++ R/Flag_Poisson.R
@@ -3,17 +3,23 @@
 #' Add columns flagging sites that represent possible statistical outliers.
 #'
 #' @details
-#' This function provides a generalized framework for flagging sites as part of the [GSM data pipeline](https://silver-potato-cfe8c2fb.pages.github.io/articles/DataPipeline.html).
+#' This function flags sites based on the Poisson analysis result as part of the [GSM data pipeline](https://silver-potato-cfe8c2fb.pages.github.io/articles/DataPipeline.html).
 #'
 #' @section Data Specification:
-#' \code{Flag} is designed to support the input data (`dfAnalyzed`) from many different
-#' \code{Analyze} functions.  dfTransformed must have a `score` column.
-#'
+#' \code{Flag} is designed to support the input data (`dfAnalyzed`) from \code{Analyze_Poisson} function. 
+#' At a minimum, the input data must have a `SiteID` column and a column of numeric values (identified 
+#' by the `strColumn` parameter) that will be compared to the specified thresholds (`vThreshold`) to 
+#' calculate a new `Flag` column. 
+#' In short, the following columns are considered:
+#' - `GroupID` - Group ID (required)
+#' - `strColumn` - A column to use for Thresholding (required)
+#' - 'strValueColumn' - A column to be used for the sign of the flag (optional)#'
 #' @param dfAnalyzed data.frame where flags should be added.
+#' @param strColumn Name of the column to use for thresholding.
 #' @param vThreshold Vector of 4 numeric values representing lower and upper threshold values. All
 #' values in strColumn are compared to vThreshold using strict comparisons. Values less than the lower threshold or greater than the upper threshold are flagged as -1 and 1 respectively. Values equal to the threshold values are set to 0 (i.e. not flagged). If NA is provided for either threshold value it is ignored, and no values are flagged based on the threshold. NA and NaN values in strColumn are given NA flag values.
 #'
-#' @return `data.frame` with `Flag` column added 
+#' @return `data.frame` with "Flag" column added 
 #'
 #' @examples
 #' dfInput <- AE_Map_Adam()
@@ -39,25 +45,36 @@ Flag_Poisson <- function(
 ) {
   stopifnot(
     "dfAnalyzed is not a data frame" = is.data.frame(dfAnalyzed),
+    "strColumn is not character" = is.character(strColumn),
     "vThreshold is not numeric" = is.numeric(vThreshold),
     "vThreshold must be length of 4" = length(vThreshold) == 4,
     "vThreshold cannot be NULL" = !is.null(vThreshold),
+    "strColumn must be length of 1" = length(strColumn) == 1,
+    "strColumn not found in dfAnalyzed" = strColumn %in% names(dfAnalyzed),
+    "GroupID not found in dfAnalyzed" = "GroupID" %in% names(dfAnalyzed)
   )
 
+  if (all(!is.na(vThreshold))) {
+    stopifnot(
+      "vThreshold must contain cutoff for moderate/high risks in two directions (i.e., vThreshold = c(-7, -5, 5, 7))" = 
+      vThreshold[1] < vThreshold[2] < vThreshold[3] < vThreshold[4]
+    )
+  }
+  
   # Flag values outside the specified threshold.
   dfFlagged <- dfAnalyzed %>%
     mutate(
       Flag = case_when(
-        (.data$score < vThreshold[1]) ~ -2,
-        (.data$score < vThreshold[2]) ~ -1,
-        (.data$score < vThreshold[3]) ~ 0,
-        (.data$score < vThreshold[4]) ~ 1,
-        TRUE ~ 2
+        .data$score < vThreshold[1] ~ -2,
+        (.data$score >= vThreshold[1]) & (.data$score < vThreshold[2]) ~ -1,
+        (.data$score > vThreshold[3]) & (.data$score <= vThreshold[4]) ~ 1,
+        .data$score >[4] ~ 2,
+        TRUE ~ 0
       )
     )
 
   dfFlagged <- dfFlagged %>%
-    arrange(match(.data$Flag, c(-2,2, 1, -1, 0)))
+    arrange(match(.data$Flag, c(2, -2, 1, -1, 0)))
 
   return(dfFlagged)
 }
