5eab0e8827e0e3f9aed1c95ffaf1cc34764ca6cb
Matt Roumaya <matt.rouyama@gilead.com>
2022-10-07 19:40:04 +0000

    remove config + update contributor guidelines

--- data-raw/config_param.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `config_param` dataset goes here
-desc <- utils::packageDescription('gsm')
-config_param <- read.csv("data-raw/config_param.csv")
-config_param$gsm_version <- desc$Version
-usethis::use_data(config_param, overwrite = TRUE)
