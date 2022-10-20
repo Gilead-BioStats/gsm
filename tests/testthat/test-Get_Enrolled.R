3cb2966c343344587c0d90824b015e73fc8eae4e
Colleen McLaughlin <colleen.mclaughlin@atorusresearch.com>
2022-10-20 20:58:58 +0000

    Removing old code (had inconsistent naming)

--- tests/testthat/test-Get_Enrolled.R
+++
@@ -1,5 +0,0 @@
-source(testthat::test_path("testdata/data.R"))
-
-### dfSUBJ & dfConfig is correct class and has all needed columns
-
-### Creates data frame of 2 variables - site & number of enrolled participants
