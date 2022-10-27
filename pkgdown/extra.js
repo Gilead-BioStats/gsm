677ac4bab9e81aabb9898a6d7aabf3a2a820f279
Matt Roumaya <matt.rouyama@gilead.com>
2022-10-27 13:47:29 +0000

    add experimental badges to function reference

---
+++ pkgdown/extra.js
@@ -0,0 +1,25 @@
+const gsmLifecycleImage = document.createElement("img");
+gsmLifecycleImage.setAttribute(
+  "src",
+  "https://raw.githubusercontent.com/r-lib/lifecycle/main/vignettes/figures/lifecycle-experimental.svg"
+);
+
+const addToFooter = function () {
+  const aTagElements = document.querySelectorAll("a");
+  const aTagArray = Array.from(aTagElements);
+  const gsmExperimentalList = [
+    "RunStratifiedWorkflow()",
+    "Study_Report()",
+    "Study_Table()",
+    "Study_AssessmentReport()",
+  ];
+  aTagArray.map((data) => {
+    if (gsmExperimentalList.includes(data.textContent)) {
+      data.prepend(gsmLifecycleImage.cloneNode(true));
+    }
+  });
+};
+
+window.onload = (event) => {
+  addToFooter();
+};
