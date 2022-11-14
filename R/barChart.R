f3d7d414967069f06de5ff7ff1a0bc141ad7895c
Maya Gans <maya.gans@atorusresearch.com>
2022-11-14 16:51:34 -0500

    adding barchart files

---
+++ R/barChart.R
@@ -0,0 +1,111 @@
+#' KRI bar chart
+#'
+#' <Add Description>
+#'
+#' @param data data with column names:
+#'\itemize{
+#'  \item{\code{studyid}}
+#'  \item{\code{workflowid}}
+#'  \item{\code{groupid}}
+#'  \item{\code{numerator}}
+#'  \item{\code{denominator}}
+#'  \item{\code{metric}}
+#'  \item{\code{score}}
+#'  \item{\code{flag}}
+#'}
+#'
+#' @param config configuration data with columns:
+#' \itemize{
+#'  \item{\code{workflow}}
+#'  \item{\code{gsm_version}}
+#'  \item{\code{group}}
+#'  \item{\code{metric}}
+#'  \item{\code{numerator}}
+#'  \item{\code{denominator}}
+#'  \item{\code{outcome}}
+#'  \item{\code{model}}
+#'  \item{\code{score}}
+#'  \item{\code{data_inputs}}
+#'  \item{\code{data_filters}}
+#'}
+#'
+#' @param threshold a one row data frame containing columns:
+#' \itemize{
+#'  \item{\code{workflowid}}
+#'  \item{\code{gsm_version}}
+#'  \item{\code{param}}
+#'  \item{\code{index}}
+#'  \item{\code{default}}
+#'  \item{\code{configurable}}
+#'}
+#'
+#' @param yaxis either \code{'score'} or \code{'metric'}
+#' @param selectedGroupIDs group IDs to highlight, \code{NULL} by default, can be a single site or a vector.
+#' @param width width of widget, full screen by default
+#' @param height height of widget, calculated based on width
+#' @param elementId ID of container HTML element
+#'
+#' @import htmlwidgets
+#'
+#' @export
+barChart <- function(
+      data = NULL,
+      config = list(),
+      threshold = NULL,
+      yaxis = 'score',
+      selectedGroupIDs = NULL,
+      width = NULL,
+      height = NULL,
+      elementId = NULL) {
+
+  data <- dplyr::mutate(data, across(everything(), as.character))
+  config <- dplyr::mutate(config, across(everything(), as.character))
+  threshold <- dplyr::mutate(threshold, across(everything(), as.character))
+
+  # forward options using x
+  x = list(
+     data = data,
+     config = config,
+     threshold = threshold,
+     yaxis = yaxis,
+     selectedGroupIDs = as.character(selectedGroupIDs)
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'barChart',
+    x,
+    width = width,
+    height = height,
+    package = 'rbmvizwidgets',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for barChart
+#'
+#' Output and render functions for using barChart within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a barChart
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name barChart-shiny
+#'
+#' @export
+barChartOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'barChart', width, height, package = 'rbmvizwidgets')
+}
+
+#' @rdname barChart-shiny
+#' @export
+renderBarChart <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, barChartOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/barChart.js
@@ -0,0 +1,40 @@
+HTMLWidgets.widget({
+
+  name: 'barChart',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    return {
+
+      renderValue: function(x) {
+
+      let data = HTMLWidgets.dataframeToD3(x.data)
+
+      let config = HTMLWidgets.dataframeToD3(x.config)[0]
+      config.selectedGroupIDs = number_to_array(x.selectedGroupIDs)
+
+      let threshold = HTMLWidgets.dataframeToD3(x.threshold)
+
+      console.log(config)
+
+      const instance = rbmViz.default.barChart(
+           el,
+           data,
+           config,
+           threshold,
+           x.yaxis
+         )
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
