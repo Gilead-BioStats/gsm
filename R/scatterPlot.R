e80563fdc69f8f85a24610a12957c968bd86f7d2
Matt Roumaya <matt.rouyama@gilead.com>
2022-11-16 21:39:20 +0000

    add files

---
+++ R/scatterPlot.R
@@ -0,0 +1,65 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+scatterPlot <- function(
+    results,
+    workflow,
+    bounds,
+    selectedGroupIDs = NULL,
+    width = NULL,
+    height = NULL,
+    elementId = NULL
+) {
+
+
+
+  # forward options using x
+  x = list(
+    results = results,
+    workflow = workflow,
+    bounds = bounds,
+    selectedGroupIDs = as.character(selectedGroupIDs)
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'scatterPlot',
+    x,
+    width = width,
+    height = height,
+    package = 'gsm',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for scatterPlot
+#'
+#' Output and render functions for using scatterPlot within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a scatterPlot
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name scatterPlot-shiny
+#'
+#' @export
+scatterPlotOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'scatterPlot', width, height, package = 'gsm')
+}
+
+#' @rdname scatterPlot-shiny
+#' @export
+renderScatterPlot <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, scatterPlotOutput, env, quoted = TRUE)
+}
