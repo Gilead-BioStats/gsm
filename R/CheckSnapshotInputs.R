1654b2c506ecbaeabbacf6d300392e4beca25218
Matt Roumaya <matt.rouyama@gilead.com>
2022-11-22 20:04:48 +0000

    add tests and update

--- R/CheckSnapshotInputs.R
+++ R/CheckSnapshotInputs.R
@@ -1,25 +1,36 @@
-#' Title
+#' Check Snapshot Inputs
 #'
-#' @param snapshot
+#' @description
+#' Check that the output created from [gsm::Make_Snapshot()] returns all expected tables and columns within tables as defined by the
+#' data model ([gsm::rbm_data_spec]).
 #'
-#' @return
-#' @export
+#' @param snapshot `list` List returned from running [gsm::Make_Snapshot()].
+#'
+#' @return `list` Named list including:
+#' - `expected_tables` `data.frame` with columns `in_snapshot`, `expected_gismo`, and `status`.
+#' - `expected_columns` `data.frame` with columns `snapshot_table`, `snapshot_column`, `gismo_table`, `gismo_column`, and `status`.
+#' - `status_tables` `logical` TRUE if all checks pass. FALSE if not.
+#' - `status_columns` `logical` TRUE if all checks pass. FALSE if not.
 #'
 #' @examples
+#'
+#' \dontrun{
+#' snapshot <- Make_Snapshot()
+#' check_snapshot <- CheckSnapshotInputs(snapshot)
+#' }
+#'
+#' @importFrom purrr map_int imap_dfr
+#'
+#' @export
 CheckSnapshotInputs <- function(snapshot) {
 
-  # snapshot <- Make_Snapshot()
-  #
-  # snapshot$results_summary <- NULL
-  #
-  # snapshot_kri <- Make_Snapshot(lAssessments = MakeWorkflowList(strNames = c("kri0001", "kri0002")))
-
+  # get rbm_data_spec/data model
   gismo_input <- gsm::rbm_data_spec %>%
     filter(System == "Gismo") %>%
     arrange(match(Table, names(snapshot))) %>%
     split(.$Table)
 
-  browser()
+
 
 # expected tables ---------------------------------------------------------
 
@@ -28,6 +39,21 @@ CheckSnapshotInputs <- function(snapshot) {
   # if no - results_analysis should not be included
   if (exists("results_summary", where = snapshot)) {
     hasQTL <- length(grep("qtl", unique(snapshot$results_summary$workflowid))) > 0
+
+    # check to see if there are *only* QTLs being run
+    # if yes - results_bounds should not be included
+    # if no - results_bounds should be included
+    qtlOnly <- sum(purrr::map_int(c("kri", "cou"), ~length(grep(.x, unique(snapshot$results_summary$workflowid)))))
+
+    if (qtlOnly == 0) {
+      # QTLs don't produce a meaningful results_bounds data.frame
+      # Only have gsm_analysis_date appended at the end of Make_Snapshot()
+      # Duplicate expected result here
+      gismo_input$results_bounds <- tibble(Column = "gsm_analysis_date")
+    }
+
+  } else {
+    hasQTL <- FALSE
   }
 
   if (!hasQTL) {
@@ -46,9 +72,9 @@ CheckSnapshotInputs <- function(snapshot) {
 
 
 # expected columns --------------------------------------------------------
-  expected_columns_snapshot <- imap_dfr(snapshot, ~tibble(snapshot_table = .y, snapshot_column = names(.x))) %>%
+  expected_columns_snapshot <- purrr::imap_dfr(snapshot, ~tibble(snapshot_table = .y, snapshot_column = names(.x))) %>%
     arrange(snapshot_table, snapshot_column)
-  expected_columns_gismo <- imap_dfr(gismo_input, ~tibble(gismo_table = .y, gismo_column = .x$Column)) %>%
+  expected_columns_gismo <- purrr::imap_dfr(gismo_input, ~tibble(gismo_table = .y, gismo_column = .x$Column)) %>%
     arrange(gismo_table, gismo_column)
 
   expected_columns <- full_join(
@@ -57,22 +83,25 @@ CheckSnapshotInputs <- function(snapshot) {
                                    "snapshot_column" = "gismo_column"),
     keep = TRUE
   ) %>%
-    mutate(
-      status = ifelse(is.na(snapshot_table) || is.na(snapshot_column) || is.na(gismo_table) || is.na(gismo_column), FALSE, TRUE)
-    )
+    rowwise() %>%
+    mutate(status = sum(is.na(cur_data())),
+           status = ifelse(status == 0, TRUE, FALSE)) %>%
+    ungroup()
 
   columns_status <- all(expected_columns$status)
 # return ------------------------------------------------------------------
 
+
   all_checks <- list(
     expected_tables = expected_tables,
     expected_columns = expected_columns,
-    tables_status = tables_status,
-    columns_status = columns_status
+    status_tables = tables_status,
+    status_columns = columns_status,
+    bStatus = isTRUE(tables_status) & isTRUE(columns_status)
   )
 
   return(all_checks)
 
 }
 
-CheckSnapshotInputs(snapshot)
+
