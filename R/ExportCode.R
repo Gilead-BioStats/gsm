b5c9a982a0601c3425ff1332db9a6fa0fb0d7410
Matt Roumaya <matt.rouyama@gilead.com>
2022-11-23 20:19:45 +0000

    updates

--- R/ExportCode.R
+++ R/ExportCode.R
@@ -15,43 +15,54 @@ lMapping <- yaml::read_yaml(system.file("mappings", "mapping_rawplus.yaml", pack
 
 lAssessments <- gsm::MakeWorkflowList(strNames = c("kri0001", "kri0002", "kri0003"))
 
+ExportCode(lData, lMapping, lAssessments)
 
+ExportCode <- function(lData, lMapping, lAssessments) {
 
-packages <- glue::glue("library(gsm)
+  packages <- glue::glue("library(gsm)
                        library(tidyverse)")
 
-imap(lAssessments, function(kri, kri_name) {
-  glue::glue("--- Code for {kri_name} ---")
-  map(1:length(kri), function(index) {
-    print(paste0(kri_name, ": ", index))
-    steps <- kri$steps[[index]]
-    inputs <- steps$inputs
-    output <- steps$output
-    params <- steps$params
+  imap(lAssessments, function(kri, kri_name) {
 
-    if (!is.null(params)) {
-      params <- imap(params, ~glue::glue("{.y} = lMapping[['{params$strDomain}']][['{.x}']]"))
-      params$strDomain <- NULL
+    glue::glue("--- Code for {kri_name} ---")
 
-      arg_string <- paste0(kri$steps[[index]]$name,
-                           "(",
-                           paste(inputs, collapse = ", "),
-                           ", ",
-                           paste(params, collapse = ", \n"),
-                           ")")
-    } else {
 
-      arg_string <- paste0(kri$steps[[index]]$name,
-                           "(",
-                           paste(inputs, collapse = ", "),
-                           ")")
+    map(1:length(kri), function(index) {
 
-    }
+      print(paste0(kri_name, ": ", index))
+      steps <- kri$steps[[index]]
+      inputs <- steps$inputs
+      output <- steps$output
+      params <- steps$params
 
-    if (length(inputs) > 1) {
-      inputs <- paste0("dfs = list(", paste(inputs, collapse = ", "), ")")
-    }
-    glue::glue("{output} <- {arg_string})")
+      if (!is.null(params)) {
+        browser()
+        params <- imap(params, ~glue::glue("{.y} = lMapping[['{params$strDomain}']][['{.x}']]"))
+        params$strDomain <- NULL
+
+        arg_string <- paste0(kri$steps[[index]]$name,
+                             "(",
+                             paste("lData[['",inputs, "']]", collapse = ", "),
+                             ", ",
+                             paste(params, collapse = ", \n"),
+                             ")")
+      } else {
+
+        arg_string <- paste0(kri$steps[[index]]$name,
+                             "(",
+                             paste(inputs, collapse = ", "),
+                             ")")
+
+      }
+
+      if (length(inputs) > 1) {
+        inputs <- paste0("dfs = list(", paste(inputs, collapse = ", "), ")")
+      }
+      glue::glue("{output} <- {arg_string})")
+    })
+
   })
 
-})
+}
+
+
