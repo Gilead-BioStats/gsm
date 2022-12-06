12f3c177b0c7b4acf668b3bfdc9bcc7f07c2da1b
Matt Roumaya <matt.rouyama@gilead.com>
2022-12-06 19:05:18 +0000

    widgets for DataChg and DataEntry

--- R/DataEntry_Assess.R
+++ R/DataEntry_Assess.R
@@ -168,15 +168,49 @@ DataEntry_Assess <- function(
 
     if (!hasName(lData, "dfBounds")) lData$dfBounds <- NULL
 
+    # rbm-viz setup -----------------------------------------------------------
+
+    dfConfig <- MakeDfConfig(
+      strMethod = strMethod,
+      strGroup = strGroup,
+      strAbbreviation = "DATAENTRY",
+      strMetric = "Data Entry Lag",
+      strNumerator = "Data Page Entered > 10 Days",
+      strDenominator = "Total Data Pages",
+      vThreshold = vThreshold
+    )
+
     if (strMethod != "Identity") {
       lCharts$scatter <- gsm::Visualize_Scatter(dfFlagged = lData$dfFlagged, dfBounds = lData$dfBounds, strGroupLabel = strGroup)
-      if (!bQuiet) cli::cli_alert_success("{.fn Visualize_Scatter} created {length(lCharts)} chart.")
+
+      lCharts$scatterJS <- scatterPlot(
+        results = lData$dfFlagged,
+        workflow = dfConfig,
+        bounds = lData$dfBounds,
+        elementId = "dataEntryAssessScatter"
+      )
+
+      if (!bQuiet) cli::cli_alert_success("Created {length(lCharts)} scatter plot{?s}.")
     }
 
     lCharts$barMetric <- gsm::Visualize_Score(dfFlagged = lData$dfFlagged, strType = "metric")
     lCharts$barScore <- gsm::Visualize_Score(dfFlagged = lData$dfFlagged, strType = "score", vThreshold = vThreshold)
-    if (!bQuiet) cli::cli_alert_success("{.fn Visualize_Score} created {length(names(lCharts)[names(lCharts) != 'scatter'])} chart{?s}.")
 
+    lCharts$barMetricJS <- barChart(
+      results = lData$dfFlagged,
+      workflow = dfConfig,
+      yaxis = "metric",
+      elementId = "dataEntryAssessMetric"
+    )
+
+    lCharts$barScoreJS <- barChart(
+      results = lData$dfFlagged,
+      workflow = dfConfig,
+      yaxis = "score",
+      elementId = "dataEntryAssessScore"
+    )
+
+    if (!bQuiet) cli::cli_alert_success("Created {length(names(lCharts)[!names(lCharts) %in% c('scatter', 'scatterJS')])} bar chart{?s}.")
 
 
     # return data -------------------------------------------------------------
