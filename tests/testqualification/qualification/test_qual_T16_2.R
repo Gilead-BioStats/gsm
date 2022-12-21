8f6d5e30c88fd72f0238611f17c1ff22d6413438
Colleen McLaughlin <colleen.mclaughlin@atorusresearch.com>
2022-12-21 18:52:43 +0000

    PD_Map_Raw() qual testing

---
+++ tests/testqualification/qualification/test_qual_T16_2.R
@@ -0,0 +1,47 @@
+test_that("Raw+ PD data can be mapped correctly to create an analysis-ready input dataset that has all required columns in the default Raw+ mapping specifications.", {
+
+
+  ########### gsm mapping ###########
+  observed <- gsm::PD_Map_Raw()
+
+
+  ########### double programming ###########
+  # read in default mapping specs
+  lMapping <- yaml::read_yaml(system.file("mappings", "mapping_rawplus.yaml", package = "gsm"))
+
+  # create cols vector to facilitate connecting lMapping with source data variables
+  cols <- c(SubjectID = lMapping$dfSUBJ$strIDCol,
+            SiteID = lMapping$dfSUBJ$strSiteCol,
+            StudyID = lMapping$dfSUBJ$strStudyCol,
+            CountryID = lMapping$dfSUBJ$strCountryCol,
+            CustomGroupID = lMapping$dfSUBJ$strCustomGroupCol,
+            Exposure = lMapping$dfSUBJ$strTimeOnStudyCol,
+            "Count",
+            "Rate")
+
+  # read in raw source PD data
+  pd_raw_orig <- clindata::rawplus_protdev
+
+  # count unique number of PDs within each subject and remove duplicate records
+  pd_raw <- pd_raw_orig %>%
+    group_by_at(lMapping$dfSUBJ$strIDCol) %>%
+    select(lMapping$dfPD$strIDCol) %>%
+    mutate(Count = n()) %>%
+    distinct()
+
+  # read in raw source DM data
+  dm_raw_orig <- clindata::rawplus_dm
+  dm_raw <- dm_raw_orig
+
+  # join DM and PD data - full_join() to keep records from both data frames
+  expected <- full_join(dm_raw, pd_raw) %>%
+    mutate(Count = replace_na(Count, 0),
+           Rate = as.numeric(Count)/!!sym(lMapping$dfSUBJ$strTimeOnStudyCol)) %>%
+    filter(!(!!sym(lMapping$dfSUBJ$strTimeOnStudyCol) == 0)) %>% # remove subjects that were not treated
+    select(all_of(cols))
+
+
+  ########### testing ###########
+  expect_equal(colnames(observed), colnames(expected))
+
+})
