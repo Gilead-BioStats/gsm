dff45b7f07bb5861370a9c3b1b0cf75c0c820a49
Colleen McLaughlin <colleen.mclaughlin@atorusresearch.com>
2023-01-13 17:01:41 +0000

    DataEntry updates

--- tests/testqualification/qualification/test_qual_T13_4.R
+++ tests/testqualification/qualification/test_qual_T13_4.R
@@ -30,7 +30,7 @@ test_that("A subset of raw data entry data can be mapped correctly to create an 
   # note that data_entry_lag is number of days between the visit date and the earliest field entry date
   data_entry_orig <- clindata::edc_data_entry_lag
 
-  # count unique number of PK data forms with data entry lag (i.e., >10 days between the visit date and the earliest field entry date - where data_entry_lag_fl == "Y") within each subject and remove duplicate records
+  # count unique number of data pages with data entry lag (i.e., >10 days between the visit date and the earliest field entry date - where data_entry_lag_fl == "Y") within each subject and remove duplicate records
   data_entry <- data_entry_orig %>%
     filter(!!sym(lMapping$dfDATAENT$strFormCol) == "PK") %>%
     filter(!!sym(lMapping$dfDATAENT$strDataEntryLagCol) %in% unique(lMapping$dfDATAENT$strDataEntryLagVal)) %>%
@@ -39,25 +39,25 @@ test_that("A subset of raw data entry data can be mapped correctly to create an 
     select(lMapping$dfDATAENT$strIDCol, Count) %>%
     distinct()
 
-  # count number of overall PK data forms within each subject and remove duplicate records
-  data_entry_forms <- data_entry_orig %>%
+  # count number of overall data pages within each subject and remove duplicate records
+  data_entry_pages <- data_entry_orig %>%
     filter(!!sym(lMapping$dfDATAENT$strFormCol) == "PK") %>%
     group_by_at(lMapping$dfSUBJ$strIDCol) %>%
     mutate(Total = n()) %>%
-    select(lMapping$dfLB$strIDCol, Total) %>%
+    select(lMapping$dfDATAENT$strIDCol, Total) %>%
     distinct()
 
   # combine into one data frame
-  data_entry_all <- full_join(data_entry, data_entry_forms)
+  data_entry_all <- full_join(data_entry, data_entry_pages, by = "subjid")
 
   # read in raw source DM data
   dm_raw_orig <- clindata::rawplus_dm
   dm_raw <- dm_raw_orig
 
   # join DM and data entry lag counts - full_join() to keep records from both data frames
-  expected <- full_join(dm_raw, data_entry_all) %>%
+  expected <- full_join(dm_raw, data_entry_all, by = "subjid") %>%
     mutate(Count = replace_na(Count, 0)) %>%
-    filter(Total != 0 | !is.na(Total)) %>% # remove subjects without any data forms
+    filter(Total != 0 | !is.na(Total)) %>% # remove subjects without any data pages
     arrange(!!sym(lMapping$dfSUBJ$strIDCol)) %>%
     select(all_of(cols))
 
