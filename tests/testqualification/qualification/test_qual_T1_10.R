459b191ed7210b9038c6c0be006fadb28633d5f1
Chelsea Dickens <chelsea.dickens@atorusresearch.com>
2023-01-23 20:16:08 +0000

    add tests for AE_Assess

---
+++ tests/testqualification/qualification/test_qual_T1_10.R
@@ -0,0 +1,46 @@
+test_that("AE assessment can return a correctly assessed data frame for the poisson test grouped by the poissonsite variable when given correct input data and a site with low enrollment from safetyData, from safetyData and the results should be flagged correctly.", {
+  # gsm analysis
+  dfInput <- gsm::AE_Map_Raw()
+
+  nMinDenominator <- 5
+
+  test1_10 <- AE_Assess(
+    dfInput = dfInput,
+    strMethod = "Poisson",
+    nMinDenominator = nMinDenominator
+  )
+
+  # Double Programming
+  t1_10_input <- dfInput
+
+  t1_10_transformed <- dfInput %>%
+    qualification_transform_counts()
+
+  t1_10_analyzed <- t1_10_transformed %>%
+    qualification_analyze_poisson()
+
+  class(t1_10_analyzed) <- c("tbl_df", "tbl", "data.frame")
+
+  t1_10_flagged <- t1_10_analyzed %>%
+    qualification_flag_poisson()
+
+  t1_10_summary <- t1_10_flagged %>%
+    select(GroupID, Numerator, Denominator, Metric, Score, Flag) %>%
+    arrange(desc(abs(Metric))) %>%
+    arrange(match(Flag, c(2, -2, 1, -1, 0))) %>%
+    mutate(Score = case_when(Denominator >= nMinDenominator ~ Score,
+                             Denominator < nMinDenominator ~ NA_real_),
+           Flag = case_when(Denominator >= nMinDenominator ~ Flag,
+                             Denominator < nMinDenominator ~ NA_real_))
+
+  T1_10 <- list(
+    "dfTransformed" = t1_10_transformed,
+    "dfAnalyzed" = t1_10_analyzed,
+    "dfFlagged" = t1_10_flagged,
+    "dfSummary" = t1_10_summary
+  )
+
+  # compare results
+  # remove bounds dataframe for now
+  expect_equal(test1_10$lData[names(test1_10$lData) != "dfBounds"], T1_10)
+})
