82913c18185b16301f4ec9e69bdb9fad4a34fec8
Matt Roumaya <matt.rouyama@gilead.com>
2023-02-28 21:59:21 +0000

    add MakeTrendData to pass to report

--- R/MakeTimeSeriesLongitudinal.R
+++ R/MakeTimeSeriesLongitudinal.R
@@ -1,6 +1,6 @@
 #' Create longitudinal snapshot from results_summary.
 #'
-#' @param dir
+#' @param cDirectory
 #'
 #' @return `data.frame` containing longitudinal snapshots of `{gsm}` analyses.
 #'
@@ -15,34 +15,36 @@
 #' @importFrom purrr map_df
 #'
 #' @export
-MakeTimeSeriesLongitudinal <- function(dir) {
+MakeTimeSeriesLongitudinal <- function(cDirectory) {
 
-  results_summary <- purrr::map_df(list.files(dir), function(x) {
-    read.csv(paste0(dir, "/", x, "/results_summary.csv"))
+  results_summary <- purrr::map_df(list.files(cDirectory), function(x) {
+    read.csv(paste0(cDirectory, "/", x, "/results_summary.csv"))
   })
 
-  status_param <- purrr::map_df(list.files(dir), function(x) {
-    read.csv(paste0(dir, "/", x, "/status_param.csv"))
+  status_param <- purrr::map_df(list.files(cDirectory), function(x) {
+    read.csv(paste0(cDirectory, "/", x, "/status_param.csv"))
   })
 
-  meta_param <- purrr::map_df(list.files(dir), function(x) {
-    read.csv(paste0(dir, "/", x, "/meta_param.csv")) %>%
+  meta_param <- purrr::map_df(list.files(cDirectory), function(x) {
+    read.csv(paste0(cDirectory, "/", x, "/meta_param.csv")) %>%
       mutate(gsm_analysis_date = as.Date(gsm_analysis_date, "%Y-%m-%d"))
   }) %>%
     filter(gsm_analysis_date == max(gsm_analysis_date))
 
-  meta_workflow <- purrr::map_df(list.files(dir), function(x) {
-    read.csv(paste0(dir, "/", x, "/meta_workflow.csv")) %>%
+  meta_workflow <- purrr::map_df(list.files(cDirectory), function(x) {
+    read.csv(paste0(cDirectory, "/", x, "/meta_workflow.csv")) %>%
       mutate(gsm_analysis_date = as.Date(gsm_analysis_date, "%Y-%m-%d"))
   }) %>%
     filter(gsm_analysis_date == max(gsm_analysis_date))
 
-  list(
+  all_data <- list(
     results_summary = results_summary,
     status_param = status_param,
     meta_param = meta_param,
     meta_workflow = meta_workflow
   )
+
+  return(all_data)
 
 }
 
---
+++ R/MakeTrendData.R
@@ -0,0 +1,20 @@
+#' Make Time Series Trend Data
+#'
+#' @param lStudyAssess `list` The output of [gsm::Study_Assess()].
+#' @param cDirectory `character` Path to longitudinal snapshots, returned by [gsm::Make_Snapshot()].
+#'
+#'
+#' @examples
+#'
+#' @return `list` The output of [gsm::Study_Assess()] with longitudinal data appended.
+#'
+#' @export
+MakeTrendData <- function(lStudyAssess, cDirectory) {
+
+  longitudinal <- MakeTimeSeriesLongitudinal(cDirectory)
+
+  lStudyAssess[["longitudinal"]] <- longitudinal
+
+  return(lStudyAssess)
+
+}
