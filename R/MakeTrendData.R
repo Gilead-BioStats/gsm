78410456c1927b7727c4d08faa2fd5c2e29fa1c8
Matt Roumaya <matt.rouyama@gilead.com>
2023-03-03 16:38:16 +0000

    add timeseries chart to report

--- R/MakeTrendData.R
+++ R/MakeTrendData.R
@@ -2,7 +2,7 @@
 #'
 #' @param lStudyAssess `list` The output of [gsm::Study_Assess()].
 #' @param cDirectory `character` Path to longitudinal snapshots, returned by [gsm::Make_Snapshot()].
-#'
+#' @param bAppendTimeSeriesCharts `logical` Append time series charts to KRIs? Default: `TRUE`.
 #'
 #' @examples
 #' \dontrun{
@@ -16,9 +16,20 @@
 #' @return `list` The output of [gsm::Study_Assess()] with longitudinal data appended.
 #'
 #' @export
-MakeTrendData <- function(lStudyAssess, cDirectory) {
+MakeTrendData <- function(lStudyAssess, cDirectory, bAppendTimeSeriesCharts = TRUE) {
 
   longitudinal <- MakeTimeSeriesLongitudinal(cDirectory)
+
+  if (bAppendTimeSeriesCharts) {
+    lStudyAssess <- lStudyAssess %>%
+      purrr::map(function(kri) {
+        kri$lResults$lCharts[['timeSeriesContinuousJS']] <- timeSeriesContinuous(kri = kri$name,
+                                                                                 raw_results = longitudinal$results_summary,
+                                                                                 raw_param = longitudinal$params,
+                                                                                 raw_workflow = longitudinal$meta_workflow)
+        return(kri)
+      })
+  }
 
   lStudyAssess[["longitudinal"]] <- longitudinal %>%
     purrr::map(function(x) {
@@ -27,6 +38,8 @@ MakeTrendData <- function(lStudyAssess, cDirectory) {
           gsm_analysis_date = as.Date(.data$gsm_analysis_date, "%Y-%d-%m")
           )
     })
+
+
 
   return(lStudyAssess)
 
