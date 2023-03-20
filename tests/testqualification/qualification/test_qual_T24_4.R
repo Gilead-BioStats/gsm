17b3f2aeb725659c3791af79c386e6c4b1302d83
Matt Roumaya <matt.rouyama@gilead.com>
2023-03-20 20:50:02 +0000

    update tests

--- tests/testqualification/qualification/test_qual_T24_4.R
+++ tests/testqualification/qualification/test_qual_T24_4.R
@@ -2,9 +2,9 @@ test_that("A subset of Raw+ Protocol Deviation data can be mapped correctly to c
 
 
   ########### gsm mapping ###########
-  subset <- FilterData(dfInput = clindata::rawplus_protdev,
-                       strCol = "importnt",
-                       anyVal = "Y") # filtering only for important PDs
+  subset <- FilterData(dfInput = clindata::ctms_protdev,
+                       strCol = "DeemedImportant",
+                       anyVal = "Yes") # filtering only for important PDs
 
   observed <- gsm::PD_Map_Raw_Binary(
     dfs = list(
@@ -15,7 +15,7 @@ test_that("A subset of Raw+ Protocol Deviation data can be mapped correctly to c
 
   ########### double programming ###########
   # read in default mapping specs
-  lMapping <- yaml::read_yaml(system.file("mappings", "mapping_rawplus.yaml", package = "gsm"))
+  lMapping <- yaml::read_yaml(system.file("mappings", "mapping_ctms.yaml", package = "gsm"))
 
   # create cols vector to facilitate connecting lMapping with source data variables
   cols <- c(SubjectID = lMapping$dfSUBJ$strIDCol,
@@ -27,12 +27,12 @@ test_that("A subset of Raw+ Protocol Deviation data can be mapped correctly to c
             "Total")
 
   # read in raw source PD data
-  pd_raw_orig <- clindata::rawplus_protdev
+  pd_raw_orig <- clindata::ctms_protdev
 
   # assign binary indicator for occurrence of PDs within each subject and remove duplicate records
   pd_raw <- pd_raw_orig %>%
     filter(!!sym(lMapping$dfPD$strImportantCol) == lMapping$dfPD$strImportantVal) %>%
-    group_by_at(lMapping$dfSUBJ$strIDCol) %>%
+    group_by_at(lMapping$dfPD$strIDCol) %>%
     select(lMapping$dfPD$strIDCol) %>%
     mutate(Count = 1) %>%
     distinct()
@@ -42,7 +42,7 @@ test_that("A subset of Raw+ Protocol Deviation data can be mapped correctly to c
   dm_raw <- dm_raw_orig
 
   # join DM and PD data - full_join() to keep records from both data frames
-  expected <- full_join(dm_raw, pd_raw, by = "subjid") %>%
+  expected <- full_join(dm_raw, pd_raw, by = c("subjid" = "SubjectEnrollmentNumber")) %>%
     group_by_at(lMapping$dfSUBJ$strIDCol) %>%
     mutate(Count = replace_na(Count, 0),
            Total = n()) %>%
