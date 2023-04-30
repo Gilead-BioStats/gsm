b7651129cd1e192dc94486ebc864dd8a9d4a852c
Spencer Childress <spencer.c.childress@gmail.com>
2023-04-30 13:44:02 -0400

    Update R/util-StackSnapshots.R

--- R/util-StackSnapshots.R
+++ R/util-StackSnapshots.R
@@ -22,7 +22,7 @@ StackSnapshots <- function(cPath, lSnapshot = NULL) {
     "[ cPath ] does not exist." = file.exists(cPath)
   )
 
-  snapshots <- list.dirs(cPath) %>%
+  snapshots <- list.dirs(cPath, recursive = FALSE) %>%
       # require YYYY-MM-DD naming convention of snapshot directories
       .[grepl('/\\d{4}-\\d{2}-\\d{2}$', .)]
 
