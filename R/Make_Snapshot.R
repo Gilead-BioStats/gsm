200c2f6f7c355898bd78ac6beeb3e714d028677e
Spencer Childress <spencer.c.childress@gmail.com>
2023-05-02 12:54:08 -0400

    drop workflows with falsy status

--- R/Make_Snapshot.R
+++ R/Make_Snapshot.R
@@ -264,7 +264,7 @@ bFlowchart = FALSE
 
   # results_summary ---------------------------------------------------------
   results_summary <- purrr::map(lResults, ~ .x[["lResults"]]) %>%
-    purrr::discard(is.null) %>%
+    purrr::discard(~is.null(.x) | .x$lChecks$status == FALSE) %>%
     purrr::imap_dfr(~ .x$lData$dfSummary %>%
       mutate(
         KRIID = .y,
