193f9f15fa768df40ac3c4ecd5826168c2b0a7b8
Spencer Childress <spencer.c.childress@gmail.com>
2023-06-21 11:28:33 -0400

    allow for non-packaged workflows

--- R/util-MakeWorkflowList.R
+++ R/util-MakeWorkflowList.R
@@ -1,12 +1,12 @@
 #' `r lifecycle::badge("stable")`
 #'
-#' Load assessments from a package/directory.
+#' Load workflows from a package/directory.
 #'
 #' @details
-#' `MakeWorkflowList()` is a utility function that creates a workflow mapping for assessments used in [gsm::Study_Assess()].
+#' `MakeWorkflowList()` is a utility function that creates a list of workflows for use in [gsm::Study_Assess()].
 #'
 #' @param strNames `array of character` List of workflows to include. NULL (the default) includes all workflows in the specified locations.
-#' @param strPath `character` The location of assessment YAML files. If package is specified, function will look in `/inst` folder.
+#' @param strPath `character` The location of workflow YAML files. If package is specified, function will look in `/inst` folder.
 #' @param strPackage `character` The name of the package with assessments.
 #' @param bRecursive `logical` Find files in nested folders? Default FALSE.
 #'
@@ -16,7 +16,7 @@
 #' # get specific workflow files
 #' workflow <- MakeWorkflowList(strNames = c("kri0001", "kri0005", "cou0003"))
 #'
-#' @return `list` A list of assessments with workflow and parameter metadata.
+#' @return `list` A list of workflows with workflow and parameter metadata.
 #'
 #' @importFrom cli cli_alert_warning
 #' @importFrom purrr map map_chr keep set_names
@@ -25,44 +25,67 @@
 #'
 #' @export
 
-MakeWorkflowList <- function(strNames = NULL, strPath = "workflow", strPackage = "gsm", bRecursive = FALSE) {
+MakeWorkflowList <- function(
+  strNames = NULL,
+  strPath = "workflow",
+  strPackage = "gsm",
+  bRecursive = FALSE
+) {
+  stopifnot(
+    '[ strPath ] must be specified.' = !is.null(strPath)
+  )
+
   if (!is.null(strPackage)) {
     path <- system.file(strPath, package = strPackage)
+  } else {
+    path <- strPath
   }
 
-  yaml_files <- list.files(path, pattern = "\\.yaml$", full.names = TRUE, recursive = bRecursive)
+  stopifnot(
+    '[ strPath ] must exist.' = dir.exists(path)
+  )
+
+  yaml_files <- list.files(
+    path,
+    pattern = "\\.yaml$",
+    full.names = TRUE,
+    recursive = bRecursive
+  )
 
   # copied from tools package
   file_path_sans_ext <- function(x) {
     sub("([^.]+)\\.[[:alnum:]]+$", "\\1", x)
   }
 
-  assessments <- yaml_files %>%
-    purrr::map(function(path) {
-      assessment <- yaml::read_yaml(path)
-      assessment$path <- path
-      if (!utils::hasName(assessment, "name")) {
-        assessment$name <- path %>%
+  workflows <- yaml_files %>%
+    purrr::map(function(yaml_file) {
+      workflow <- yaml::read_yaml(yaml_file)
+      workflow$path <- yaml_file
+
+      if (!utils::hasName(workflow, "name")) {
+        workflow$name <- workflow$path %>%
           file_path_sans_ext() %>%
           basename()
       }
-      return(assessment)
-    })
-  names(assessments) <- assessments %>% purrr::map_chr(~ .x$name)
+
+      return(workflow)
+    }) %>%
+    stats::setNames(purrr::map_chr(., ~.x$name))
 
   if (!is.null(strNames)) {
-    not_found <- strNames[!strNames %in% names(assessments)]
+    not_found <- strNames[!strNames %in% names(workflows)]
 
     if (length(not_found) > 0) {
-      cli::cli_alert_warning("{.val {not_found}} {?is/are} not {?a /}supported workflow{?/s}! Check the output of {.fn MakeAssessmentList} for NULL values.")
-      assessments <- c(
+      cli::cli_alert_warning("{.val {not_found}} {?is/are} not {?a /}supported workflow{?/s}! Check the output of {.fn MakeWorkflowList} for NULL values.")
+
+      workflows <- c(
         vector(mode = "list", length = length(not_found)) %>% purrr::set_names(nm = not_found),
-        purrr::keep(assessments, names(assessments) %in% strNames)
+        purrr::keep(workflows, names(workflows) %in% strNames)
       )
     } else {
-      assessments <- purrr::keep(assessments, names(assessments) %in% strNames)
+      workflows <- purrr::keep(workflows, names(workflows) %in% strNames)
     }
   }
 
-  return(assessments)
+  return(workflows)
 }
