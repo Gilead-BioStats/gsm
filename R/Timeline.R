526a89c30d913751a371a2a002f665c68a512bbb
Jacob Anderson <jacob.anderson@atorusresearch.com>
2023-07-27 17:30:42 +0000

    added legend items and removed interactivity

--- R/Timeline.R
+++ R/Timeline.R
@@ -17,7 +17,6 @@
 #'
 #' @export
 
-
 Make_Timeline <- function(study, n_breaks = 10, date_format = "%b\n%Y"){
 # function to extract date columns
 is.convertible.to.date <- function(x) !is.na(as.Date(as.character(x), tz = 'UTC', format = '%Y-%m-%d'))
@@ -43,14 +42,16 @@ breaks <- pretty(seq(min(d$date), max(d$date), by = "day"), n_breaks)
 # Define blank plot theme
 empty <- function()(theme(plot.background = element_rect(fill = "white"),
                           panel.background = element_rect(fill = "white"),
-                          legend.position = c(0.5, 0.5),
+                          legend.position = c(0.5, 0.45),
                           legend.key = element_rect(fill = "white"),
                           legend.text.align = 0,
                           legend.direction = "horizontal",
-                          legend.background = element_rect(linetype = 1, linewidth = .35, color = "black"),
+                          # legend.background = element_rect(linetype = 1, linewidth = .35, color = "black"),
                           legend.text = element_text(size = 8),
                           legend.title = element_blank(),
                           legend.margin = margin(t = 0, r = 1.5, b = 1.5, l = 1.5, unit = "mm"),
+                          legend.box.background = element_rect(fill = "white", color = "black"),
+                          legend.spacing.y = unit(0,"cm"),
                           axis.title = element_blank(),
                           axis.text.y = element_blank(),
                           axis.ticks.y = element_blank(),
@@ -60,32 +61,27 @@ empty <- function()(theme(plot.background = element_rect(fill = "white"),
 
 # Generate Plot
 a <- ggplot(d, aes(date, disp))+
-     scale_x_date(date_labels = date_format,
-                  limits = c(min(d$date) - (as.numeric(max(d$date) - min(d$date)) *.1),
-                             max(d$date) + (as.numeric(max(d$date) - min(d$date)) *.1)),
-                  expand = c(.15, -.15)) +
-     gggenes::geom_gene_arrow(aes(xmin = min(date) - (as.numeric(max(date) - min(date)) *.1),
-                         xmax = max(date) + (as.numeric(max(date) - min(date)) *.1),
-                         y = 0),
-                     color = "dodgerblue",
-                     arrowhead_height = unit(15, "mm"),
-                     arrow_body_height = unit(ifelse(grepl("\n", date_format), 10, 7), "mm")) +
-     ggiraph::geom_point_interactive(aes(color = label,
-                                shape = estimate,
-                                tooltip = paste0(activity, "\n", "(", date, ")"),
-                                data_id = date),
-                            size = 3) +
-     scale_shape_manual(values = c(19, 1)) +
-     annotate(geom = "text", x = breaks, y = 0, label = format(breaks, format = date_format), size = 3, fontface = 2) +
-     expand_limits(y = c(30,-50)) +
-     guides(alpha = "none",
-            shape = "none",
-            color = guide_legend(ncol = 3,
-                                 title.position = "top")) +
-     empty()
+   scale_x_date(date_labels = date_format,
+                limits = c(min(d$date) - (as.numeric(max(d$date) - min(d$date)) *.1),
+                           max(d$date) + (as.numeric(max(d$date) - min(d$date)) *.1)),
+                expand = c(.15, -.15)) +
+   gggenes::geom_gene_arrow(aes(xmin = min(date) - (as.numeric(max(date) - min(date)) *.1),
+                       xmax = max(date) + (as.numeric(max(date) - min(date)) *.1),
+                       y = 0),
+                   color = "dodgerblue",
+                   arrowhead_height = unit(15, "mm"),
+                   arrow_body_height = unit(ifelse(grepl("\n", date_format), 10, 7), "mm")) +
+  ggiraph::geom_point_interactive(aes(color = label,
+                                      shape = estimate),
+                                  size = 3) +
+   scale_shape_manual(values = c(19, 1), labels = c("Actual", "Estimated")) +
+   annotate(geom = "text", x = breaks, y = 0, label = format(breaks, format = date_format), size = 3, fontface = 2) +
+   expand_limits(y = c(30,-50)) +
+   guides(alpha = "none",
+          color = guide_legend(ncol = 3)) +
+   empty()
 
 ggiraph::girafe(ggobj = a)
 
 }
-
 
