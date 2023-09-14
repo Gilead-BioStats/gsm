534e687dcdf7edf568532a2cc19d126759468643
Jacob Anderson <jacob.anderson@atorusresearch.com>
2023-09-14 21:49:56 +0000

    updated Study_Report, KRIReportByCountry, and KRIReportBySite

--- R/Study_Report.R
+++ R/Study_Report.R
@@ -26,7 +26,7 @@
 #' # Adding metadata for a single snapshot
 #' one_snapshot <- Make_Snapshot()
 #' Study_Report(
-#'   lAssessments = one_snapshot$lStudyAssessResults,
+#'   lSnapshot = one_snapshot,
 #'   dfStudy = one_snapshot$lSnapshot$status_study
 #' )
 #'
@@ -58,6 +58,8 @@ Study_Report <- function(
   # input check
   lAssessments <- if("lStudyAssessResults" %in% names(lSnapshot)){lSnapshot$lStudyAssessResults} else {lSnapshot}
   lStatus <- if("lStatus" %in% names(lSnapshot)){lSnapshot$lStatus} else {NULL}
+  dfStudy <- if("status_study" %in% names(lSnapshot$lSnapshot)){lSnapshot$lSnapshot$status_study} else {NULL}
+  dfSite <- if("status_site" %in% names(lSnapshot$lSnapshot)){lSnapshot$lSnapshot$status_site} else {NULL}
   stopifnot(
     "strReportType is not 'site' or 'country' or 'QTL'" = strReportType %in% c("site", "country", "QTL"),
     "strReportType must be length 1" = length(strReportType) == 1
@@ -66,6 +68,7 @@ Study_Report <- function(
   # set output path
   if (is.null(strOutpath) & strReportType == "site") {
     strOutpath <- paste0(getwd(), "/gsm_site_report.html")
+
   } else if (is.null(strOutpath) & strReportType == "country") {
     strOutpath <- paste0(getwd(), "/gsm_country_report.html")
 
