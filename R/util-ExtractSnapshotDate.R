7f557b6a60a98850ae4bfe364a1330f696b83555
Jacob Anderson <jacob.anderson@atorusresearch.com>
2023-10-09 21:12:50 +0000

    updated imports to pass checks

--- R/util-ExtractSnapshotDate.R
+++ R/util-ExtractSnapshotDate.R
@@ -4,6 +4,11 @@
 #'
 #' @return `data.frame` a data.frame containing the foldername and the associated snapshot date.
 #'
+#' @import dplyr
+#' @importFrom purrr map, set_names
+#' @importFrom stringr str_extract
+#' @importFrom tibble set_names, rownames_to_column
+#'
 #' @export
 ExtractSnapshotDate <- function(snapshot_paths){
   acceptable_files <- c("meta_param.csv",
@@ -16,14 +21,14 @@ ExtractSnapshotDate <- function(snapshot_paths){
                         "status_study.csv",
                         "status_workflow.csv")
 
-  output <- map(snapshot_paths, ~paste0(., "/", acceptable_files)) %>%
-    set_names(str_extract(snapshot_paths, "([^/]+$)")) %>%
+  output <- purrr::map(snapshot_paths, ~paste0(., "/", acceptable_files)) %>%
+    purrr::set_names(stringr::str_extract(snapshot_paths, "([^/]+$)")) %>%
     map(., ~read.csv(.[file.exists(.)][1], , nrows=1) %>%
           pull(gsm_analysis_date)) %>%
-    as_tibble() %>%
+    tibble::as_tibble() %>%
     t() %>%
     as.data.frame() %>%
-    rownames_to_column() %>%
+    tibble::rownames_to_column() %>%
     `colnames<-`(c("foldername", "snapshot_date")) %>%
     mutate(snapshot_date = as.Date(snapshot_date))
 
