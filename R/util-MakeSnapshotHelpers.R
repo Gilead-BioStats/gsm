ff78f3c9847f40bd0b7fe1718a314aad124a483b
Matt Roumaya <matt.rouyama@gilead.com>
2023-12-07 18:21:10 +0000

    move SubsetStackedSnapshots

--- R/util-MakeSnapshotHelpers.R
+++ R/util-MakeSnapshotHelpers.R
@@ -764,3 +764,29 @@ MakeRptStudySnapshot <- function(lMeta, gsm_analysis_date) {
 
   return(output)
 }
+
+
+#' SubsetStackedSnapshots
+#'
+#' @description
+#' This function is used to subset a list of stacked snapshots within [gsm::Make_Snapshot()].
+#'
+#' @param workflowid `character` workflow ID or `kri_id`.
+#'
+#'
+#' @keywords internal
+SubsetStackedSnapshots <- function(workflowid, lStackedSnapshots) {
+  subset_snapshots <- purrr::map(lStackedSnapshots, function(x) {
+
+    if ("kri_id" %in% names(x)) {
+      x %>%
+        filter(
+          kri_id == workflowid
+        )
+    }
+
+  }) %>%
+    purrr::discard(is.null)
+
+  return(subset_snapshots)
+}
