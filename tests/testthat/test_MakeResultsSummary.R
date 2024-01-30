e2c0016d5ee7f8b7033b5991eb9f611218002cee
Jacob Anderson <jacob.anderson.science@gmail.com>
2024-01-30 10:58:08 -0800

    incorporated review feedback

--- tests/testthat/test_MakeResultsSummary.R
+++ tests/testthat/test_MakeResultsSummary.R
@@ -5,7 +5,7 @@ source(testthat::test_path("testdata/data.R"))
 dfConfigWorkflow <- gsm::config_workflow
 
 # Create a sample list of results
-lResults <- Study_Assess()
+lResults <- Study_Assess(lData = lData)
 
 # Write a test
 test_that("MakeResultsSummary generates correct summary data frame", {
@@ -13,14 +13,7 @@ test_that("MakeResultsSummary generates correct summary data frame", {
   results_summary <- MakeResultsSummary(lResults, dfConfigWorkflow)
 
   # Check if the resulting data frame has the expected structure
-  expect_true("studyid" %in% names(results_summary), "Missing 'studyid' column")
-  expect_true("workflowid" %in% names(results_summary), "Missing 'workflowid' column")
-  expect_true("groupid" %in% names(results_summary), "Missing 'groupid' column")
-  expect_true("numerator" %in% names(results_summary), "Missing 'numerator' column")
-  expect_true("denominator" %in% names(results_summary), "Missing 'denominator' column")
-  expect_true("metric" %in% names(results_summary), "Missing 'metric' column")
-  expect_true("score" %in% names(results_summary), "Missing 'score' column")
-  expect_true("flag" %in% names(results_summary), "Missing 'flag' column")
+  expect_snapshot(names(results_summary))
 
   # Check if the resulting data frame has the expected number of rows
   expect_true(is.data.frame(results_summary))
--- tests/testthat/test_Study_Report.R
+++ tests/testthat/test_Study_Report.R
@@ -4,36 +4,25 @@ source(testthat::test_path("testdata/data.R"))
 # Write tests for Study_Report function
 test_that("Study_Report function works as expected", {
   snapshot <- Make_Snapshot(lData = lData)
+  part_snap <- snapshot[c("lSnapshots", "lCharts", "lInputs")]
 
   # Create temporary output file path
   # tmp_outpath <- tempfile(fileext = ".html")
 
   # Test the function with different report types
   expect_error(
-    Study_Report(snapshot, strOutpath = tmp_outpath, strReportType = "person")
+    Study_Report(snapshot, strReportType = "person")
   )
 
   expect_error(
-    Study_Report(snapshot, strOutpath = tmp_outpath, strReportType = c("site", "QTL"))
+    Study_Report(snapshot, strReportType = c("site", "QTL"))
   )
 
+  expect_error(
+    expect_message(
+      Study_Report(part_snap)
+    )
+  )
 
-  # Study_Report(snapshot, strOutpath = tmp_outpath, strReportType = "site")
-  # expect_true(
-  #   file.exists(tmp_outpath), "Site report file should be created"
-  # )
-  #
-  # Study_Report(snapshot, strOutpath = tmp_outpath, strReportType = "country")
-  # expect_true(
-  #   file.exists(tmp_outpath), "Country report file should be created"
-  # )
-  #
-  # Study_Report(snapshot, strOutpath = tmp_outpath, strReportType = "QTL")
-  # expect_true(
-  #   file.exists(tmp_outpath), "QTL report file should be created"
-  # )
-
-  # Clean up - remove the temporary output file
-  # unlink(tmp_outpath)
 })
 
