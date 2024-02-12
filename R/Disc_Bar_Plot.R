6ff5c25f77973c816335258352702fd52c09c9ef
Jacob Anderson <jacob.anderson.science@gmail.com>
2024-02-12 13:28:31 -0800

    made plots horizontal with scrolling

--- R/Disc_Bar_Plot.R
+++ R/Disc_Bar_Plot.R
@@ -8,66 +8,41 @@
 #' @keywords internal
 Disc_Bar_Plot <- function(dfInput, strGroup){
   if(strGroup == "Study"){
-    plot_data <- dfInput %>%
-      filter(!is.empty(`Discontinuation Reason`)) %>%
-      group_by(`Discontinuation Reason`) %>%
-      summarise(disp = n())
-
-    ggplotly(
-      ggplot(plot_data,
-             aes(x = reorder(`Discontinuation Reason`, desc(disp)),
-                 y = disp,
-                 text = paste0(
-                   glue("Total Patients: {plot_data$disp}"), "</br></br>",
-                   glue("Reason: {plot_data$`Discontinuation Reason`}")
-                 )
-             )
-      ) +
-        geom_col(fill = "dodgerblue", alpha = .8) +
-        xlab("Discontinuation Reason") +
-        ylab("# Patients Disc.") +
-        theme(
-          axis.text.x = element_text(angle = 45),
-          axis.title.x = element_blank(),
-          axis.title.y = element_text(face = "bold", size = 11),
-          panel.background = element_rect(fill = "white"),
-          panel.grid = element_blank(),
-          axis.line = element_line(color = "lightgrey", size = .25)
-        ), tooltip = "text"
-    )
+    plot_ly(dfInput) %>%
+      add_trace(
+        y = ~reorder(`Discontinuation Reason`, disc),
+        x = ~disc,
+        type = "bar",
+        orientation = "h",
+        text = paste0(
+          glue("Total Patients: {dfInput$disc}"), "</br></br>",
+          glue("Reason: {dfInput$`Discontinuation Reason`}")
+        ),
+        textposition = "none",
+        hoverinfo = "text") %>%
+      layout(yaxis = list(title = ""),
+             xaxis = list(title = list(text = "<b>Patients Discontinued</b>"),
+                          titlefont = list(size = 16))
+      )
   } else if (strGroup == "Site") {
-    plot_data <- dfInput %>%
-      filter(!is.empty(`Discontinuation Reason`)) %>%
-      group_by(`Site ID`, `Discontinuation Reason`) %>%
-      summarise(disc = n()) %>%
-      group_by(`Site ID`) %>%
-      mutate(total = sum(disc))
-
-    ggplotly(
-      ggplot(plot_data,
-             aes(x = reorder(`Site ID`, desc(total)),
-                 y = disc,
-                 fill = `Discontinuation Reason`,
-                 text = paste0(
-                   glue("Site: {plot_data$`Site ID`}"), "</br></br>",
-                   glue("Patients Discontinued: {plot_data$disc}"), "</br>",
-                   glue("Reason: {plot_data$`Discontinuation Reason`}")
-                 )
-             )
-      ) +
-        geom_col(show.legend = FALSE) +
-        xlab("Discontinuation Reason") +
-        ylab("# Patients Disc.") +
-        expand_limits(y = max(plot_data$disc) * 1.1) +
-        theme(
-          axis.text.x = element_text(angle = 90),
-          axis.title.x = element_blank(),
-          axis.title.y = element_text(face = "bold", size = 11),
-          panel.background = element_rect(fill = "white"),
-          panel.grid = element_blank(),
-          axis.line = element_line(color = "lightgrey", size = .25)
-        ), tooltip = "text"
-    )
+    plot_ly(dfInput) %>%
+      add_trace(
+        y = ~reorder(`Site ID`, total),
+        x = ~disc,
+        color = ~`Discontinuation Reason`,
+        type = "bar",
+        orientation = "h",
+        text = paste0(
+          glue("Site: {dfInput$`Site ID`}"), "</br></br>",
+          glue("Patients Discontinued: {dfInput$disc}"), "</br>",
+          glue("Reason: {dfInput$`Discontinuation Reason`}")
+        ),
+        hoverinfo = "text"
+      ) %>%
+      layout(yaxis = list(title = ""),
+             xaxis = list(title = list(text = "<b>Patients Discontinued</b>"),
+                          titlefont = list(size = 16)),
+             barmode = "stack")
   }
 
 }
