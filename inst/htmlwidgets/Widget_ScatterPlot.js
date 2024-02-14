9a64ca222ec41e37b0b0244d75ad2e9abb91738d
Matt Roumaya <matt.rouyama@gilead.com>
2024-02-14 02:01:03 +0000

    revert

--- inst/htmlwidgets/Widget_ScatterPlot.js
+++ inst/htmlwidgets/Widget_ScatterPlot.js
@@ -5,50 +5,64 @@ HTMLWidgets.widget({
         return {
             renderValue: function(x) {
 
-          // scatter plot configuration
-          const lLabels = x.lLabels;
-
-          if (typeof x.selectedGroupIDs !== 'undefined') {
-            lLabels.selectedGroupIDs = number_to_array(x.selectedGroupIDs);
-          }
-
-          if (x.addSiteSelect)
-            lLabels.clickCallback = function (d) {
+                // scatter plot configuration
+                const lLabels = x.lLabels;
+                lLabels.selectedGroupIDs = number_to_array(x.selectedGroupIDs);
 
 
-              if (typeof Shiny !== "undefined") {
-                const namespace = "gsmApp";
 
-                if (instance.data.config.selectedGroupIDs.length > 0) {
-                  console.log(`Selected site ID: ${instance.data.config.selectedGroupIDs}`);
-
-                  instance.helpers.updateConfig(instance, instance.data.config);
-
-                  Shiny.setInputValue("site", instance.data.config.selectedGroupIDs);
-                }
-              } else {
-              // clickCallback.bind(null, instance, siteSelect);
-
-              instance.data.config.selectedGroupIDs = instance.data.config.selectedGroupIDs.includes(d.groupid) ? "None" : d.groupid;
-              siteSelect.value = instance.data.config.selectedGroupIDs;
-              instance.helpers.updateConfig(instance, instance.data.config);
+                if (x.addSiteSelect)
+                    lLabels.clickCallback = function(d) { // clickCallback.bind(null, instance, siteSelect);
 
 
-              instance.helpers.updateConfig(instance, instance.data.config);
-              }
 
-            };
+                        instance.data.config.selectedGroupIDs = instance.data.config.selectedGroupIDs.includes(d.groupid)
+                            ? 'None'
+                            : d.groupid;
+                        siteSelect.value = instance.data.config.selectedGroupIDs;
+                        instance.helpers.updateConfig(instance, instance.data.config);
 
 
-          // generate scatter plot
-          const instance = rbmViz.default.scatterPlot(el, x.dfSummary, lLabels, x.dfBounds);
+                        if (typeof Shiny !== 'undefined') {
 
-          // add dropdown that highlights sites
-          let siteSelect;
-          if (x.addSiteSelect) {
-            siteSelect = addSiteSelect(el, x.dfSummary, instance, x.siteSelectLabelValue);
-          }
+                          const namespace = 'gsmApp';
 
+                          if (instance.data.config.selectedGroupIDs.length > 0) {
+
+                            console.log(
+                              `Selected site ID: ${instance.data.config.selectedGroupIDs}`
+                            );
+
+                            Shiny.setInputValue(
+                              'site',
+                              instance.data.config.selectedGroupIDs
+                            )
+
+                          }
+
+                        }
+
+                        instance.helpers.updateConfig(
+                          instance,
+                          instance.data.config
+                        )
+
+                    };
+
+
+                // generate scatter plot
+                const instance = rbmViz.default.scatterPlot(
+                    el,
+                    x.dfSummary,
+                    lLabels,
+                    x.dfBounds
+                );
+
+
+                // add dropdown that highlights sites
+                let siteSelect;
+                if (x.addSiteSelect)
+                    siteSelect = addSiteSelect(el, x.dfSummary, instance, x.siteSelectLabelValue);
             },
             resize: function(width, height) {
             }
