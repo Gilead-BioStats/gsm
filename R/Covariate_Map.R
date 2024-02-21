b3c0f5d76b1c1489dad1d37815e71d0e16f04f22
Jacob Anderson <jacob.anderson.science@gmail.com>
2024-02-21 13:10:40 -0800

    added mapping

--- R/Covariate_Map.R
+++ R/Covariate_Map.R
@@ -1,21 +1,31 @@
 #' Covariate Map
 #'
-#' @return
+#' @param dfCovariate `data.frame` dataframe of kri covariate of interest
+#' @param strCovariate `string` name of covariate df
+#' @param strCovariteColName `string` name of covariate column of interest
+#' @param strWorkflowId `string` name of kri workflowid
+#'
 #' @export
 #'
-#' @examples
-Covariate_Map <- function(dfCovariate, strCovariteColName, strWorkflowId) {
-
+Covariate_Map <- function(dfCovariate, strCovariate, strCovariteColName, strWorkflowId) {
+  output <- list()
   # do the mapping
+  initial <- dfCovariate %>%
+    select(
+      "Study ID" = studyid,
+      "Site ID" = siteid,
+      "Subject ID" = subjid,
+      "Metric" = strCovariteColName
+    ) %>%
+    distinct()
 
   # return table with count and percentage of each covariate
-
-  covariates[[strWorkflowId]] <- list(
-    study = data.frame(),
-    site = data.frame()
+  output[[strWorkflowId]] <- list(
+    study = filter_covariate(initial, strGroup = "study"),
+    site = filter_covariate(initial, strGroup = "site")
   )
 
-  return(covariates)
+  return(output)
 
 }
 
---
+++ R/filter_covariate.R
@@ -0,0 +1,27 @@
+#' filter covariate by group
+#'
+#' @param df `data.frame` data frame to filter
+#' @param strGroup `string` group to filter by. options include: "study", "site", "subject
+#'
+#' @export
+#'
+#' @keywords internal
+filter_covariate <- function(df, strGroup = "study"){
+  if(strGroup == "study"){
+    output <- df %>%
+      filter(!is.empty(Metric)) %>%
+      group_by(`Study ID`, Metric) %>%
+      summarise("Total #" = n_distinct(`Subject ID`), .groups = "drop") %>%
+      mutate(`%` = pct(round(`Total #`/sum(`Total #`) * 100))) %>%
+      arrange(desc(`Total #`))
+  } else if(strGroup == "site"){
+    output <- df %>%
+      filter(!is.empty(Metric)) %>%
+      group_by(`Site ID`, Metric) %>%
+      summarise("Total #" = n_distinct(`Subject ID`), .groups = "drop") %>%
+      group_by(`Site ID`) %>%
+      mutate(`%` = pct(round(`Total #`/sum(`Total #`) * 100))) %>%
+      arrange(desc(`Total #`))
+  }
+  return(output)
+}
