4139edac24889afd9eaa35966a6eb349b49a104c
Jeremy Wildfire <jwildfire@gmail.com>
2024-04-22 16:26:57 -0400

    change from mapping to hard-coded column names

--- R/Make_Input_Rate.R
+++ R/Make_Input_Rate.R
@@ -2,8 +2,7 @@
 #' @description Calculate a subject level rate from numerator and denominator data
 #' 
 #' @param dfs `list` of `data.frame`s including dfSUBJ, dfNumerator, and dfDenominator. Default: NULL
-#' @param lMapping `list` Column metadata with structure `domain$key`, where `key` contains the name
-#' @param lDomains `list` List of domains to use for mapping subj, domain and mapping dfata defaults. Default: NULL
+#' @param strIDCol `character` Column name in to use for subject ID - should be included in all `dfs`. Default: NULL
 #' @param strNumeratorMethod `character` Method to calculate numerator. Default: "Count"
 #' @param strDenominatorMethod `character` Method to calculate denominator. Default: "Count"
 #' @param strNumeratorCol `character` Column name in `dfNumerator` to use for numerator calculation. Default: NULL
@@ -19,23 +18,16 @@
 #'   dfNumerator = clindata::rawplus_ae,
 #'   dfDenominator = clindata::rawplus_dm
 #' ),
-#' lMapping = gsm::Read_Mapping("rawplus"),
-#' lDomains = list(
-#'  dfSubjects="dfSUBJ",
-#'  dfNumerator="dfAE",
-#'  dfDenominator="dfSUBJ"
-#' ),
+#' strIDCol = "subjid",
 #' strNumeratorMethod = "Count",
 #' strDenominatorMethod = "Sum",
-#' strDenominatorCol = "strTimeOnStudyCol"
+#' strDenominatorCol = "timeontreatment"
 #' )
 #' 
 #' @export
 
 Make_Input_Rate <- function(
     dfs = NULL,
-    lMapping = NULL,
-    lDomains = NULL,
     strNumeratorMethod = "Count",
     strDenominatorMethod = "Count",
     strNumeratorCol = NULL,
@@ -43,19 +35,9 @@ Make_Input_Rate <- function(
     bQuiet = TRUE       
 ) {
 
-#Check if dfs, lMapping, and lDomains are NULL
-if(is.null(dfs) | is.null(lMapping) | is.null(lDomains)){
-    stop("dfs, lMapping, and lDomains must be provided")
-}
-
-# Check if dfs, lMapping, and lDomains are NULL or not lists
-if (!is.list(dfs) | !is.list(lMapping) | !is.list(lDomains)) {
-    stop("dfs, lMapping, and lDomains must be provided as lists")
-}
-
-# check that dfs and domains have 3 elements each
-if (length(dfs) != 3 | length(lDomains) != 3) {
-    stop("dfs and lDomains must have 3 elements each")
+#Check if dfs is NULL
+if(is.null(dfs)){
+    stop("dfs, must be provided")
 }
 
 # Check if strNumeratorMethod and strDenominatorMethod are valid
@@ -83,29 +65,26 @@ if (!all(c("dfSubjects", "dfNumerator", "dfDenominator") %in% names(dfs))) {
     stop("dfs must contain dfSubjects, dfNumerator, and dfDenominator")
 }
 
-#Get needed columns in dfSUBJ
-subjMapping <- lMapping[[lDomains$dfSubjects]]
-dfSUBJ_mapped <- dfs$dfSubjects %>%
-      select(
-        SubjectID = subjMapping[["strIDCol"]],
-        any_of(
-          c(
-            SiteID = subjMapping[["strSiteCol"]],
-            StudyID = subjMapping[["strStudyCol"]],
-            CountryID = subjMapping[["strCountryCol"]],
-            CustomGroupID = subjMapping[["strCustomGroupCol"]]
-          )
-        )
-      )
+# Check that dfSUBJ contains columsn for SubjectID, SiteID, StudyID, and CountryID
+if (!all(c("SubjectID", "SiteID", "StudyID", "CountryID") %in% colnames(dfs$dfSubjects))) {
+    stop("dfSubjects must contain columns for SubjectID, SiteID, StudyID, and CountryID")
+}
 
+# check that "SubjectID" is in all dfs
+if (!all(c("SubjectID") %in% colnames(dfs$dfNumerator)) | !all(c("SubjectID") %in% colnames(dfs$dfDenominator))) {
+    stop("dfNumerator and dfDenominator must contain a column for SubjectID")
+}
+
+#Get needed columns in dfSUBJ
+dfSubjects <- dfs$dfSubjects %>% select(SubjectID, SiteID, StudyID, CountryID)
+        
 #Calculate Numerator
-numeratorMapping <- lMapping[[lDomains$dfNumerator]]
 dfNumerator <- dfs$dfNumerator
-dfNumerator$SubjectID <- dfNumerator[[numeratorMapping[["strIDCol"]]]]
+
 if(strNumeratorMethod == "Count"){
     dfNumerator$Numerator <- 1
 } else {
-    dfNumerator$Numerator <- dfNumerator[[numeratorMapping[[strNumeratorCol]]]]
+    dfNumerator$Numerator <- dfNumerator[[strNumeratorCol]]
 }
 
 dfNumerator_subj <- dfNumerator %>%
@@ -115,15 +94,12 @@ dfNumerator_subj <- dfNumerator %>%
     ungroup()
 
 #Calculate Denominator
-denominatorMapping <- lMapping[[lDomains$dfDenominator]]
 dfDenominator <- dfs$dfDenominator
-dfDenominator$SubjectID <- dfDenominator[[denominatorMapping[["strIDCol"]]]]
 if(strDenominatorMethod == "Count"){
     dfDenominator$Denominator <- 1
 } else {
-    dfDenominator$Denominator <- dfDenominator[[denominatorMapping[[strDenominatorCol]]]]
+    dfDenominator$Denominator <- dfDenominator[[strDenominatorCol]]
 }
-
 
 dfDenominator_subj <- dfDenominator %>%  
     select(SubjectID, Denominator) %>%
@@ -132,7 +108,7 @@ dfDenominator_subj <- dfDenominator %>%
     ungroup()
 
 # Merge Numerator and Denominator with Subject Data. Keep all data in Subject. Fill in missing numerator/denominators with 0
-dfInput <- dfSUBJ_mapped %>%
+dfInput <- dfSubjects %>%
     left_join(dfNumerator_subj, by = "SubjectID") %>%
     left_join(dfDenominator_subj, by = "SubjectID") %>%
     mutate(Numerator = if_else(is.na(Numerator), 0, Numerator),
