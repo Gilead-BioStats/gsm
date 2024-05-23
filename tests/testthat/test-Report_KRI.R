2788f126b325e3493ad1fa4d16c5c266bbb5731e
= <laura.maxwell@atorusresearch.com>
2024-05-23 10:27:47 -0400

    fix Report_KRI() tests

--- tests/testthat/test-Report_KRI.R
+++ tests/testthat/test-Report_KRI.R
@@ -1,63 +1,63 @@
-# lCharts <- list(
-#   kri0001 = NULL
-# )
-#
-# dfSummary <- tibble::tibble(
-#  GroupID = c("10", "100", "101", "102", "103"),
-#  GroupType = rep("Site", 5),
-#  Numerator = seq(2,10,2),
-#  Denomicator = seq(10,50,10),
-#  Metric = c(0.02, 0.02, 0.02, 0.02, 0.02),
-#  Score = c(1,3,2,1,1),
-#  Flag = c(0,2,1,0,0),
-#  MetricID = rep("kri0001", 5)
-# )
-#
-# dfStudy <- clindata::ctms_study %>% rename(StudyID = protocol_number)
-# dfSite <- clindata::ctms_site %>% rename(SiteID = site_num)
-# dfMetrics <- tibble::tibble(
-#   metric = "Adverse Event Rate",
-#   workflowid = "kri0001",
-#   group = "Site"
-# )
-#
-# test_that("Check default output path when strOutpath is NULL", {
-#     expect_output(
-#       Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
-#                  strOutpath = NULL) %>% grepl(paste0(getwd(), "/kri_report.html"), .),
-#       fixed = TRUE )
+lCharts <- list(
+  kri0001 = NULL
+)
+
+dfSummary <- tibble::tibble(
+ GroupID = c("10", "100", "101", "102", "103"),
+ GroupType = rep("Site", 5),
+ Numerator = seq(2,10,2),
+ Denomicator = seq(10,50,10),
+ Metric = c(0.02, 0.02, 0.02, 0.02, 0.02),
+ Score = c(1,3,2,1,1),
+ Flag = c(0,2,1,0,0),
+ MetricID = rep("kri0001", 5)
+)
+
+dfStudy <- clindata::ctms_study %>% rename(StudyID = protocol_number)
+dfSite <- clindata::ctms_site %>% rename(SiteID = site_num)
+dfMetrics <- tibble::tibble(
+  metric = "Adverse Event Rate",
+  workflowid = "kri0001",
+  group = "Site"
+)
+
+test_that("Check default output path when strOutpath is NULL", {
+    expect_output(
+      Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
+                 strOutpath = NULL) %>% grepl(paste0(getwd(), "/kri_report.html"), .),
+      fixed = TRUE )
+})
+
+test_that("Output path is correctly assigned", {
+  withr::with_envvar(c("RSTUDIO_PANDOC" = ""), {
+    expect_identical(
+      Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
+                 strOutpath = "custom_path.html") %>%
+        grepl("custom_path.html", .),
+      TRUE
+    )
+  })
+})
+
+# test_that("Function handles NULL arguments properly", {
+#   expect_silent(Report_KRI())
 # })
 #
-# test_that("Output path is correctly assigned", {
+# test_that("Function handles non-NULL arguments properly", {
 #   withr::with_envvar(c("RSTUDIO_PANDOC" = ""), {
-#     expect_identical(
-#       Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
-#                  strOutpath = "custom_path.html") %>%
-#         grepl("custom_path.html", .),
-#       TRUE
-#     )
+#     expect_silent(Report_KRI(
+#       lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
+#       strOutpath = "test_output.html"
+#     ))
 #   })
 # })
-#
-# # test_that("Function handles NULL arguments properly", {
-# #   expect_silent(Report_KRI())
-# # })
-# #
-# # test_that("Function handles non-NULL arguments properly", {
-# #   withr::with_envvar(c("RSTUDIO_PANDOC" = ""), {
-# #     expect_silent(Report_KRI(
-# #       lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
-# #       strOutpath = "test_output.html"
-# #     ))
-# #   })
-# # })
-#
-# test_that("Verifying rendering of RMarkdown file", {
-#   withr::with_envvar(c("RSTUDIO_PANDOC" = ""), {
-#     expect_message(
-#       Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
-#                  strOutpath = "test_report.html"),
-#       "Output created: test_report.html"
-#     )
-#   })
-# })
+
+test_that("Verifying rendering of RMarkdown file", {
+  withr::with_envvar(c("RSTUDIO_PANDOC" = ""), {
+    expect_message(
+      Report_KRI(lCharts = lCharts, dfSummary = dfSummary, dfStudy = dfStudy, dfSite = dfSite, dfMetrics = dfMetrics,
+                 strOutpath = "test_report.html"),
+      "Output created: test_report.html"
+    )
+  })
+})
