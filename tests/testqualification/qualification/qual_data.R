6bf62cbfcaf76b24c0d3c608cebee9bf21a7a68c
Jacob Anderson <jacob.anderson.science@gmail.com>
2024-06-19 08:43:59 -0700

    update qual specs csv
    - added qual_data for testing
    - removed depricated steps from cou0003 yaml

---
+++ tests/testqualification/qualification/qual_data.R
@@ -0,0 +1,20 @@
+lData <- gsm::UseClindata(
+  list(
+    "dfSUBJ" = "clindata::rawplus_dm",
+    "dfAE" = "clindata::rawplus_ae",
+    "dfPD" = "clindata::ctms_protdev",
+    "dfCONSENT" = "clindata::rawplus_consent",
+    "dfIE" = "clindata::rawplus_ie",
+    "dfLB" = "clindata::rawplus_lb",
+    "dfSTUDCOMP" = "clindata::rawplus_studcomp",
+    "dfSDRGCOMP" = "clindata::rawplus_sdrgcomp %>%
+            dplyr::filter(.data$phase == 'Blinded Study Drug Completion')",
+    "dfDATACHG" = "clindata::edc_data_points",
+    "dfDATAENT" = "clindata::edc_data_pages",
+    "dfQUERY" = "clindata::edc_queries",
+    "dfENROLL" = "clindata::rawplus_enroll"
+  )
+)
+mapping_workflow <- MakeWorkflowList("mapping")
+lData_mapped <- suppressMessages(RunWorkflow(lWorkflow = mapping_workflow, lData = lData))$mapping$lResults
+kri_workflows <- MakeWorkflowList(c(sprintf("kri%04d", 1:12), sprintf("cou%04d", 1:12)))
--- tests/testqualification/qualification/test_qual_T1_1.R
+++ tests/testqualification/qualification/test_qual_T1_1.R
@@ -1,46 +1,18 @@
-test_that("Given an appropriate subset of Adverse Event data, the assessment function correctly performs an Adverse Event Assessment grouped by the Site variable using the Poisson method and correctly assigns Flag variable values when given a custom threshold.", {
-  # gsm analysis
-  # dfInput <- gsm::AE_Map_Raw(dfs = list(
-  #   dfAE = clindata::rawplus_ae %>% filter(aeser_std_nsv == "Y"),
-  #   dfSUBJ = clindata::rawplus_dm
-  # ))
-  #
-  # test1_1 <- AE_Assess(
-  #   dfInput = dfInput,
-  #   strMethod = "Poisson",
-  #   vThreshold = c(-3, -1, 1, 3)
-  # )
-  #
-  # # Double Programming
-  # t1_input <- dfInput
-  #
-  # t1_transformed <- dfInput %>%
-  #   qualification_transform_counts()
-  #
-  # t1_analyzed <- t1_transformed %>%
-  #   qualification_analyze_poisson()
-  #
-  # class(t1_analyzed) <- c("tbl_df", "tbl", "data.frame")
-  #
-  # t1_flagged <- t1_analyzed %>%
-  #   qualification_flag_poisson(threshold = c(-3, -1, 1, 3))
-  #
-  # t1_summary <- t1_flagged %>%
-  #   select(GroupID, Numerator, Denominator, Metric, Score, Flag) %>%
-  #   arrange(desc(abs(Metric))) %>%
-  #   arrange(match(Flag, c(2, -2, 1, -1, 0)))
-  #
-  # t1_1 <- list(
-  #   "dfTransformed" = t1_transformed,
-  #   "dfAnalyzed" = t1_analyzed,
-  #   "dfFlagged" = t1_flagged,
-  #   "dfSummary" = t1_summary
-  # )
-  #
-  # # remove metadata that is not part of qualification
-  # test1_1$lData$dfConfig <- NULL
-  #
-  # # compare results
-  # # remove bounds dataframe for now
-  # expect_equal(test1_1$lData[names(test1_1$lData) != "dfBounds"], t1_1)
+test_that("Given appropriate data arguments, correctly derive rate.", {
+  source(system.file("tests", "testqualification", "qualification", "qual_data.R", package = "gsm"))
+
+  Input_Rate_Inputs <- map_df(kri_workflows, function(kri){
+    reshape2::melt(kri$steps[map_lgl(kri$steps, ~.x$output == "dfAnalyzed")][[1]]$name)
+      }, .id = "workflow")
+
+  Input_Rate_Inputs <- map(kri_workflows, function(kri){
+    kri$steps[map_lgl(kri$steps, ~.x$name == "Input_Rate")][[1]]$params
+  })
+
+  Input_Rate_Results <- map(kri_workflows, function(kri){
+    suppressMessages(
+      RunStep(kri$steps[kri$steps$name == "Input_Rate"], lData = lData_mapped, lMeta = kri$meta)
+    )
+  })
+
 })
