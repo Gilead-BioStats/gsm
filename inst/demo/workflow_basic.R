5bb0293843f77c118c6dd3ac708c5435a29552f0
= <laura.maxwell@atorusresearch.com>
2024-06-20 17:16:50 -0400

    add demo scripts

---
+++ inst/demo/workflow_basic.R
@@ -0,0 +1,19 @@
+# AE KRI Workflow
+
+dfInput <- Input_Rate(
+  dfSubjects= clindata::rawplus_dm,
+  dfNumerator= clindata::rawplus_ae,
+  dfDenominator = clindata::rawplus_dm,
+  strSubjectCol = "subjid",
+  strGroupCol = "siteid",
+  strNumeratorMethod= "Count",
+  strDenominatorMethod= "Sum",
+  strDenominatorCol= "timeonstudy"
+)
+
+dfTransformed <- Transform_Rate(dfInput)
+dfAnalyzed <- Analyze_NormalApprox(dfTransformed, strType = "rate")
+dfFlagged <- Flag_NormalApprox(dfAnalyzed, vThreshold = c(-3,-2,2,3))
+dfSummarized <- Summarize(dfFlagged)
+table(dfSummarized$Flag)
+
