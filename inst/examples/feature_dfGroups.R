d17d736606d47eba49330fed87741363f496aeab
Jeremy Wildfire <jwildfire@gmail.com>
2024-06-26 08:47:08 -0400

    add new dfGroups workflow #1597

--- inst/examples/feature_dfGroups.R
+++ inst/examples/feature_dfGroups.R
@@ -1,24 +1,17 @@
 devtools::load_all()
-# Raw data
-lRaw <- gsm::UseClindata(
-  list(
-    "dfSUBJ" = "clindata::rawplus_dm",
-    "dfAE" = "clindata::rawplus_ae",
-    "dfPD" = "clindata::ctms_protdev",
-    "dfLB" = "clindata::rawplus_lb",
-    "dfSTUDCOMP" = "clindata::rawplus_studcomp",
-    "dfSDRGCOMP" = "clindata::rawplus_sdrgcomp %>%
-      dplyr::filter(.data$phase == 'Blinded Study Drug Completion')",
-    "dfDATACHG" = "clindata::edc_data_points",
-    "dfDATAENT" = "clindata::edc_data_pages",
-    "dfQUERY" = "clindata::edc_queries",
-    "dfENROLL" = "clindata::rawplus_enroll"
-  )
+
+wf_ctms <- MakeWorkflowList(strNames = "reporting_groups")[[1]]
+
+dfEnrolled <- clindata::rawplus_dm %>% dplyr::filter(enrollyn=="Y")
+
+lData <- list(
+    ctms_site = clindata::ctms_site, 
+    ctms_study = clindata::ctms_study,
+    dfEnrolled = dfEnrolled
 )
 
-# Prepare Workflows
-wf_mapping <- MakeWorkflowList(strNames = "mapping")[[1]]
-wf_groups <- MakeWorkflowList(strPath = '/Users/jwildfire/github/gsm/inst/workflow')[[1]]
+lGroups <- RunWorkflow(lWorkflow = wf_ctms, lData = lData)
 
-lMapped <- RunWorkflow(lWorkflow = wf_mapping, lData = lRaw)$lData
-lCounts <- RunWorkflow(lWorkflow = wf_groups, lData = lMapped)
+dfGroups <- lGroups$lData$dfGroups
+head(dfGroups)
+table(paste(dfGroups$GroupLevel, dfGroups$Param))
