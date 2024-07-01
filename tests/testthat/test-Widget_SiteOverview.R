434b77120a44727eb29f9c9d9a4c0bae4f48deed
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-01 15:24:02 -0500

    Update/clean tests.

--- tests/testthat/test-Widget_SiteOverview.R
+++ tests/testthat/test-Widget_SiteOverview.R
@@ -1,17 +1,19 @@
-# Start testing
 test_that("Widget_SiteOverview processes data correctly", {
-  source(testthat::test_path("testdata", "create_simple_data.R"), local = TRUE)
+  source(test_path("testdata", "create_simple_data.R"), local = TRUE)
   widget <- Widget_SiteOverview(dfSummary, lConfig, dfSite, dfWorkflow)
+  expect_s3_class(widget, c("WidgetSiteOverview", "htmlwidget"))
 
-  # Test that dfSummary is converted to lowercase
-  expect_true(all(names(fromJSON(widget$x$dfSummary)) == c("StudyID", "GroupID", "Metric")))
+  expect_named(
+    fromJSON(widget$x$dfSummary),
+    c("StudyID", "GroupID", "Metric")
+  )
 
-  expect_true(class(fromJSON(widget$x$dfGroups)) == "data.frame")
-  expect_true(class(fromJSON(widget$x$strGroupSubset)) == "data.frame")
+  expect_s3_class(fromJSON(widget$x$dfGroups), "data.frame")
+  expect_s3_class(fromJSON(widget$x$strGroupSubset), "data.frame")
 })
 
 # test_that("Widget_SiteOverview generates unique elementId", {
-#   source(testthat::test_path("testdata", "create_simple_data.R"), local = TRUE)
+#   source(test_path("testdata", "create_simple_data.R"), local = TRUE)
 #   widget1 <- Widget_SiteOverview(dfSummary, lConfig, dfSite, dfWorkflow, elementId = "siteOverview")
 #   widget2 <- Widget_SiteOverview(dfSummary, lConfig, dfSite, dfWorkflow, elementId = "siteOverview")
 #
@@ -19,7 +21,7 @@ test_that("Widget_SiteOverview processes data correctly", {
 # })
 
 test_that("Widget_SiteOverview creates a valid HTML widget", {
-  source(testthat::test_path("testdata", "create_simple_data.R"), local = TRUE)
+  source(test_path("testdata", "create_simple_data.R"), local = TRUE)
   widget <- Widget_SiteOverview(dfSummary, lConfig, dfSite, dfWorkflow)
 
   # Check if the widget is of class htmlwidget
