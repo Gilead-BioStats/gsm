c54a01bae66773eaf813bd81d92ea335b4ae6683
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-09 17:21:08 -0500

    Refactor Report_FlagOverTime() for readability.

--- R/Report_FlagOverTime.R
+++ R/Report_FlagOverTime.R
@@ -5,10 +5,28 @@
 #
 # @return An object of class `gt_tbl`.
 
-FlagOverTime <- function(dfSummary, dfMetrics) {
+Report_FlagOverTime <- function(dfSummary, dfMetrics) {
+  dfFlagOverTime <- .widen_summary(dfSummary, dfMetrics)
+  date_cols <- stringr::str_which(
+    colnames(dfFlagOverTime),
+    r"(\d{4}-\d{2}-\d{2})"
+  )
+
   rlang::check_installed("gt")
-  # Create a table that shows flags over time for each site/KRI combination
-  dfFlagOverTime <- dfSummary %>%
+  dfFlagOverTime %>%
+    dplyr::group_by(.data$GroupLevel, .data$GroupID) %>%
+    gt::gt() %>%
+    fmt_sign_rag(columns = date_cols) %>%
+    gt::tab_spanner(label = "Flag", columns = date_cols) %>%
+    gt::tab_header(
+      title = "Flag Over Time",
+      subtitle = "Flags over time for each site/KRI combination"
+    ) %>%
+    gt::opt_vertical_padding(0.5)
+}
+
+.widen_summary <- function(dfSummary, dfMetrics) {
+  dfSummary %>%
     dplyr::left_join(dfMetrics, by = "MetricID") %>%
     dplyr::select(
       "GroupID",
@@ -19,45 +37,52 @@ FlagOverTime <- function(dfSummary, dfMetrics) {
       "Flag"
     ) %>%
     tidyr::pivot_wider(names_from = "snapshot_date", values_from = "Flag")
+}
 
-    ncol <- dim(dfFlagOverTime)[2]
+fmt_sign_rag <- function(data,
+                         columns = gt::everything(),
+                         rows = gt::everything()) {
+  data_color_rag(data, columns = columns, rows = rows) %>%
+    fmt_sign(columns = columns, rows = rows)
+}
 
-  # Create a  table with flags over time for each site/KRI combination
-  gt_table <- dfFlagOverTime %>%
-    dplyr::group_by(GroupLevel, GroupID) %>%
-    gt::gt() %>%
-    gt::data_color(
-        method = "numeric",
-        palette =  c("#000000","#FF0000", "#FFA500", "#008000", "#FFA500", "#FF0000","#000000"),
-        domain = c(-2.5,-1.5,-0.5,0.5,1.5,2.5)
-    ) %>%
-    gt::text_transform(
-        locations = gt::cells_body(columns = 5:ncol),
-        fn = function(x) {
-            dplyr::case_when(
-                x == -2 ~ "-",
-                x == -1 ~ "-",
-                x == 0 ~ "",
-                x == 1 ~ "+",
-                x == 2 ~ "+",
-                TRUE ~ ""
-            )
-        }
-    ) %>%
-    gt::cols_align(
-        columns = 5:ncol,
-        align = "center"
-    ) %>%
-    gt::opt_vertical_padding(0.5) %>%
-    gt::opt_vertical_padding(0.5) %>%
-    gt::tab_header(
-        title = "Flag Over Time",
-        subtitle = "Flags over time for each site/KRI combination"
-    ) %>%
-    gt::tab_spanner(
-        label = "Flag",
-        columns = 5:ncol
-    )
+data_color_rag <- function(data,
+                           columns = gt::everything(),
+                           rows = gt::everything()) {
+  gt::data_color(
+    data,
+    columns = columns,
+    rows = rows,
+    fn = .n_to_rag
+  )
+}
 
-  return(gt_table)
+fmt_sign <- function(data,
+                     columns = gt::everything(),
+                     rows = gt::everything()) {
+  gt::fmt(
+    data,
+    columns = columns,
+    rows = rows,
+    compat = c("numeric", "integer"),
+    fns = .n_to_sign
+  ) %>%
+    gt::cols_align(align = "center", columns = columns)
+}
+
+.n_to_sign <- function(x) {
+  dplyr::case_when(
+    x < 0 ~ "-",
+    x > 0 ~ "+",
+    TRUE ~ ""
+  )
+}
+
+.n_to_rag <- function(x) {
+  dplyr::case_when(
+    is.na(x) ~ "#808080",
+    abs(x) == 2 ~ "#A52A2A",
+    abs(x) == 1 ~ "#FFA500",
+    TRUE ~ "#008000"
+  )
 }
