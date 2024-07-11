46c103246ed48ac53d88d7144036f254415bcca1
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-11 11:39:50 -0500

    Readability tweaks.

--- inst/examples/MissingVisits.R
+++ inst/examples/MissingVisits.R
@@ -1,36 +1,62 @@
-# This example demonstrates how to calculate a Key Risk Indicator (KRI) for missing visits.
+# This example demonstrates how to calculate a Key Risk Indicator (KRI) for
+# missing visits.
 
 # ----- Simulate a data set with participant-level visit status ----#
-# Simulate a data set with participant-level visit status
 set.seed(123)
 Visit <- c("V1", "V2", "V3", "V4", "V5")
 sites <- seq(1,20)
 n <- 1000
 
-dfSubjects <- data.frame(SubjID = 1:n, SiteID = sample(sites, n, replace = TRUE))
+dfSubjects <- data.frame(
+  SubjID = 1:n,
+  SiteID = sample(sites, n, replace = TRUE)
+)
 dfVisits <- tidyr::crossing(dfSubjects, Visit)
-dfVisits$Status <- sample(c("Attended", "Missed"), nrow(dfVisits), prob=c(0.5,0.5), replace = TRUE)
-dfVisits$Status[dfVisits$SiteID == 1] <- sample(c("Attended", "Missed"), length(dfVisits$Status[dfVisits$SiteID == 1]), prob=c(0.9,0.1), replace = TRUE)
-dfVisits$Status[dfVisits$SiteID == 2] <- sample(c("Attended", "Missed"), length(dfVisits$Status[dfVisits$SiteID == 2]), prob=c(0.8,0.2),replace = TRUE)
-dfVisits$Status[dfVisits$SiteID == 3] <- sample(c("Attended", "Missed"), length(dfVisits$Status[dfVisits$SiteID == 3]), prob=c(0,1),replace = TRUE)
+dfVisits$Status <- sample(
+  c("Attended", "Missed"),
+  nrow(dfVisits),
+  prob = c(0.5,0.5),
+  replace = TRUE
+)
+dfVisits$Status[dfVisits$SiteID == 1] <- sample(
+  c("Attended", "Missed"),
+  length(dfVisits$Status[dfVisits$SiteID == 1]),
+  prob = c(0.9,0.1),
+  replace = TRUE
+)
+dfVisits$Status[dfVisits$SiteID == 2] <- sample(
+  c("Attended", "Missed"),
+  length(dfVisits$Status[dfVisits$SiteID == 2]),
+  prob = c(0.8,0.2),
+  replace = TRUE
+)
+dfVisits$Status[dfVisits$SiteID == 3] <- sample(
+  c("Attended", "Missed"),
+  length(dfVisits$Status[dfVisits$SiteID == 3]),
+  prob = c(0,1),
+  replace = TRUE
+)
 
 dfVisits$VisitOccurred <- ifelse(dfVisits$Status == "Attended", 1, 0)
 
 # ----- Generate a KRI for missing visits -----
 dfInput <- Input_Rate(
-  dfSubjects= dfSubjects,
-  dfNumerator= dfVisits,
+  dfSubjects = dfSubjects,
+  dfNumerator = dfVisits,
   dfDenominator = dfVisits,
   strSubjectCol = "SubjID",
   strGroupCol = "SiteID",
-  strNumeratorCol= "VisitOccurred",
-  strNumeratorMethod="Sum",
-  strDenominatorMethod= "Count"
+  strNumeratorCol = "VisitOccurred",
+  strNumeratorMethod = "Sum",
+  strDenominatorMethod = "Count"
 )
 
 dfTransformed <- Transform_Rate(dfInput)
 dfAnalyzed <- Analyze_NormalApprox(dfTransformed, strType = "rate")
-dfBounds <- Analyze_NormalApprox_PredictBounds(dfTransformed = dfTransformed,vThreshold = c(-2,-1,1,2))
+dfBounds <- Analyze_NormalApprox_PredictBounds(
+  dfTransformed = dfTransformed,
+  vThreshold = c(-2,-1,1,2)
+)
 dfFlagged <- Flag_NormalApprox(dfAnalyzed, vThreshold = c(-2,-1,1,2))
 dfSummary <- Summarize(dfFlagged)
 
@@ -38,22 +64,32 @@ table(dfSummary$Flag)
 
 # ----- Generate a scatter plot for the KRI -----
 lMetric <- list(
-    MetricID = "kri0001", 
-    Metric = "Missing Visits", 
-    MetricType = "Rate", 
-    Numerator = "Visits Occurred", 
-    Denominator = "Visits Expected", 
-    Score= "Adjusted Z-Score",
-    Group = "Site", 
-    Description = "KRI for missing visits", 
-    BoundsType = "NormalApprox", 
+    MetricID = "kri0001",
+    Metric = "Missing Visits",
+    MetricType = "Rate",
+    Numerator = "Visits Occurred",
+    Denominator = "Visits Expected",
+    Score = "Adjusted Z-Score",
+    Group = "Site",
+    Description = "KRI for missing visits",
+    BoundsType = "NormalApprox",
     strThreshold = "-2,-1,1,2"
 )
 
-Widget_ScatterPlot(dfSummary = dfSummary, lMetric = lMetric, dfBounds = dfBounds)
+Widget_ScatterPlot(
+  dfSummary = dfSummary,
+  lMetric = lMetric,
+  dfBounds = dfBounds
+)
 
 # ----- Run KRI via a workflow ----
-workflow <- MakeWorkflowList(strPath ="/Users/jwildfire/github/gsm/inst/examples", strNames = "MissingVisitsKRI")
-result <- RunWorkflows(lWorkflow = workflow, lData = list(dfSubjects = dfSubjects,dfVisits = dfVisits))
-Widget_ScatterPlot(dfSummary = result$dfSummary,  dfBounds = result$dfBounds, lMetric = workflow$MissingVisitsKRI$meta)
-
+workflow <- MakeWorkflowList(strNames = "MissingVisitsKRI")
+result <- RunWorkflows(
+  lWorkflows = workflow,
+  lData = list(dfSubjects = dfSubjects,dfVisits = dfVisits)
+)
+Widget_ScatterPlot(
+  dfSummary = result$dfSummary,
+  dfBounds = result$dfBounds,
+  lMetric = workflow$MissingVisitsKRI$meta
+)
