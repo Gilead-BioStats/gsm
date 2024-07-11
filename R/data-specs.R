9a6c9553c2e933a5d58239a98c7d3a141ba5ab63
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-11 10:16:37 -0500

    Document sampleMetrics.
    I also cleaned up the vignette a bit more. The column `nMinDenominator` in sampleMetrics isn't mentioned in the vignette. Should it be? dfMetrics is also missing information in the bulleted list describing it (in both instances).

--- R/data-specs.R
+++ R/data-specs.R
@@ -83,34 +83,6 @@ example_groups <- function() {
     )
 }
 
-#' sampleInput Dataset
-#'
-#' @format A data frame with 25 rows and 5 columns:
-#' \describe{
-#'   \item{SubjectID}{unique subject identifier}
-#'   \item{GroupID}{grouping variable}
-#'   \item{GroupLevel}{level of grouping variable}
-#'   \item{Numerator}{number of flags in group}
-#'   \item{Denominator}{total subjects in group}
-#' }
-#' @source Generated from `sampleInput.csv` dataset in the `gsm` package.
-"sampleInput"
-
-#' sampleSummary Dataset
-#'
-#' @format A data frame with 2126 rows and 7 columns:
-#' \describe{
-#'   \item{GroupID}{grouping variable}
-#'   \item{GroupLevel}{level of grouping variable}
-#'   \item{Numerator}{number of flags in group}
-#'   \item{Denominator}{total subjects in group}
-#'   \item{Metric}{The calculated rate/metric value}
-#'   \item{Score}{The statistical score}
-#'   \item{Flag}{The ordinal flag to be applied}
-#' }
-#' @source Generated from `sampleSummary.csv` dataset in the `gsm` package.
-"sampleSummary"
-
 #' sampleBounds Dataset
 #'
 #' @format A data frame with 12667 rows and 8 columns:
@@ -126,6 +98,49 @@ example_groups <- function() {
 #' }
 #' @source Generated from `sampleBounds.csv` dataset in the `gsm` package.
 "sampleBounds"
+
+#' sampleGroups Dataset
+#'
+#' @format A data frame with 3903 rows and 4 columns:
+#' \describe{
+#'   \item{GroupID}{grouping variable}
+#'   \item{GroupLevel}{level of grouping variable}
+#'   \item{Param}{Parameter of interest for the group}
+#'   \item{Value}{Value of specified Param}
+#' }
+#' @source Generated from `sampleGroups.csv` dataset in the `gsm` package.
+"sampleGroups"
+
+#' sampleInput Dataset
+#'
+#' @format A data frame with 25 rows and 5 columns:
+#' \describe{
+#'   \item{SubjectID}{unique subject identifier}
+#'   \item{GroupID}{grouping variable}
+#'   \item{GroupLevel}{level of grouping variable}
+#'   \item{Numerator}{number of flags in group}
+#'   \item{Denominator}{total subjects in group}
+#' }
+#' @source Generated from `sampleInput.csv` dataset in the `gsm` package.
+"sampleInput"
+
+#' sampleMetrics Dataset
+#'
+#' @format A data frame with 12 rows and 11 columns:
+#' \describe{
+#'   \item{MetricId}{unique metric identifier}
+#'   \item{File}{yaml file for workflow}
+#'   \item{GroupLevel}{level of grouping variable}
+#'   \item{Abbreviation}{abbreviation for the metric}
+#'   \item{Numerator}{data source for the numerator}
+#'   \item{Denominator}{data source for the denominator}
+#'   \item{Model}{model used to calculate metric}
+#'   \item{Score}{type of score reported}
+#'   \item{strThreshold}{thresholds to be used for bounds and flags}
+#'   \item{nMinDenominator}{minimum denominator required to return a score and calculate a flag}
+#' }
+#' @source Generated from `sampleMetrics.csv` dataset in the `gsm` package.
+"sampleMetrics"
 
 #' sampleResults Dataset
 #'
@@ -145,14 +160,17 @@ example_groups <- function() {
 #' @source Generated from `sampleResults.csv` dataset in the `gsm` package.
 "sampleResults"
 
-#' sampleGroups Dataset
+#' sampleSummary Dataset
 #'
-#' @format A data frame with 3903 rows and 4 columns:
+#' @format A data frame with 2126 rows and 7 columns:
 #' \describe{
 #'   \item{GroupID}{grouping variable}
 #'   \item{GroupLevel}{level of grouping variable}
-#'   \item{Param}{Parameter of interest for the group}
-#'   \item{Value}{Value of specified Param}
+#'   \item{Numerator}{number of flags in group}
+#'   \item{Denominator}{total subjects in group}
+#'   \item{Metric}{The calculated rate/metric value}
+#'   \item{Score}{The statistical score}
+#'   \item{Flag}{The ordinal flag to be applied}
 #' }
-#' @source Generated from `sampleGroups.csv` dataset in the `gsm` package.
-"sampleGroups"
+#' @source Generated from `sampleSummary.csv` dataset in the `gsm` package.
+"sampleSummary"
