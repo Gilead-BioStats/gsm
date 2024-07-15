f685215c2745f55c67ce7d15d77ae40351e2b36f
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-15 14:11:10 -0500

    Fix spacing in Report_MetricTable

--- R/Report_MetricTable.R
+++ R/Report_MetricTable.R
@@ -13,80 +13,76 @@
 #' @return A datatable containing the summary table
 #'
 #' @export
-
-Report_MetricTable <- function(
-    dfResults,
-    dfGroups,
-    strSnapshotDate = NULL,
-    strGroupLevel = "Site",
-    strGroupDetailsParams = NULL
-) {
-
-    # Check for multiple snapshots --------------------------------------------
-    # use most recent snapshot date if strSnapshotDate is missing
-    if(is.null(strSnapshotDate)){
-        if ("SnapshotDate" %in% colnames(dfResults) & nrow(dfResults) > 0) {
-            strSnapshotDate <- max(dfResults$SnapshotDate)
-        } else if (!"SnapshotDate" %in% colnames(dfResults) & nrow(dfResults) > 0){
-            strSnapshotDate <- as.Date(Sys.Date())
-            dfResults$SnapshotDate <- strSnapshotDate
-        }
-    } else {
-        strSnapshotDate <- as.Date(strSnapshotDate)
+Report_MetricTable <- function(dfResults,
+                               dfGroups,
+                               strSnapshotDate = NULL,
+                               strGroupLevel = "Site",
+                               strGroupDetailsParams = NULL) {
+  # Check for multiple snapshots --------------------------------------------
+  # use most recent snapshot date if strSnapshotDate is missing
+  if (is.null(strSnapshotDate)) {
+    if ("SnapshotDate" %in% colnames(dfResults) & nrow(dfResults) > 0) {
+      strSnapshotDate <- max(dfResults$SnapshotDate)
+    } else if (!"SnapshotDate" %in% colnames(dfResults) & nrow(dfResults) > 0) {
+      strSnapshotDate <- as.Date(Sys.Date())
+      dfResults$SnapshotDate <- strSnapshotDate
     }
+  } else {
+    strSnapshotDate <- as.Date(strSnapshotDate)
+  }
 
-    if(nrow(dfResults) > 0){
-        dfResults <- dfResults %>% filter(.data$SnapshotDate == strSnapshotDate)
+  if (nrow(dfResults) > 0) {
+    dfResults <- dfResults %>% filter(.data$SnapshotDate == strSnapshotDate)
+  }
+
+  # Add Group Metadata ------------------------------------------------------------
+  if (is.null(strGroupDetailsParams)) {
+    if (strGroupLevel == "Site") {
+      strGroupDetailsParams <- c("Country", "Status", "InvestigatorLastName", "ParticipantCount")
+    } else if (strGroupLevel == "Country") {
+      strGroupDetailsParams <- c("SiteCount","ParticipantCount")
     }
+  }
 
-    # Add Group Metadata ------------------------------------------------------------
-    if(is.null(strGroupDetailsParams)){
-        if(strGroupLevel == "Site"){
-            strGroupDetailsParams <- c("Country", "Status", "InvestigatorLastName", "ParticipantCount")
-        } else if(strGroupLevel == "Country"){
-            strGroupDetailsParams <- c("SiteCount","ParticipantCount")
-        }
-    }
+  dfGroups_wide <- dfGroups %>%
+    filter(.data$GroupLevel == strGroupLevel) %>%
+    filter(.data$Param %in% strGroupDetailsParams) %>%
+    pivot_wider(names_from = "Param", values_from = "Value") %>%
+    select(-"GroupLevel")
 
-    dfGroups_wide <- dfGroups %>%
-        filter(.data$GroupLevel == strGroupLevel) %>%
-        filter(.data$Param %in% strGroupDetailsParams) %>%
-        pivot_wider(names_from="Param", values_from="Value") %>%
-        select(-"GroupLevel")
+  if (nrow(dfResults) > 0 & nrow(dfGroups_wide) > 0) {
+    dfResults <- dfResults %>% left_join(dfGroups_wide, by = c("GroupID" = "GroupID"))
+  }
 
-    if(nrow(dfResults) > 0 & nrow(dfGroups_wide) > 0){
-        dfResults <- dfResults %>% left_join(dfGroups_wide, by = c("GroupID" = "GroupID"))
-    }
+  # Select Flagged metrics and format table -----------------------------------
+  if (nrow(dfResults) > 0 & any(c(-2, -1, 1, 2) %in% unique(dfResults$Flag))) {
+    SummaryTable <- dfResults %>%
+      filter(.data$Flag != 0) %>%
+      arrange(desc(abs(.data$Score))) %>%
+      mutate(
+        'Flag' = map(.data$Flag, Report_FormatFlag),
+        across(
+          where(is.numeric),
+          ~ round(.x, 3)
+        )
+      ) %>%
+      select(
+        any_of(c(
+          "Site" = "GroupID",
+          "Country",
+          "Status",
+          "PI" = "InvestigatorLastName",
+          "Subjects" = "ParticipantCount"
+        )),
+        everything()
+      ) %>%
+      select(-'MetricID') %>%
+      kableExtra::kbl(format = "html", escape = FALSE) %>%
+      kableExtra::kable_styling("striped", full_width = FALSE)
 
-    # Select Flagged metrics and format table -----------------------------------
-    if (nrow(dfResults) > 0 & any(c(-2, -1, 1, 2) %in% unique(dfResults$Flag))) {
-        SummaryTable <- dfResults %>%
-            filter(.data$Flag != 0) %>%
-            arrange(desc(abs(.data$Score))) %>%
-            mutate(
-                'Flag' = map(.data$Flag, Report_FormatFlag),
-                across(
-                    where(is.numeric),
-                    ~ round(.x, 3)
-                )
-            ) %>%
-            select(
-                any_of(c(
-                    "Site" = "GroupID",
-                    "Country",
-                    "Status",
-                    "PI" = "InvestigatorLastName",
-                    "Subjects" = "ParticipantCount"
-                )),
-                everything()
-            ) %>%
-            select(-'MetricID') %>%
-            kableExtra::kbl(format="html", escape=FALSE) %>%
-            kableExtra::kable_styling("striped", full_width = FALSE)
+  } else {
+    SummaryTable <- "Nothing flagged for this KRI."
+  }
 
-    } else {
-        SummaryTable<- "Nothing flagged for this KRI."
-    }
-
-    return(SummaryTable)
+  return(SummaryTable)
 }
