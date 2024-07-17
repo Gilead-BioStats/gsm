8e2284706bb3431ab1f29d8efcbe7ffa2097443a
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-17 08:25:19 -0500

    Use analyticsInput in Transform_Rate tests.

--- tests/testthat/_snaps/Transform_Rate.md
+++ tests/testthat/_snaps/Transform_Rate.md
@@ -3,16 +3,13 @@
     Code
       row_removed
     Output
-      # A tibble: 9 x 5
-        GroupID GroupLevel Numerator Denominator  Metric
-        <chr>   <chr>          <dbl>       <dbl>   <dbl>
-      1 139     site               5         901 0.00555
-      2 143     site               3         170 0.0176 
-      3 162     site               3         370 0.00811
-      4 167     site               3         360 0.00833
-      5 173     site               6         680 0.00882
-      6 189     site               4         815 0.00491
-      7 29      site               2         450 0.00444
-      8 58      site               1         225 0.00444
-      9 78      site               2          50 0.04   
+      # A tibble: 6 x 5
+        GroupID GroupLevel Numerator Denominator Metric
+        <chr>   <chr>          <int>       <dbl>  <dbl>
+      1 G2      Site              34         372 0.0914
+      2 G3      Site              15         190 0.0789
+      3 G4      Site               8         131 0.0611
+      4 G5      Site              17         115 0.148 
+      5 G6      Site              23         244 0.0943
+      6 G7      Site              20         259 0.0772
 
--- tests/testthat/test-Transform_Rate.R
+++ tests/testthat/test-Transform_Rate.R
@@ -1,120 +1,88 @@
 test_that("output is created as expected", {
-  input <- tibble::tribble(
-    ~SubjectID, ~GroupID, ~GroupLevel, ~StudyID, ~CountryID, ~CustomGroupID, ~Exposure, ~Count, ~Rate,
-    "0496", "5", "site", "AA-AA-000-0000", "US", "0X167", 730, 5, 5/720,
-    "1350", "78", "site", "AA-AA-000-0000", "US", "0X002", 50, 2, 2/50,
-    "0539", "139", "site", "AA-AA-000-0000", "US", "0X052", 901, 5, 5/901,
-    "0329", "162", "site", "AA-AA-000-0000", "US", "0X049", 370, 3, 3/370,
-    "0429", "29", "site", "AA-AA-000-0000", "Japan", "0X116", 450, 2, 2/450,
-    "1218", "143", "site", "AA-AA-000-0000", "US", "0X153", 170, 3, 3/170,
-    "0808", "173", "site", "AA-AA-000-0000", "US", "0X124", 680, 6, 6/680,
-    "1314", "189", "site", "AA-AA-000-0000", "US", "0X093", 815, 4, 4/815,
-    "1236", "58", "site", "AA-AA-000-0000", "China", "0X091", 225, 1, 1/225,
-    "0163", "167", "site", "AA-AA-000-0000", "US", "0X059", 360, 3, 3/360
-  )
   dfTransformed <- Transform_Rate(
-    dfInput = input,
-    strNumeratorCol = "Count",
-    strDenominatorCol = "Exposure"
+    dfInput = analyticsInput
   )
 
   expect_true(is.data.frame(dfTransformed))
-  expect_equal(names(dfTransformed), c("GroupID", "GroupLevel", "Numerator", "Denominator", "Metric"))
-  expect_equal(sort(unique(input$GroupID)), sort(dfTransformed$GroupID))
-  expect_equal(length(unique(input$GroupID)), length(unique(dfTransformed$GroupID)))
-  expect_equal(length(unique(input$GroupID)), nrow(dfTransformed))
+  expect_equal(
+    names(dfTransformed),
+    c("GroupID", "GroupLevel", "Numerator", "Denominator", "Metric")
+  )
+  expect_equal(
+    sort(unique(analyticsInput$GroupID)),
+    sort(dfTransformed$GroupID)
+  )
+  expect_equal(
+    length(unique(analyticsInput$GroupID)),
+    length(unique(dfTransformed$GroupID))
+  )
+  expect_equal(
+    length(unique(analyticsInput$GroupID)),
+    nrow(dfTransformed)
+  )
 })
 
 # Count / Exposure
 
 test_that("incorrect inputs throw errors", {
-  input <- tibble::tribble(
-    ~SubjectID, ~GroupID, ~GroupLevel, ~StudyID, ~CountryID, ~CustomGroupID, ~Exposure, ~Count, ~Rate,
-    "0496", "5", "site", "AA-AA-000-0000", "US", "0X167", 730, 5, 5/720,
-    "1350", "78", "site", "AA-AA-000-0000", "US", "0X002", 50, 2, 2/50,
-    "0539", "139", "site", "AA-AA-000-0000", "US", "0X052", 901, 5, 5/901,
-    "0329", "162", "site", "AA-AA-000-0000", "US", "0X049", 370, 3, 3/370,
-    "0429", "29", "site", "AA-AA-000-0000", "Japan", "0X116", 450, 2, 2/450,
-    "1218", "143", "site", "AA-AA-000-0000", "US", "0X153", 170, 3, 3/170,
-    "0808", "173", "site", "AA-AA-000-0000", "US", "0X124", 680, 6, 6/680,
-    "1314", "189", "site", "AA-AA-000-0000", "US", "0X093", 815, 4, 4/815,
-    "1236", "58", "site", "AA-AA-000-0000", "China", "0X091", 225, 1, 1/225,
-    "0163", "167", "site", "AA-AA-000-0000", "US", "0X059", 360, 3, 3/360
+  expect_error(
+    Transform_Rate(list()),
+    "dfInput is not a data frame"
   )
-  expect_error(Transform_Rate(list()))
 
   expect_error(
     Transform_Rate(
-      dfInput = input %>% mutate(Count = as.character(Count)),
-      strNumeratorCol = "Count",
-      strDenominatorCol = "Exposure"
+      dfInput = analyticsInput %>%
+        dplyr::mutate(Numerator = as.character(Numerator))
     ),
     "strNumeratorColumn is not numeric"
   )
 
   expect_error(
     Transform_Rate(
-      dfInput = input %>% mutate(Exposure = as.character(Exposure)),
-      strNumeratorCol = "Count",
-      strDenominatorCol = "Exposure"
+      dfInput = analyticsInput %>%
+        dplyr::mutate(Denominator = as.character(Denominator))
     ),
     "strDenominatorColumn is not numeric"
   )
 
   expect_error(
     Transform_Rate(
-      dfInput = input %>% mutate(Count = ifelse(row_number() == 1, NA, Count)),
-      strNumeratorCol = "Count",
-      strDenominatorCol = "Exposure"
+      dfInput = analyticsInput %>%
+        dplyr::mutate(Numerator = ifelse(row_number() == 1, NA, Numerator))
     ),
     "NA's found in numerator"
   )
 
   expect_error(
     Transform_Rate(
-      dfInput = input %>% mutate(Exposure = ifelse(row_number() == 1, NA, Exposure)),
-      strNumeratorCol = "Count",
-      strDenominatorCol = "Exposure"
+      dfInput = analyticsInput %>%
+        dplyr::mutate(Denominator = ifelse(row_number() == 1, NA, Denominator))
     ),
     "NA's found in denominator"
   )
 
   expect_error(
     Transform_Rate(
-      dfInput = input %>% select(-GroupID),
-      strNumeratorCol = "Count",
-      strDenominatorCol = "Exposure"
+      dfInput = analyticsInput %>% dplyr::select(-"GroupID")
     ),
     "Required columns not found in input data"
   )
 })
 
 test_that("rows with a denominator of 0 are removed", {
-  input <- tibble::tribble(
-    ~SubjectID, ~GroupID, ~GroupLevel, ~StudyID, ~CountryID, ~CustomGroupID, ~Exposure, ~Count, ~Rate,
-    "0496", "5", "site", "AA-AA-000-0000", "US", "0X167", 730, 5, 5/720,
-    "1350", "78", "site", "AA-AA-000-0000", "US", "0X002", 50, 2, 2/50,
-    "0539", "139", "site", "AA-AA-000-0000", "US", "0X052", 901, 5, 5/901,
-    "0329", "162", "site", "AA-AA-000-0000", "US", "0X049", 370, 3, 3/370,
-    "0429", "29", "site", "AA-AA-000-0000", "Japan", "0X116", 450, 2, 2/450,
-    "1218", "143", "site", "AA-AA-000-0000", "US", "0X153", 170, 3, 3/170,
-    "0808", "173", "site", "AA-AA-000-0000", "US", "0X124", 680, 6, 6/680,
-    "1314", "189", "site", "AA-AA-000-0000", "US", "0X093", 815, 4, 4/815,
-    "1236", "58", "site", "AA-AA-000-0000", "China", "0X091", 225, 1, 1/225,
-    "0163", "167", "site", "AA-AA-000-0000", "US", "0X059", 360, 3, 3/360
-  )
-  testInput <- input %>%
-    group_by(GroupID) %>%
-    mutate(
-      Exposure = ifelse(
-        GroupID == input$GroupID[1],
+  testInput <- analyticsInput %>%
+    dplyr::group_by(GroupID) %>%
+    dplyr::mutate(
+      Denominator = ifelse(
+        GroupID == analyticsInput$GroupID[[1]],
         0,
-        Exposure
+        Denominator
       ),
       Rate = ifelse(
-        GroupID == input$GroupID[1],
+        GroupID == analyticsInput$GroupID[[1]],
         NaN,
-        Rate
+        Numerator/Denominator
       )
     ) %>%
     ungroup()
@@ -122,9 +90,7 @@ test_that("rows with a denominator of 0 are removed", {
   expect_warning(
     {
       row_removed <- Transform_Rate(
-        dfInput = testInput,
-        strNumeratorCol = "Count",
-        strDenominatorCol = "Exposure"
+        dfInput = testInput
       )
     },
     class = "gsm_wrn-remove_rows"
