3059bd5bb04bfba91ead7adf23c5c1a69af72a73
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-19 15:23:50 -0500

    Set up for all workflows.

--- tests/test_workflows/main.R
+++ tests/test_workflows/main.R
@@ -1,5 +1,60 @@
 # Add tests here for
 devtools::load_all()
+
+get_workflow_data <- function(strNames) {
+  workflows <- MakeWorkflowList(strNames = strNames)
+  lMapped <- get_mapped_data(workflows)
+  results <- map(
+    workflows,
+    function(this_workflow) {
+      quiet_RunWorkflow(
+        lWorkflow = this_workflow,
+        lData = lMapped,
+        bReturnData = FALSE
+      )
+    }
+  )
+  yaml_outputs <- map(workflows, ~map_vec(.x$steps, ~.x$output))
+  return(
+    list(
+      workflows = workflows,
+      yaml_outputs = yaml_outputs,
+      results = results
+    )
+  )
+}
+
+get_mapped_data <- function(workflows) {
+  wf_mapping <- MakeWorkflowList(strNames = "data_mapping")$data_mapping
+
+  # Don't run things we don't use.
+  used_params <- map(workflows, ~ map(.x$steps, "params")) %>%
+    unlist() %>% unique()
+  wf_mapping$steps <- purrr::keep(
+    wf_mapping$steps,
+    ~ .x$output %in% used_params
+  )
+  lData <- UseClindata(
+    list(
+      "dfSUBJ" = "clindata::rawplus_dm",
+      "dfAE" = "clindata::rawplus_ae",
+      "dfPD" = "clindata::ctms_protdev",
+      "dfCONSENT" = "clindata::rawplus_consent",
+      "dfIE" = "clindata::rawplus_ie",
+      "dfLB" = "clindata::rawplus_lb",
+      "dfSTUDCOMP" = "clindata::rawplus_studcomp",
+      "dfSDRGCOMP" = "clindata::rawplus_sdrgcomp %>%
+            dplyr::filter(.data$phase == 'Blinded Study Drug Completion')",
+      "dfDATACHG" = "clindata::edc_data_points",
+      "dfDATAENT" = "clindata::edc_data_pages",
+      "dfQUERY" = "clindata::edc_queries",
+      "dfENROLL" = "clindata::rawplus_enroll"
+    )
+  )
+  lMapped <- quiet_RunWorkflow(lWorkflow = wf_mapping, lData = lData)
+  return(lMapped)
+}
+
 workflow_test_dir <- "tests/test_workflows"
 workflow_test_files <- grep(
   "^test",
---
+++ tests/test_workflows/test-cou.R
@@ -0,0 +1,40 @@
+# Tests based on tests/testthat/test-RunWorkflow.R. If something breaks
+# here, check whether that has changed.
+
+test_that("cou workflows work", {
+  workflow_data <- get_workflow_data("cou")
+  results <- workflow_data$results
+  workflows <- workflow_data$workflows
+  yaml_outputs <- workflow_data$yaml_outputs
+
+  expect_no_error({
+    purrr::iwalk(
+      workflows,
+      function(this_workflow, this_name) {
+        expect_identical(
+          this_workflow, results[[this_name]][names(this_workflow)]
+        )
+      }
+    )
+  })
+
+  expect_no_error({
+    purrr::iwalk(
+      results,
+      function(this_result, this_name) {
+        expect_setequal(yaml_outputs[[this_name]], names(this_result$lData))
+      }
+    )
+  })
+
+  expect_no_error({
+    purrr::iwalk(
+      yaml_outputs,
+      function(this_output_set, this_name) {
+        expect_true(
+          all(map_int(results[[this_name]]$lData[this_output_set], NROW) > 0)
+        )
+      }
+    )
+  })
+})
