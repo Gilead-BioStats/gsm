bff1dc9db6349315ecb9176212575e5be926d961
Spencer Childress <spencer.c.childress@gmail.com>
2024-07-19 10:15:14 -0400

    clean up bar chart widget

--- inst/htmlwidgets/Widget_BarChart.js
+++ inst/htmlwidgets/Widget_BarChart.js
@@ -10,14 +10,19 @@ HTMLWidgets.widget({
                 // Update y-axis variable.
                 input.lMetric.y = input.strOutcome;
 
-                // add click event listener to chart
+                // Add click event listener to chart.
                 if (input.bAddGroupSelect)
                     input.lMetric.clickCallback = function(d) {
                         instance.data.config.selectedGroupIDs = instance.data.config.selectedGroupIDs.includes(d.GroupID)
                             ? 'None'
                             : d.GroupID;
+
+                        // Update group select.
                         groupSelect.value = instance.data.config.selectedGroupIDs;
+
+                        // Set country select to 'None' if a group ID is selected.
                         countrySelect.value = "None";
+
                         instance.helpers.updateConfig(
                             instance,
                             instance.data.config,
@@ -31,15 +36,11 @@ HTMLWidgets.widget({
                               'site',
                               instance.data.config.selectedGroupIDs
                             )
-                            Shiny.setInputValue(
-                              'country',
-                              instance.data.config.selectedCountryIDs
-                            )
                           }
                         }
                   };
 
-                // generate bar chart
+                // Generate bar chart.
                 const instance = rbmViz.default.barChart(
                     el,
                     input.dfResults,
@@ -48,9 +49,8 @@ HTMLWidgets.widget({
                     input.dfGroups
                 );
 
-                // add dropdown that highlights groups
-                let groupSelect;
-                let countrySelect;
+                // Add dropdown that highlights group IDs.
+                let groupSelect, countrySelect;
                 if (input.bAddGroupSelect) {
                     groupSelect = addGroupSelect(
                         el,
@@ -58,7 +58,13 @@ HTMLWidgets.widget({
                         instance,
                         `Highlighted ${input.lMetric.Group || 'group'}: `
                     );
-                    countrySelect = addCountrySelect(el, input.dfGroups, instance, groupSelect);
+
+                    countrySelect = addCountrySelect(
+                        el,
+                        input.dfGroups,
+                        instance,
+                        groupSelect
+                    );
                 }
             },
             resize: function(width, height) {
