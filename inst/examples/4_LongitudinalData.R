dae513f7f613bab23d63b172f51e4989b0954cd7
Jeremy Wildfire <jwildfire@gmail.com>
2024-07-19 09:54:48 -0400

    add longitudinal example

--- inst/examples/4_LongitudinalData.R
+++ inst/examples/4_LongitudinalData.R
@@ -1,1 +1,14 @@
-#### 4.1 Site-Level KRI Report 
+#### 4.1 Site-Level KRI Report with multiple SnapshotDate
+lCharts <- MakeCharts(
+  dfResults = gsm::reportingResults,
+  dfGroups = gsm::reportingGroups,
+  dfMetrics = gsm::reportingMetrics,
+  dfBounds = gsm::reportingBounds
+)
+
+Report_KRI(
+  lCharts = lCharts,
+  dfResults =  gsm::reportingResults,
+  dfGroups =  gsm::reportingGroups,
+  dfMetrics = gsm::reportingMetrics
+)
