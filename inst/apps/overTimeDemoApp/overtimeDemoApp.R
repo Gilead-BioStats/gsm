4534b828adf7135bc753136a2b9e57f747069617
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-23 14:25:07 -0500

    KRI Over Time Demo App
    Closes #1704.

--- inst/apps/overTimeDemoApp/overtimeDemoApp.R
+++ inst/apps/overTimeDemoApp/overtimeDemoApp.R
@@ -2,35 +2,69 @@ devtools::load_all()
 library(shiny)
 # TODO
 # - Improve layout
-# - Filter the overview table to just the latest snapshot when rendering
 # - Build the flag over time widget and deploy here (#1664)
 # - Run this with data that includes all KRIs (#1703)
 
-
+# Load data
+dfResults <- gsm::reportingResults
+dfMetrics <- gsm::reportingMetrics
+dfGroups <- gsm::reportingGroups
 
 # Make a basic shiny app framework
 ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .gt_table {
+        margin-left: 0 !important;
+      }
+    "))
+  ),
   titlePanel("KRI Dashboard"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("GroupID", "Group", NULL, multiple=TRUE, selectize = FALSE),
-      selectInput("MetricID", "Metric", NULL, multiple=TRUE, selectize = FALSE),
-      selectInput("Dates", "Dates",NULL, multiple=TRUE, selectize = FALSE)
+      selectInput(
+        "GroupID",
+        "Group",
+        unique(dfResults$GroupID),
+        selected = unique(dfResults$GroupID),
+        multiple = TRUE,
+        selectize = FALSE
+      ),
+      selectInput(
+        "MetricID",
+        "Metric",
+        unique(dfResults$MetricID),
+        selected = unique(dfResults$MetricID),
+        multiple = TRUE,
+        selectize = FALSE
+      ),
+      selectInput(
+        "Dates",
+        "Dates",
+        unique(dfResults$SnapshotDate),
+        selected = unique(dfResults$SnapshotDate),
+        multiple = TRUE,
+        selectize = FALSE
+      )
     ),
     mainPanel(
       tabsetPanel(
         tabPanel(
-            "Charts",
+          "Charts",
+          div(
             div(
-                htmltools::div(
-                    style = "table-layout: fixed; width: 100%; overflow: auto; display: inline-block; max-height: 300px;",
-                    uiOutput("FlagOverTime")
-                ),
-                htmltools::div(
-                    style = "table-layout: fixed; width: 100%; overflow: auto; display: inline-block;",
-                    uiOutput("GroupOverview")
-                )
+              style = "overflow: auto; max-height: 400px; margin-bottom: 15px;",
+              uiOutput("FlagOverTime")
+            ),
+            div(
+              style = "overflow: auto;",
+              shiny::div(
+                style = "font-size: 20px; font-family: system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';",
+                "Group Overview"
+              ),
+              uiOutput("GroupOverview")
             )
+          )
         ),
         tabPanel("Data", DT::dataTableOutput("results"))
       )
@@ -39,43 +73,40 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-    dfResults <- gsm::reportingResults
-    dfMetrics <- gsm::reportingMetrics %>% dplyr::filter(MetricID %in% unique(dfResults$MetricID)) 
-    dfGroups <- gsm::reportingGroups
+  rMetrics <- reactive({
+    dfMetrics %>% dplyr::filter(MetricID %in% input$MetricID)
+  })
+  rResults <- reactive({
+    dfResults %>%
+      dplyr::filter(GroupID %in% input$GroupID) %>%
+      dplyr::filter(MetricID %in% input$MetricID) %>%
+      dplyr::filter(SnapshotDate %in% input$Dates)
+  })
+  rResults_Latest <- reactive({
+    rResults() %>%
+      dplyr::filter(SnapshotDate == max(SnapshotDate))
+  })
 
-    # set the values for the selectInput
-    updateSelectInput(session, "GroupID", choices = unique(dfResults$GroupID), selected = unique(dfResults$GroupID))
-    updateSelectInput(session, "MetricID", choices = unique(dfResults$MetricID), selected = unique(dfResults$MetricID))
-    updateSelectInput(session, "Dates", choices = unique(dfResults$SnapshotDate), selected = unique(dfResults$SnapshotDate))
-    rMetrics <- reactive({dfMetrics %>% dplyr::filter(MetricID %in% input$MetricID)})
-    rResults <- reactive({
-    results<-dfResults %>%
-        dplyr::filter(GroupID %in% input$GroupID) %>%
-        dplyr::filter(MetricID %in% input$MetricID) %>% 
-        dplyr::filter(SnapshotDate %in% input$Dates)
-        print(dim(results))
-        return(results)
-    })
+  # Widget_GroupOverview
+  output$GroupOverview <- renderUI({
+    Widget_GroupOverview(
+      dfResults = rResults_Latest(),
+      dfMetrics = rMetrics(),
+      dfGroups = dfGroups,
+      strGroupSubset = "all"
+    )
+  })
 
-    # Widget_GroupOverview
-    output$GroupOverview <- renderUI({
-        gsm::Widget_GroupOverview(
-            dfResults = rResults(),
-            dfMetrics = rMetrics(),
-            dfGroups = dfGroups,
-            strGroupSubset = "all"
-        )
-    })
+  # Report_FlagOverTime
+  output$FlagOverTime <- renderUI({
+    Report_FlagOverTime(
+      dfResults = rResults(),
+      dfMetrics = rMetrics()
+    )
+  })
 
-    # Report_FlagOverTime
-    output$FlagOverTime <- renderUI({
-        gsm::Report_FlagOverTime(
-            dfResults = rResults(),
-            dfMetrics = dfMetrics
-        )
-    })
-
-    output$results <- DT::renderDataTable({rResults()})
+  # Data
+  output$results <- DT::renderDataTable({rResults()})
 }
 
 # Run the shiny app
