1f5800547de0887ba5788fb72660fd5a3dd9cc20
Laura Maxwell <laura.maxwell1@gilead.com>
2024-07-26 10:56:08 -0400

    remove urls
    Co-authored-by: Jon Harmon <jon.harmon@atorusresearch.com>

--- R/Flag_NormalApprox.R
+++ R/Flag_NormalApprox.R
@@ -7,7 +7,7 @@
 #'
 #' @details
 #' This function flags sites based on the funnel plot with normal approximation analysis result as part of
-#' the [GSM data model](https://gilead-biostats.github.io/gsm/articles/DataModel.html).
+#' the GSM data model (see `vignette("DataModel")`).
 #'
 #' @section Data Specification:
 #' \code{Flag_NormalApprox} is designed to support the input data (`dfAnalyzed`) from \code{Analyze_NormalApprox} function.
