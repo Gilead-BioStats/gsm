3b13213e2305f97d77fee36277e78d362337910d
Devin Pastoor <devin.pastoor@gmail.com>
2024-08-04 15:30:09 -0400

    normalize bReturnData and bKeepInputData between RunWorkflows and RunWorkflow

--- R/RunWorkflows.R
+++ R/RunWorkflows.R
@@ -9,8 +9,8 @@
 #'
 #' @param lWorkflows `list` A named list of metadata defining how the workflow should be run.
 #' @param lData `list` A named list of domain-level data frames. Names should match the values specified in `lMapping` and `lAssessments`, which are generally based on the expected inputs from `X_Map_Raw`.
-#' @param bKeepInputData `boolean` should the input data be returned? Default is `FALSE`.
 #' @param bReturnData `boolean` should function return only bData or should meta and steps be included? Default is `TRUE`.
+#' @param bKeepInputData `boolean` should the input data be returned? Default is `FALSE`.
 #'
 #' @return A named list of results, where the names correspond to the names of
 #' the workflows
