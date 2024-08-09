644837b379bb3b93ed2c1afc224a7506a22d4e02
Jeremy Wildfire <jwildfire@gmail.com>
2024-08-09 09:52:04 -0400

    rename combineSpecs

--- R/util-CombineSpecs.R
+++ R/util-CombineSpecs.R
@@ -1,11 +1,11 @@
 #' Combine Specifications
 #'
-#' This function combines a list of specifications into a single specification.
+#' Combine a list of specifications into a single specification.
 #'
 #' @param lSpecs A list of specifications.
 #' @return A list representing the combined specification.
 #' @examples
-#' all_wf <- gsm::MakeWorkflowList()
+#' all_wf <- MakeWorkflowList()
 #' all_specs <- CombineSpecs(all_wf)
 #'
 #' @export
@@ -14,12 +14,8 @@ CombineSpecs <- function(lSpecs){
     all_specs <- list()
     for (spec in lSpecs){
         for (domain in names(spec)){
-            if (domain %in% names(all_specs)){
-                all_specs[[domain]] <- c(all_specs[[domain]], spec[[domain]])
-            } else {
-                all_specs[[domain]] <- spec[[domain]]
-            }
+            all_specs[[domain]] <- c(all_specs[[domain]], spec[[domain]])
         }
     }
-    return(lSpec)
+    return(all_specs)
 }
