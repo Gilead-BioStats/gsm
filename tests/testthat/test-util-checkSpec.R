7f10f03b30495fe07929e75ffc2ae829897b998d
Jon Harmon <jon.harmon@atorusresearch.com>
2024-09-04 11:51:07 -0500

    Repeat yourself in tests.
    It's handy to be able to run tests one at a time.
    
    Also got rid of the noise so we can more easily see failures.

--- tests/testthat/test-util-checkSpec.R
+++ tests/testthat/test-util-checkSpec.R
@@ -1,27 +1,44 @@
-# example lSpec
-lSpec <- list(
-  df1 = list(
-    a = list(required = TRUE),
-    b = list(required = TRUE)
-  ),
-  df2 = list(
-    x = list(required = TRUE),
-    y = list(required = TRUE)
-  )
-)
-
-
 test_that("All data.frames and columns are present", {
+  # example lSpec
+  lSpec <- list(
+    df1 = list(
+      a = list(required = TRUE),
+      b = list(required = TRUE)
+    ),
+    df2 = list(
+      x = list(required = TRUE),
+      y = list(required = TRUE)
+    )
+  )
+
   # Example data
   lData <- list(
     df1 = data.frame(a = 1:3, b = 4:6),
     df2 = data.frame(x = 7:9, y = 10:12)
   )
 
-  expect_message(CheckSpec(lData, lSpec), "All")
+  expect_message(
+    expect_message(expect_message(
+      expect_message(CheckSpec(lData, lSpec), "All 2 data"),
+      "All specified"
+    ), "All specified"),
+    "All 4 required"
+  )
 })
 
 test_that("Missing data.frames trigger an error", {
+  # example lSpec
+  lSpec <- list(
+    df1 = list(
+      a = list(required = TRUE),
+      b = list(required = TRUE)
+    ),
+    df2 = list(
+      x = list(required = TRUE),
+      y = list(required = TRUE)
+    )
+  )
+
   # Example data with one missing data.frame
   lData <- list(
     df1 = data.frame(a = 1:3, b = 4:6)
@@ -34,6 +51,18 @@ test_that("Missing data.frames trigger an error", {
 })
 
 test_that("Missing columns trigger a warning", {
+  # example lSpec
+  lSpec <- list(
+    df1 = list(
+      a = list(required = TRUE),
+      b = list(required = TRUE)
+    ),
+    df2 = list(
+      x = list(required = TRUE),
+      y = list(required = TRUE)
+    )
+  )
+
   # Example data with a missing column
   lData <- list(
     df1 = data.frame(a = 1:3),
@@ -41,12 +70,24 @@ test_that("Missing columns trigger a warning", {
   )
 
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(expect_message(CheckSpec(lData, lSpec), "All 2 data"), "All specified"), "All specified"),
     regexp = "missing columns are: df1\\$b"
   )
 })
 
 test_that("Multiple missing columns are correctly reported", {
+  # example lSpec
+  lSpec <- list(
+    df1 = list(
+      a = list(required = TRUE),
+      b = list(required = TRUE)
+    ),
+    df2 = list(
+      x = list(required = TRUE),
+      y = list(required = TRUE)
+    )
+  )
+
   # Example data with multiple missing columns
   lData <- list(
     df1 = data.frame(a = 1:3),
@@ -54,7 +95,7 @@ test_that("Multiple missing columns are correctly reported", {
   )
 
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(expect_message(CheckSpec(lData, lSpec), "All 2 data"), "All specified"), "All specified"),
     regexp = "missing columns are: df1\\$b and df2\\$y"
   )
 })
@@ -71,7 +112,7 @@ test_that("Missing column only gets a flag when it is required", {
     )
   )
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(CheckSpec(lData, lSpec), "All 1 data"), "All specified"),
     regexp = "All 4 required columns"
   )
 
@@ -85,10 +126,9 @@ test_that("Missing column only gets a flag when it is required", {
     )
   )
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(CheckSpec(lData, lSpec), "All 1 data"), "All specified"),
     regexp = "Not all required columns"
   )
-
 })
 
 test_that("Validate column type works", {
@@ -102,7 +142,7 @@ test_that("Validate column type works", {
     )
   )
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(CheckSpec(lData, lSpec), "All 1"), "All 4"),
     regexp = "All specified columns"
   )
 
@@ -115,8 +155,7 @@ test_that("Validate column type works", {
     )
   )
   expect_message(
-    CheckSpec(lData, lSpec),
+    expect_message(expect_message(CheckSpec(lData, lSpec), "All 1"), "All 4"),
     regexp = "Not all columns"
   )
-
 })
