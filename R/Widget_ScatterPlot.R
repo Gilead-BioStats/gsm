e5ce6f6ce8b4bf09378616f2294d542f53e0cba2
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-09-10 18:43:18 +0000

    ensure the || and && are consistent

--- R/Widget_ScatterPlot.R
+++ R/Widget_ScatterPlot.R
@@ -43,8 +43,8 @@ Widget_ScatterPlot <- function(
   stopifnot(
     "dfResults is not a data.frame" = is.data.frame(dfResults),
     "lMetric must be a list, but not a data.frame" = is.list(lMetric) && !is.data.frame(lMetric),
-    "dfGroups is not a data.frame" = is.null(dfGroups) | is.data.frame(dfGroups),
-    "dfBounds is not a data.frame" = is.null(dfBounds) | is.data.frame(dfBounds),
+    "dfGroups is not a data.frame" = is.null(dfGroups) || is.data.frame(dfGroups),
+    "dfBounds is not a data.frame" = is.null(dfBounds) || is.data.frame(dfBounds),
     "bAddGroupSelect is not a logical" = is.logical(bAddGroupSelect),
     "strShinyGroupSelectID is not a character" = is.character(strShinyGroupSelectID),
     "bDebug is not a logical" = is.logical(bDebug)
--- R/Widget_TimeSeries.R
+++ R/Widget_TimeSeries.R
@@ -43,7 +43,7 @@ Widget_TimeSeries <- function(
   stopifnot(
     "dfResults is not a data.frame" = is.data.frame(dfResults),
     "lMetric must be a list, but not a data.frame" = is.list(lMetric) & !is.data.frame(lMetric),
-    "dfGroups is not a data.frame" = is.null(dfGroups) | is.data.frame(dfGroups),
+    "dfGroups is not a data.frame" = is.null(dfGroups) || is.data.frame(dfGroups),
     "strOutcome must be length 1" = length(strOutcome) == 1,
     "strOutcome is not a character" = is.character(strOutcome),
     "bAddGroupSelect is not a logical" = is.logical(bAddGroupSelect),
