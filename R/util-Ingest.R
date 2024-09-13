463eb5ee9678c1586db222c439ba306cb166fe57
Spencer Childress <spencer.c.childress@gmail.com>
2024-09-13 10:46:23 -0400

    get `Ingest` working

--- R/util-Ingest.R
+++ R/util-Ingest.R
@@ -1,41 +1,72 @@
-
 #' Ingests data from a source based on a given specification.
 #'
-#' @param lSource A list of source data frames.
-#' @param lSpec A list of column specifications.
+#' This function ingests data from a source based on a given specification. It iterates over each
+#' domain in the specification and checks if the columns exist in the source data. It then
+#' constructs a query to select the columns from the source and calls the `RunQuery` function to
+#' retrieve the data. The resulting data frames are stored in a list, where each data frame
+#' corresponds to a domain in the specification.
 #'
-#' @return A list of data frames, where each data frame corresponds to a domain in the specification.
+#' @param lSource `list` A named list of source data frames.
+#' @param lSpec `list` A named list of column specifications.
 #'
-#' @details This function ingests data from a source based on a given specification. It iterates over each domain in the specification and checks if the columns exist in the source data. It then constructs a query to select the columns from the source and calls the `RunQuery` function to retrieve the data. The resulting data frames are stored in a list, where each data frame corresponds to a domain in the specification.
+#' @return `list` A named list of data frames, where each data frame corresponds to a domain in the
+#' specification.
+#'
+#' @examples
+#' lSource <- list(
+#'   Source_ctms_study = clindata::ctms_study,
+#'   Source_ctms_site = clindata::ctms_site,
+#'   Source_SUBJ = clindata::rawplus_dm,
+#'   Source_AE = clindata::rawplus_ae,
+#'   Source_PD = clindata::ctms_protdev,
+#'   Source_LB = clindata::rawplus_lb,
+#'   Source_STUDCOMP = clindata::rawplus_studcomp,
+#'   Source_SDRGCOMP = clindata::rawplus_sdrgcomp,
+#'   Source_DATAENT = clindata::edc_data_pages,
+#'   Source_DATACHG = clindata::edc_data_points,
+#'   Source_QUERY = clindata::edc_queries,
+#'   Source_ENROLL = clindata::rawplus_enroll
+#' )
+#'
+#' lSpec <- MakeWorkflowList('ingest')[[1]]$spec
+#' lRaw <- Ingest(lSource, lSpec)
 #' 
 #' @export
 
-Ingest <- function(lSource, lSpec){
+Ingest <- function(lSource, lSpec) {
     lRaw <- lSpec %>% imap(
-        function(rawDomain, columnSpec){
+        function(columnSpecs, domain) {
             # check that the columns exist in the source data
 
             # write a query to select the columns from the source
-            strColQuery <- ''
-            for(columnName in names(columns)){
-                if('target_col' %in% columnSpec[columnName]){
-                targetName <- columSpec[columnName]$target_col  
-                strColQuery <- glue("{strColQuery}, {columnName} as {targetName}")
+            strColQuery <- c()
+            for (columnName in names(columnSpecs)) {
+                columnSpec <- columnSpecs[[columnName]]
+
+                if('target_col' %in% columnSpec[columnName]) {
+                    targetName <- columSpec[columnName]$target_col  
+                    strColQuery <- c(strColQuery, glue::glue("{columnName} as {targetName}"))
                 } else {
-                strColQuery <- glue("{strColQuery}, {columnName} ")
+                    strColQuery <- c(strColQuery, columnName)
                 }
             }
-            strQuery <- glue("SELECT {strColQuery} FROM df")
+
+            strQuery <- glue("SELECT {paste(strColQuery, collapse = ', ')} FROM df")
+            print(strQuery)
 
             # call RunQuery to get the data
-            #replace "source_" with "raw_" in the domain name
-            sourceDomain <- rawDomain %>% str_replace("Source_", "Raw_")
-            df <- RunQuery(lSource[[sourceDomain]], strQuery =  strQuery)
+            # replace "source_" with "raw_" in the domain name
+            # sourceDomain <- domain %>% str_replace("Source_", "Raw_")
+            df <- RunQuery(
+                lSource[[ domain ]],
+                strQuery =  strQuery
+            )
+
             return(df)
         }
     )
 
-    names(lRaw) <- names(lSpec)
+    #names(lRaw) <- names(lSpec)
 
     return(lRaw)
 }
