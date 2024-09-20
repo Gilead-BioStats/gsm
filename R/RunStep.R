c3e25577ecf0098d7ee7ce7b1e8edbeb5f5096ae
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-09-20 18:15:36 +0000

    match current website

--- R/RunStep.R
+++ R/RunStep.R
@@ -4,30 +4,30 @@
 #' `r lifecycle::badge("stable")`
 #'
 #' Runs a single step of an assessment workflow. This function is called by `RunWorkflow` for each
-#' step in the workflow. It prepares the parameters for the function call and then calls the function 
-#' specified in `lStep$name` with the prepared parameters. 
-#' 
-#' The primary utility of this function is to provide a prioritized parser for function parameterization.  
-#' Parameters should be specified as a named list in `lStep$params`, where each element is a key-value pair 
-#' that will be parsed and then passed to the specified function as a set of parameter names/values. 
-#' Parameter values should be specified as scalar strings. Those values are then pulled from `lMeta` or `lData` 
-#' when possible. When no matching `lData` or `lMeta` objects are found, parameter values are passed through as 
-#' stings. Note that parsing vectorized parameters is not supported at this time; they are passed directly 
-#' as character vectors. To pass a vector or list, we recommend saving it as an object in `lData`. 
-#' 
+#' step in the workflow. It prepares the parameters for the function call and then calls the function
+#' specified in `lStep$name` with the prepared parameters.
+#'
+#' The primary utility of this function is to provide a prioritized parser for function parameterization.
+#' Parameters should be specified as a named list in `lStep$params`, where each element is a key-value pair
+#' that will be parsed and then passed to the specified function as a set of parameter names/values.
+#' Parameter values should be specified as scalar strings. Those values are then pulled from `lMeta` or `lData`
+#' when possible. When no matching `lData` or `lMeta` objects are found, parameter values are passed through as
+#' stings. Note that parsing vectorized parameters is not supported at this time; they are passed directly
+#' as character vectors. To pass a vector or list, we recommend saving it as an object in `lData`.
+#'
 #' Full prioritization for parsing parameters is below:
-#' 
-#' 1. If a single parameter value is equal to "lMeta", the the full lMeta object is passed to the function 
+#'
+#' 1. If a single parameter value is equal to "lMeta", the the full lMeta object is passed to the function
 #' (for the given paramName).
 #' 2. If a single parameter value is equal to "lData", the full lData object is passed to the function.
 #' 3. If a single parameter value is equal to "lSpec", the full lSpec object is passed to the function.
-#' 4. If a single parameter value is found in names(lMeta), that property is pulled from lMeta (e.g. 
+#' 4. If a single parameter value is found in names(lMeta), that property is pulled from lMeta (e.g.
 #' lMeta${paramVal}) and passed to the function.
-#' 5. If a single parameter value is found in names(lData), that property is pulled from lData (e.g. 
+#' 5. If a single parameter value is found in names(lData), that property is pulled from lData (e.g.
 #' lData${paramVal}) and passed to the function.
-#' 6. Otherwise single parameter value is passed to the function as a string. 
-#' 7. If the parameter value is a vector, the vector is passed to the function as a vector or strings. 
-#' 
+#' 6. Otherwise single parameter value is passed to the function as a string.
+#' 7. If the parameter value is a vector, the vector is passed to the function as a vector or strings.
+#'
 #' @param lStep `list` single workflow step (typically pulled from `lWorkflow$steps`). Should
 #'   include the name of the function to run (`lStep$name`), name of the object where the function result should be saved (`lStep$output`) and configurable parameters (`lStep$params`) (if any)
 #' @param lData `list` a named list of domain level data frames.
@@ -35,7 +35,7 @@
 #' @param lMeta `list` a named list of meta data.
 #'
 #' @examples
-#' wf_mapping <- MakeWorkflowList("mapping")
+#' wf_mapping <- MakeWorkflowList(strPath = "/inst/workflow/1_mappings")
 #' lStep <- MakeWorkflowList("kri0001")[["kri0001"]][["steps"]][[1]]
 #' lMeta <- MakeWorkflowList("kri0001")[["kri0001"]][["meta"]]
 #' lData <- list(
