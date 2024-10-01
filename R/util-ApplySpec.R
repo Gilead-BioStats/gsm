d6ca050bf599bc91ed8bf3387cc7ed02b8770660
Spencer Childress <spencer.c.childress@gmail.com>
2024-10-01 11:07:57 -0400

    add header documentation to `ApplySpec`

--- R/util-ApplySpec.R
+++ R/util-ApplySpec.R
@@ -1,3 +1,23 @@
+#' Apply Data Specification
+#'
+#' @description
+#' `r lifecycle::badge("stable")`
+#'
+#' Apply a column specification to a data frame. The column specification is a
+#' named list where the names are the target column names and the values are
+#' lists of column specifications. The column specifications can include:
+#'   - `required`: Whether the column is required.
+#'   - `type`: The data type to convert the column to.
+#'   - `source_col`: The name of the source column to map to the target column.
+#'
+#' @param dfSource `data.frame` Data frame to apply the column specification to.
+#' @param columnSpecs `list` Column specification.
+#' @param domain `character` Domain name.
+#'
+#' @return `data.frame` `dfSource` with the column specification applied.
+#'
+#' @export
+
 ApplySpec <- function(dfSource, columnSpecs, domain) {
     # Add all columns to the spec if '_all' is present.
     if ('_all' %in% names(columnSpecs)) {
@@ -49,21 +69,21 @@ ApplySpec <- function(dfSource, columnSpecs, domain) {
     strQuery <- glue("SELECT {strColQuery} FROM df")
 
     # call RunQuery to get the data
-    dfMapped <- RunQuery(
+    dfTarget <- RunQuery(
         dfSource,
         strQuery = strQuery
     )
 
-    # Apply data types to each column in [ dfMapped ].
-    for (col in names(dfMapped)) {
+    # Apply data types to each column in [ dfTarget ].
+    for (col in names(dfTarget)) {
         if (col %in% names(columnSpecs)) {
             spec <- columnSpecs[[ col ]]
             if ('type' %in% names(spec)) {
                 # TODO: handle character NA values
-                dfMapped[[ col ]] <- as(dfMapped[[ col ]], spec$type)
+                dfTarget[[ col ]] <- as(dfTarget[[ col ]], spec$type)
             }
         }
     }
 
-    return(dfMapped)
+    return(dfTarget)
 }
