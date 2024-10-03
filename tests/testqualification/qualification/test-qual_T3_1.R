56d641d06551f5028eb0fb3e7238f8d9b180a4ce
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-10-03 19:04:17 +0000

    modified to make it work

--- tests/testqualification/qualification/test-qual_T3_1.R
+++ tests/testqualification/qualification/test-qual_T3_1.R
@@ -1,9 +1,4 @@
-# This test no longer applies right because of `CheckSpec()` ? RunWorkflow has it such that `lData` must contain all data.frames in `lSpec`.
-
-
 ## Test Setup
-source(system.file("tests", "testqualification", "qualification", "qual_data.R", package = "gsm"))
-
 ae_workflow <- flatten(MakeWorkflowList("kri0001"))
 
 steps <- c(1, 3:length(ae_workflow$steps))
@@ -12,10 +7,18 @@ outputs <- map_vec(ae_workflow$steps[steps], ~ .x$output)
 
 ## Test Code
 testthat::test_that("Given pre-processed input data, a properly specified Workflow for a KRI creates summarized and flagged data", {
-  test <- robust_runworkflow(ae_workflow, list(dfInput = gsm::analyticsInput), step = steps)
+  test <- robust_runworkflow(
+    ae_workflow,
+    list(
+      Analysis_Input = gsm::analyticsInput,
+      Mapped_AE = mapped_data[["Mapped_AE"]],
+      Mapped_SUBJ = mapped_data[["Mapped_SUBJ"]]
+    ),
+    step = steps
+  )
   expect_true(all(outputs %in% names(test)))
   expect_true(is.vector(test$vThreshold))
-  expect_true(all(map_lgl(test[outputs[outputs != "vThreshold"]], is.data.frame)))
+  expect_true(all(map_lgl(test[outputs[!(outputs %in% c("vThreshold", "lAnalysis"))]], is.data.frame)))
   expect_equal(nrow(test$Analysis_Flagged), nrow(test$Analysis_Summary))
   expect_identical(sort(test$Analysis_Flagged$GroupID), sort(test$Analysis_Summary$GroupID))
 })
