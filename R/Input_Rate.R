f0c5f771c8db995478f86530b8778ffa2230d239
Jon Harmon <jon.harmon@atorusresearch.com>
2024-10-23 14:41:10 -0500

    Update rbm-viz. (#1907)
    * Update rbm-viz.
    
    * Update RunStep docs.
    
    * Qualify fields to avoid notes.

--- R/Input_Rate.R
+++ R/Input_Rate.R
@@ -136,16 +136,16 @@ Input_Rate <- function(
   # Calculate Numerator
   dfNumerator_subj <- dfNumerator %>%
     rename(SubjectID = {{ strSubjectCol }}) %>%
-    mutate(Numerator = if(strNumeratorMethod == "Count") 1 else .data[[strNumeratorCol]]) %>%
-    group_by(SubjectID) %>%
-    summarise(Numerator = sum(Numerator, na.rm = TRUE))
+    mutate(Numerator = if (strNumeratorMethod == "Count") 1 else .data[[strNumeratorCol]]) %>%
+    group_by(.data$SubjectID) %>%
+    summarise(Numerator = sum(.data$Numerator, na.rm = TRUE))
 
   # Calculate Denominator
   dfDenominator_subj <- dfDenominator %>%
     rename(SubjectID = {{ strSubjectCol }}) %>%
-    mutate(Denominator = if(strDenominatorMethod == "Count") 1 else .data[[strDenominatorCol]]) %>%
-    group_by(SubjectID) %>%
-    summarise(Denominator = sum(Denominator, na.rm = TRUE))
+    mutate(Denominator = if (strDenominatorMethod == "Count") 1 else .data[[strDenominatorCol]]) %>%
+    group_by(.data$SubjectID) %>%
+    summarise(Denominator = sum(.data$Denominator, na.rm = TRUE))
 
   if (all(dfDenominator_subj$Denominator == 0)) {
     cli::cli_abort(
