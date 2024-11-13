9b4768683a4da7d15d1ff37e443bbf8981fd2e0c
zdz2101 <zelos.zhu@atorusresearch.com>
2024-11-13 10:53:56 -0800

    test get/set, good first draft

--- tests/testthat/test-util-logger.R
+++ tests/testthat/test-util-logger.R
@@ -14,21 +14,18 @@ test_that("default logger works as expected", {
 # Example unit test for set_logger
 
 test_that("logger writes to a unique log file", {
-  log_file <- tempfile("test_log_", fileext = ".log")
-
-  # Set a logger with the unique log file
-  set_logger(name = "test_logger", output_target = "file", log_file = log_file)
+  set_logger(name = "test_logger", output_target = "file")
 
   # Log a message
   logger <- get_logger("test_logger")
   log4r::info(logger, "Write to user specified log.")
 
   # Check if the log file exists and contains the message
-  expect_true(file.exists(log_file))
-  log_content <- readLines(log_file)
+  expect_true(file.exists("test_logger.log"))
+  log_content <- readLines("test_logger.log")
   expect_true(grepl("Write to user specified", log_content))
 
   # Clean up the test log file
-  file.remove(log_file)
+  file.remove("test_logger.log")
 })
 
