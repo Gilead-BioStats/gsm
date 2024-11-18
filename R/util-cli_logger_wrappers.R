90497b65ea5a662c76eb8d8b8ac90cd266a0c690
zdz2101 <zelos.zhu@atorusresearch.com>
2024-11-18 14:37:05 -0800

    omg finally

--- R/util-cli_logger_wrappers.R
+++ R/util-cli_logger_wrappers.R
@@ -1,8 +1,10 @@
-#' do some work
-#' @param reps fake argument
 #' @export
-do_work <- function(message, cli_det) {
-  log_info(.le$logger, message = message, cli_det = cli_det)
-  return(TRUE)
+LogMessage <- function(level, message, cli_detail = NULL) {
+  switch(
+    toupper(level),
+    "INFO" = log_info(logger = .le$logger, message = message, cli_detail = cli_detail),
+    "WARN" = log_warn(logger = .le$logger, message = message),
+    "ERROR" = log_error(logger = .le$logger, message = message)
+  )
 }
 
