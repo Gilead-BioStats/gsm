3e5eca03e0b59318be176aa97f2d6005098124b4
zdz2101 <zelos.zhu@atorusresearch.com>
2024-11-21 08:26:11 -0800

    Align with dev

--- R/util-CalculatePercentage.R
+++ R/util-CalculatePercentage.R
@@ -15,18 +15,25 @@
 #' @return A data frame containing two additional columns for the precentage value and associated string
 #' @export
 CalculatePercentage <- function(data, strCurrentCol, strTargetCol, strPercVal, strPercStrVal) {
-  if (!(strCurrentCol %in% names(data) & strTargetCol %in% names(data))) {
+  if (!(strCurrentCol %in% names(data))) {
     LogMessage(
       level = "fatal",
-      message = "Check that both {strCurrentCol} and {strTargetCol} are in data"
+      message = "Check that {strCurrentCol} is in data"
     )
   }
 
-  data <- data %>%
-    dplyr::mutate(
-      {{ strPercVal }} := round(.data[[strCurrentCol]] * 100 / .data[[strTargetCol]], 1),
-      {{ strPercStrVal }} := paste0(.data[[strCurrentCol]], " / ", .data[[strTargetCol]], " (", .data[[strPercVal]], "%)")
-    )
+  if (strTargetCol %in% names(data)) {
+    data <- data %>%
+      dplyr::mutate(
+        {{ strPercVal }} := round(.data[[strCurrentCol]] * 100 / .data[[strTargetCol]], 1),
+        {{ strPercStrVal }} := paste0(.data[[strCurrentCol]], " / ", .data[[strTargetCol]], " (", .data[[strPercVal]], "%)")
+      )
+  } else {
+    data <- data %>%
+      dplyr::mutate(
+        {{ strPercStrVal }} := .data[[strCurrentCol]]
+      )
+  }
 
   return(data)
 }
