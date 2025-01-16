5b51c92ca668b7f54ff53afe85517dfd335b26f0
zdz2101 <zelos.zhu@atorusresearch.com>
2025-01-16 12:39:04 -0800

    fix roxygen stuff

--- R/aaa-shared.R
+++ R/aaa-shared.R
@@ -5,20 +5,20 @@
 #' @description Reused parameter definitions are gathered here for easier usage.
 #'
 #' @param dfMetrics `data.frame` Metric-specific metadata for use in charts and
-#'   reporting. Created by passing an `lWorkflow` object to [MakeMetric()].
+#'   reporting. Created by passing an `lWorkflow` object to [gsm.mapping::MakeMetric()].
 #'   Expected columns: `File`, `MetricID`, `Group`, `Abbreviation`, `Metric`,
 #'   `Numerator`, `Denominator`, `Model`, `Score`, and `Threshold`. For more
 #'   details see the Data Model vignette: `vignette("DataModel", package =
 #'   "gsm")`.
 #' @param dfResults `data.frame` A stacked summary of analysis pipeline output.
 #'   Created by passing a list of results returned by [Summarize()] to
-#'   [BindResults()]. Expected columns: `GroupID`, `GroupLevel`, `Numerator`,
+#'   [gsm.mapping::BindResults()]. Expected columns: `GroupID`, `GroupLevel`, `Numerator`,
 #'   `Denominator`, `Metric`, `Score`, `Flag`, `MetricID`, `StudyID`,
 #'   `SnapshotDate`.
 #' @param dfBounds `data.frame` Set of predicted percentages/rates and upper-
 #'   and lower-bounds across the full range of sample sizes/total exposure
 #'   values for reporting. Created by passing `dfResults` and `dfMetrics` to
-#'   [MakeBounds()]. Expected columns: `Threshold`, `Denominator`, `Numerator`,
+#'   [gsm.mapping::MakeBounds()]. Expected columns: `Threshold`, `Denominator`, `Numerator`,
 #'   `Metric`, `MetricID`, `StudyID`, `SnapshotDate`.
 #' @param dfGroups `data.frame` Group-level metadata dictionary. Created by
 #'   passing CTMS site and study data to [gsm.mapping::MakeLongMeta()]. Expected columns:
@@ -28,7 +28,7 @@
 #'   `GroupLevel`, `Numerator`, `Denominator` and/or columns specified in
 #'   `strCountCol` and `strGroupCol`.
 #' @param lMetric `list` Metric-specific metadata for use in charts and
-#'   reporting. Created by passing an `lWorkflow` object to [MakeMetric()] and
+#'   reporting. Created by passing an `lWorkflow` object to [gsm.mapping::MakeMetric()] and
 #'   turing it into a list. Expected columns: `File`,`MetricID`, `Group`,
 #'   `Abbreviation`, `Metric`, `Numerator`, `Denominator`, `Model`, `Score`, and
 #'   `strThreshold`. For more details see the Data Model vignette:
